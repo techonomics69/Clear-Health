5e55a5919af879bbaa52eda324ee9a4c4aa47813
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-04-19 12:10:53 +0530

    getCitiesFromGivenState change in response

--- app/Http/Controllers/API/CaseManagementController.php
+++ app/Http/Controllers/API/CaseManagementController.php
@@ -386,9 +386,9 @@ public function create_patient(Request $request)
     $response = curl_exec($curl);
 
     curl_close($curl);
-    echo $response;
+    //echo $response;
 
-    return $this->sendResponse(json_decode($response),'State data recieved successfully');
+    return $this->sendResponse(json_decode($response),'City data recieved successfully');
 
 
 

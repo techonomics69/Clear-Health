5c96e3077ad62963b3141066c687e0b0e75dab56
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-04-21 19:43:32 +0530

    Update QuizController.php

--- app/Http/Controllers/API/QuizController.php
+++ app/Http/Controllers/API/QuizController.php
@@ -27,7 +27,7 @@ class QuizController extends BaseController
     {
         $quiz = Quiz::find($id);
         $quiz['sub_questions'] = Quiz::join('sub_question_answer', 'quizzes.id', '=', 'sub_question_answer.parent_question_id')         
-            ->select('sub_question_answer.*')
+            ->select('sub_question_answer.*','quizzes.question')
             ->where('quizzes.sub_question','=','No')
             ->orderBy('sub_question_answer.id','ASC')
             ->get();

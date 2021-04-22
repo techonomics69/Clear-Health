da98c9840328cface947391673b592baf38ff625
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-04-22 11:06:25 +0530

    Update ProductController.php

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -120,7 +120,7 @@ class ProductController extends BaseController
 
     public function getskincareplan()
     {
-        $skincareplan = Product::where('used_for_plan','Yes')->first();
+        $skincareplan = Product::where('used_for_plan','Yes')->get();
         $skincareplan['image'] = url('/public/images/Products/').'/'.$skincareplan['image'];
   
         if (is_null($skincareplan)) {

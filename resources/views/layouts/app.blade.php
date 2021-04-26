5bc5910d918627d269707d97e834ca5a2a6b8acb
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-04-26 14:24:31 +0530

    active change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -492,7 +492,7 @@ data-open="click" data-menu="vertical-menu" data-col="2-columns">
                 @endcanany
 
                  @canany(['customer-list', 'customer-create', 'customer-edit', 'customer-delete'])
-                <li class="nav-item sub-menu has-sub product-sub-menu {{ Request::is('admin/customer*') ? 'active' : '' }}">
+                <li class="nav-item sub-menu has-sub customer-sub-menu {{ Request::is('admin/customer*') ? 'active' : '' }}">
                   <a class="menu-item" href="#">
                   <svg xmlns="http://www.w3.org/2000/svg" id="Capa_1" enable-background="new 0 0 512 512" height="20" viewBox="0 0 512 512" width="20"><g><circle cx="256" cy="119.631" r="87"/><circle cx="432" cy="151.63" r="55"/><circle cx="80" cy="151.63" r="55"/><path d="m134.19 256.021c-21.65-17.738-41.257-15.39-66.29-15.39-37.44 0-67.9 30.28-67.9 67.49v109.21c0 16.16 13.19 29.3 29.41 29.3 70.026 0 61.59 1.267 61.59-3.02 0-77.386-9.166-134.137 43.19-187.59z"/><path d="m279.81 241.03c-43.724-3.647-81.729.042-114.51 27.1-54.857 43.94-44.3 103.103-44.3 175.48 0 19.149 15.58 35.02 35.02 35.02 211.082 0 219.483 6.809 232-20.91 4.105-9.374 2.98-6.395 2.98-96.07 0-71.226-61.673-120.62-111.19-120.62z"/><path d="m444.1 240.63c-25.17 0-44.669-2.324-66.29 15.39 51.965 53.056 43.19 105.935 43.19 187.59 0 4.314-7.003 3.02 60.54 3.02 16.8 0 30.46-13.61 30.46-30.34v-108.17c0-37.21-30.46-67.49-67.9-67.49z"/></g></svg>
                    <span class="menu-title siebartext" data-i18n="">Customers</span> 
@@ -521,12 +521,12 @@ data-open="click" data-menu="vertical-menu" data-col="2-columns">
                                              
                   </a>
                     <ul class="menu-content">
-                        <li class="sub-menu product-list ">
+                        <li class="sub-menu product-list {{ Request::is('admin/products') ? 'active' : '' }}">
                           <a href="{{ route('products.index') }}">
                             Products List
                           </a>                          
                         </li>
-                        <li class="sub-menu  product-create">
+                        <li class="sub-menu  product-create {{ Request::is('admin/products/create') ? 'active' : '' }} ">
                           <a href="{{ route('products.create') }}">
                             Product Create
                           </a>                          
@@ -948,6 +948,17 @@ data-open="click" data-menu="vertical-menu" data-col="2-columns">
                   
                       $(".user-sub-menu").addClass('open active');
                       $(".user-list").addClass('active');
+                    }
+                  }
+
+                 else if(pagename == "customer"){
+                  if(splitUrl[splitUrl.length-1]=="create"){
+                    $(".customer-sub-menu").addClass('open active');
+                      $(".customer-create").addClass('active');
+                  }
+                  else{
+                  $(".customer-menu").addClass('open active');
+                      $(".customer-list").addClass('active');
                     }
                   }
 

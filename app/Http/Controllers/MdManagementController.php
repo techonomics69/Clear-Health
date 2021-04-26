f78e57ef07bf3d212ad7b040ede63cb2a7a190a3
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-04-26 16:41:03 +0530

    Update MdManagementController.php

--- app/Http/Controllers/MdManagementController.php
+++ app/Http/Controllers/MdManagementController.php
@@ -115,9 +115,6 @@ class MdManagementController extends Controller
         $mdmanagement = Mdmanagement::find($id);
 
         $this->validate($request, [
-            'name' => 'required|unique:md_managment,name',
-            'status' => 'required|not_in:0',
-            'language_id' => 'required|not_in:0',
             'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:5000',         
         ]);
         

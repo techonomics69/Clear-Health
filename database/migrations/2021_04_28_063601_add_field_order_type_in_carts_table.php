053bafe893970b352351db203615b66f66af6c65
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-04-28 12:26:34 +0530

    Update 2021_04_28_063601_add_field_order_type_in_carts_table.php

--- database/migrations/2021_04_28_063601_add_field_order_type_in_carts_table.php
+++ database/migrations/2021_04_28_063601_add_field_order_type_in_carts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddFieldOrderTypeInCartsTablee extends Migration
+class AddFieldOrderTypeInCartsTable extends Migration
 {
     /**
      * Run the migrations.

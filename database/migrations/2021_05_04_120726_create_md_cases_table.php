6492a0dd35c5ccefd4b114f3a7b5dd381ec1403a
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-04 18:00:01 +0530

    Create 2021_05_04_120726_create_md_cases_table.php

---
+++ database/migrations/2021_05_04_120726_create_md_cases_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMdCasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('md_cases', function (Blueprint $table) {
+            $table->id();
+            $table->string('prioritized_at')->nullable();
+            $table->string('prioritized_reason')->nullable();
+            $table->string('cancelled_at')->nullable();
+            $table->timestamp('created_at'); 
+            $table->string('support_reason')->nullable();
+            $table->string('case_id');
+            $table->string('status');
+            $table->string('user_id');
+            $table->string('system_case_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('md_cases');
+    }
+}

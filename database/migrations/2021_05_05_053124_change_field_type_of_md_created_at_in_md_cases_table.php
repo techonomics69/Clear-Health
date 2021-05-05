ef32a8978861c759279054ee39163ec1020dfe4b
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-05 11:16:54 +0530

    Create 2021_05_05_053124_change_field_type_of_md_created_at_in_md_cases_table.php

---
+++ database/migrations/2021_05_05_053124_change_field_type_of_md_created_at_in_md_cases_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeFieldTypeOfMdCreatedAtInMdCasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('md_cases', function (Blueprint $table) {
+            $table->string('md_created_at')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('md_cases', function (Blueprint $table) {
+            //
+        });
+    }
+}

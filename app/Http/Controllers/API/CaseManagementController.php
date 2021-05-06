b812da69d95d82383395174b8cc61e755d866980
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-06 10:50:28 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/API/CaseManagementController.php
+++ app/Http/Controllers/API/CaseManagementController.php
@@ -14,7 +14,7 @@ use App\Models\User;
 use App\Models\Parentdetail;
 use App\Models\Mdcases;
 use App\Models\Mdmanagement;
-use File;
+use CURLFILE;
 
 class CaseManagementController extends BaseController
 {

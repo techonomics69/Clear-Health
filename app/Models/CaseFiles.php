199e646c40324fc1acdc4ea1abf8f307df50d5f3
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-07 13:44:12 +0530

    Update CaseFiles.php

--- app/Models/CaseFiles.php
+++ app/Models/CaseFiles.php
@@ -9,5 +9,5 @@ class CaseFiles extends Model
 {
     use HasFactory;
     protected $table = "case_files";
-    protected $fillable = ['name', 'file', 'case_id', 'url', 'url_thumbnail', 'file_id','mime_type','system_case_id','system_file','user_id'];
+    protected $fillable = ['name', 'file', 'case_id', 'url', 'url_thumbnail', 'file_id','mime_type','system_case_id','system_file','user_id','md_file_name','md_mime_type','md_url','md_url_thumbnail','md_file_id'];
 }

ebe89a48ded42097b71606c43bac35fce8e51a33
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-10 10:41:56 +0530

    Create 2021_05_10_050535_add_field_verified_by_vouch_in_users_table.php

---
+++ database/migrations/2021_05_10_050535_add_field_verified_by_vouch_in_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFieldVerifiedByVouchInUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('verified_by_vouch')->after('status')->comment="pending,success,failed  ";
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}

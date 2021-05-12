718b9343bb67874834028d8474ea56ff033023ec
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-12 17:40:55 +0530

    Update FeeController.php

--- app/Http/Controllers/API/FeeController.php
+++ app/Http/Controllers/API/FeeController.php
@@ -50,10 +50,10 @@ class FeeController extends BaseController
             else
             {
                 $shipping_fee = Fees::where('status','1')->where('fee_type','shipping_fee')->get();
-                echo "<pre>";
+                /*echo "<pre>";
                 print_r($shipping_fee);
-                echo "</pre>";
-                //$shipping_fee=$shipping_fee['amount'];
+                echo "</pre>";*/
+                $shipping_fee=$shipping_fee['amount'];
             }
 
         }else{

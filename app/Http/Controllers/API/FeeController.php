1cf0e99c22cd90879177987cf78ad066bcc2b32d
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-12 18:12:49 +0530

    Update FeeController.php

--- app/Http/Controllers/API/FeeController.php
+++ app/Http/Controllers/API/FeeController.php
@@ -37,7 +37,7 @@ class FeeController extends BaseController
 
         if($product_type == "Non Prescribed")
         {
-            if($order_total_amount > $minimum_shipping_amount['amount'])
+            if($order_total_amount > $minimum_shipping_amount[0]['amount'])
             {
                 $shipping_fee = 0;
             }

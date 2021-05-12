bb6bec0efe8db29acc8a070609b7e5dfede11dab
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-12 17:47:53 +0530

    Update FeeController.php

--- app/Http/Controllers/API/FeeController.php
+++ app/Http/Controllers/API/FeeController.php
@@ -60,7 +60,7 @@ class FeeController extends BaseController
 
         }
         //$fees['order_total_amount'] = $order_total_amount;
-        $fees['shiping_fee'] = $shipping_fee[0];
+        $fees['shiping_fee'] = $shipping_fee;
         $fees['minimum_shipping_amount'] = $minimum_shipping_amount[0]['amount'];
 
         return $this->sendResponse($fees,'Fees Retrived successfully');

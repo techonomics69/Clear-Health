5b798420435d3d3f3e24f5d7f5749c811e7a38bd
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-14 17:42:18 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -92,6 +92,7 @@ Route::post('forgot_password',[RegisterController::class, 'forgot_password']);
     Route::post('detach_file_from_case', [CaseManagementController::class, 'detach_file_from_case']);
     Route::post('createMessageFile', [CaseManagementController::class, 'createMessageFile']);
     Route::post('createMessage', [CaseManagementController::class, 'createMessage']);
+    Route::post('setMessageAsRead', [CaseManagementController::class, 'setMessageAsRead']);
     //end of md api
 
     Route::post('user/update_vouch_status/{id}', [UserController::class, 'updateVerifiedByVouch']);

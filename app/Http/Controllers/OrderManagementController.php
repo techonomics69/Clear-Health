6aff247a74563015ed7fdf66de198ba6b5594212
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-18 17:56:56 +0530

    Update OrderManagementController.php

--- app/Http/Controllers/OrderManagementController.php
+++ app/Http/Controllers/OrderManagementController.php
@@ -49,7 +49,7 @@ foreach($order as $key=>$val){
    $product_details  = Cart::join('products', 'products.id', '=', 'carts.product_id')->whereIn('carts.id', $cart_ids)->select('products.name AS product_name')->get();//'products.name AS product_name' , 'products.price'
 
     echo "<pre>";
-    print_r($product_details[0]['product_name']);
+    print_r(implode($product_details[0]['product_name']));
     echo "<pre>";
     
    $product_name[] = $product_details[0]['product_name'] ;

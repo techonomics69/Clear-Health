c09d94e00d8750af957964807cc4f4df7ce7f0c3
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-18 14:25:20 +0530

    add answer table

---
+++ database/migrations/2021_05_18_085104_create_answer-table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnswerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answer', function (Blueprint $table) {
+            $table->id();
+            $table->string('answer');
+            $table->string('user_id');
+            $table->string('case_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}

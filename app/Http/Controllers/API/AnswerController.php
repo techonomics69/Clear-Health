35ab3fd04483e710186372499ea59eb4b3acbc22
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-18 15:02:12 +0530

    answer api add &  model add

--- app/Http/Controllers/API/AnswerController.php
+++ app/Http/Controllers/API/AnswerController.php
@@ -4,53 +4,72 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\API\BaseController as BaseController;
 use Illuminate\Http\Request;
+use App\Models\Answer;
 use Validator;
 use Exception;
 
 class AnswerController extends BaseController
 {
-        public function index()
+    public function index()
     {
 
     }
 
     //answer
-     public function answer()
+    public function answer(Request $request)      
     {
-     
+        $data = $request->all();
+        if(isset($data['token']) && !empty($data['token'])):
+
+            $data['user_id'] = (new Parser())->parse($data['token'])->getClaims()['sub']->getValue();
+
+    endif;       
+    try{
+        $validator = Validator::make($data, [
+            'user_id' => 'required',
+            'case_id' => 'required',
+        ]);
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors()->all());       
+        }
+        $answer = Answer::create($data);
+        return $this->sendResponse(array(), 'Item Added Successfully');
+    }catch(\Exception $ex){
+        return $this->sendError('Server error',array($ex->getMessage()));
     }
+}
 
 
-    public function create()
-    {
-        
-    }
+public function create()
+{
 
-    
-    public function store(Request $request)
-    {
-        
-    }
+}
 
-    
-    public function show($id)
-    {
-        
-    }
 
-    public function edit($id)
-    {
-        
-    }
+public function store(Request $request)
+{
 
-    
-    public function update(Request $request, $id)
-    {
-        
-    }
+}
 
-    public function destroy($id)
-    {
 
-    }
+public function show($id)
+{
+
+}
+
+public function edit($id)
+{
+
+}
+
+
+public function update(Request $request, $id)
+{
+
+}
+
+public function destroy($id)
+{
+
+}
 }

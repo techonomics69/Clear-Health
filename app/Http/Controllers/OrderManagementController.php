ec4d8f69111e9a0062bbbbb0de1b4d3ce902c699
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-18 16:40:38 +0530

    Update OrderManagementController.php

--- app/Http/Controllers/OrderManagementController.php
+++ app/Http/Controllers/OrderManagementController.php
@@ -9,7 +9,6 @@ use App\Models\Cart;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use App\Http\Controllers\Controller;
-use DB;
     
 class OrderManagementController extends Controller
 { 
@@ -31,50 +30,11 @@ class OrderManagementController extends Controller
     {
 
   $order = checkout::join('users', 'users.id', '=', 'checkout.user_id')
-            //->leftjoin("carts",\DB::raw("FIND_IN_SET(carts.id, checkout.cart_id)") ,">",\DB::raw("'0'"))
-            ->join('carts','carts.id', '=', 'checkout.cart_id')
-            //->join('products', 'products.id', '=', 'carts.product_id')
-            ->select('users.first_name', 'users.last_name','users.mobile', 'checkout.total_amount','checkout.case_id','checkout.created_at','checkout.order_id','checkout.medication_type','checkout.id','checkout.cart_id')->orderBy('checkout.id', 'DESC')->get();//'products.name AS product_name' , 'products.price'
-
-foreach($order as $key=>$val){
-    $cart_ids = explode(',', $val['cart_id']);
-
-   
-    $product_name = array();
-   $product_details  = Cart::join('products', 'products.id', '=', 'carts.product_id')->whereIn('carts.id', $cart_ids)->select('products.name AS product_name')->get();//'products.name AS product_name' , 'products.price'
-
-    echo "<pre>";
-    print_r(implode(',',$product_details[0]['product_name']));
-    echo "<pre>";
-    exit();
-
-   $product_name[] = $product_details[0]['product_name'] ;
-   $order[$key]['product_name'] = implode(',',$product_name);
-
-}
-
-
-echo "<pre>";
-    print_r($order);
-    echo "<pre>";
-exit();
+            ->leftjoin("carts",\DB::raw("FIND_IN_SET(carts.id, checkout.cart_id)") ,">",\DB::raw("'0'"))
+            //->join('carts','carts.id', '=', 'checkout.cart_id')
+            ->join('products', 'products.id', '=', 'carts.product_id')
+            ->select('users.name', 'users.mobile', 'products.name AS product_name' , 'carts.product_cart_price', 'checkout.total_amount','checkout.case_id','checkout.created_at')->orderBy('checkout.id', 'DESC')->get();
     
-
-
-//$product_name=array();
-//$product_name['product_name'] = $order;
-
-/*foreach($order as $orders) {
-    $product_name[]=$orders;
- 
-}*/
-
-
-/*echo "<pre>";
-print_r($product_name);
-echo "</pre>";
-die();
-*/
      return view('ordermanagement.index',compact('order'));
     }
 

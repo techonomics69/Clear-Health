4c2171de4a3909ec0a855ab5abb9be3af56b0cbc
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-19 13:03:17 +0530

    Update QuizAnswerController.php

--- app/Http/Controllers/API/QuizAnswerController.php
+++ app/Http/Controllers/API/QuizAnswerController.php
@@ -163,6 +163,11 @@ class QuizAnswerController extends BaseController
         $case_id = $request['case_id'];
 
             $answer_data = Answers::where('user_id', $user_id)->where('case_id', $case_id)->get();
+
+            echo "<pre>";
+            print_r( $answer_data);
+            echo "<pre>";
+            exit();
             $recommendation = json_decode($answer_data);
 
            

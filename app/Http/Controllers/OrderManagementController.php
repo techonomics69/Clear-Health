6bb29bb9003ef93fe590611c80158dfde31f028d
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-19 17:53:40 +0530

    Update OrderManagementController.php

--- app/Http/Controllers/OrderManagementController.php
+++ app/Http/Controllers/OrderManagementController.php
@@ -68,7 +68,7 @@ public function store(Request $request)
 
 }
 
-public function show($case_id)
+public function show($id)
 {
    $user_case_management_data = CaseManagement::join('users','case_managements.user_id', '=', 'users.id')->select('case_managements.*','users.first_name','users.last_name','users.email')->where('case_managements.id',$id)->first();
    $category = QuizCategory::pluck('name', 'id')->toArray();

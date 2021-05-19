672005c07b826831e719389a004e1abe6596c5ca
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-19 15:54:19 +0530

    Update OrderManagementController.php

--- app/Http/Controllers/OrderManagementController.php
+++ app/Http/Controllers/OrderManagementController.php
@@ -62,9 +62,9 @@ public function store(Request $request)
 
 }
 
-public function show($case_id)
+public function show($id)
 {
- $user_case_management_data = CaseManagement::join('users','case_managements.user_id', '=', 'users.id')->select('case_managements.*','users.first_name','users.last_name','users.email')->where('case_managements.id',$case_id)->first();
+ $user_case_management_data = CaseManagement::join('users','case_managements.user_id', '=', 'users.id')->select('case_managements.*','users.first_name','users.last_name','users.email')->where('case_managements.id',$id)->first();
         $category = QuizCategory::pluck('name', 'id')->toArray();
         //foreach ($user_case_management_data as $key => $value) {
         $quiz= QuizAnswer::join('quizzes','quiz_answers.question_id', '=', 'quizzes.id')->select('quiz_answers.*','quizzes.question','quizzes.category_id')->where('case_id', $user_case_management_data['id'])->OrderBy('id', 'ASC')->get();

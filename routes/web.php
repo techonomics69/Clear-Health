885e0b839e86b70bbc5131d8817617ec195a73b3
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-19 17:36:57 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -239,7 +239,7 @@ Route::group(['middleware' => ['auth']],function(){
     //Order Managment
     Route::get('admin/ordermanagement', [OrderManagementController::class, 'index'])->name('ordermanagement.index');
     /*Route::get('admin/ordermanagement/show/{case_id}', [OrderManagementController::class, 'show'])->name('ordermanagement.show');*/
-    Route::get('admin/ordermanagement/show/{$case_id}', [OrderManagementController::class, 'show'])->name('ordermanagement.show');
+    Route::get('admin/ordermanagement/show/{case_id}', [OrderManagementController::class, 'show'])->name('ordermanagement.show');
 
 
 });

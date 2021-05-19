eb4e1569a99aa3a0c8a127c2558ccc7301441630
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-19 13:16:45 +0530

    Update QuizAnswerController.php

--- app/Http/Controllers/API/QuizAnswerController.php
+++ app/Http/Controllers/API/QuizAnswerController.php
@@ -46,30 +46,30 @@ class QuizAnswerController extends BaseController
         if(isset($data['token']) && !empty($data['token'])):
 
             $data['user_id'] = (new Parser())->parse($data['token'])->getClaims()['sub']->getValue();
-            $data['case_id'] = CaseManagement::where('user_id', $data['user_id'])->OrderBy('id','desc')->first()->id;
+        $data['case_id'] = CaseManagement::where('user_id', $data['user_id'])->OrderBy('id','desc')->first()->id;
 
-        endif;       
-        try{
-            $validator = Validator::make($data, [
-                'user_id' => 'required',
-                'question_id' => 'required',
-                'answer' => 'required',
-            ]);
-            if($validator->fails()){
-                return $this->sendError('Validation Error.', $validator->errors()->all());       
-            }
-            $answer = QuizAnswer::where('user_id', $data['user_id'])->where('question_id', $data['question_id'])->where('case_id', $data['case_id'])->first();
-            if(!empty($answer)):
-            	$answer->update($data);
-            else:
-            	$quizAns = QuizAnswer::create($data);
-            endif;
-            
-            return $this->sendResponse(array(), 'Answer submitted successfully');
-        }catch(\Exception $ex){
-            return $this->sendError('Server error',array($ex->getMessage()));
+    endif;       
+    try{
+        $validator = Validator::make($data, [
+            'user_id' => 'required',
+            'question_id' => 'required',
+            'answer' => 'required',
+        ]);
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors()->all());       
         }
-    }
+        $answer = QuizAnswer::where('user_id', $data['user_id'])->where('question_id', $data['question_id'])->where('case_id', $data['case_id'])->first();
+        if(!empty($answer)):
+         $answer->update($data);
+     else:
+         $quizAns = QuizAnswer::create($data);
+     endif;
+     
+     return $this->sendResponse(array(), 'Answer submitted successfully');
+ }catch(\Exception $ex){
+    return $this->sendError('Server error',array($ex->getMessage()));
+}
+}
 
     /**
      * Display the specified resource.
@@ -143,221 +143,196 @@ class QuizAnswerController extends BaseController
         try{
             $answer = QuizAnswer::where('user_id', $request->user_id)->where('question_id', $request->question_id)->where('case_id', $request->case_id)->first();
             if(!empty($answer)){
-                 return $this->sendResponse($answer, 'Answer retrieved successfully.');
-            }else{
-                return $this->sendResponse($answer =array(), 'No Data Found.');
-            }
-           
-        }catch(\Exception $ex){
-            return $this->sendError('Server error', array($ex->getMessage()));
+             return $this->sendResponse($answer, 'Answer retrieved successfully.');
+         }else{
+            return $this->sendResponse($answer =array(), 'No Data Found.');
         }
-
+        
+    }catch(\Exception $ex){
+        return $this->sendError('Server error', array($ex->getMessage()));
     }
 
-    public function ProductRecommend(Request $request)
-    {
+}
+
+public function ProductRecommend(Request $request)
+{
         //dd($request->all());
        // try{
 
-        $user_id = $request['user_id'];
-        $case_id = $request['case_id'];
+    $user_id = $request['user_id'];
+    $case_id = $request['case_id'];
 
-            $answer_data = Answers::where('user_id', $user_id)->where('case_id', $case_id)->get();
+    $answer_data = Answers::where('user_id', $user_id)->where('case_id', $case_id)->get();
 
-           
-            $recommendation = json_decode($answer_data[0]['answer']);
-
-
-            $a1 = 0;
-            $a2 = 0;
-            $a3 = 0;
-            $b1 = 0;
-            $b2 = 0;
-            $c1 = 0;
-            $ts1 = 0;
-            $ts2 = 0;
-            $data = 'accutane';
-            foreach ($recommendation as $key => $value) {
-
-                if($value->recommendation_product == 'recommendation_1'){
-                    //dd($request->case_id);
-                    //$a1 = 0;
-                    //$answer = QuizAnswer::where('user_id', $request->user_id)->where('question_id', $value->id)->where('case_id', $request->case_id)->first();
-
-                     $answer = $value->answer;
-
-                    if(isset($answer->answer)){
-                        if($answer->answer == 'Mild– just a few pimples here and there. Mostly whiteheads and blackheads with a few inflamed bumps here and there.'){
-                            $a1 = 1;
-                        }else if($answer->answer == 'Moderate– multiple inflamed pimples with new ones popping up. Acne is inflamed and red.'){
-                            $a1 = 2;
-                        }else if($answer->answer == 'Moderately Severe– a large number of inflamed pimples with some deeper, more painful nodules that do not come to a head. Acne leaves moderate scarring.'){
-                            $a1 = 3;
-                        }else if($answer->answer == 'Severe– a large number of inflamed pimples with multiple deep, painful nodules that do not come to a head. Acne leaves significant scarring.'){
-                            $a1 = 4;
-                        }
-                    }
-                }
-                if($value->recommendation_product == 'recommendation_2'){
-                    //$a2 = 0;
-                    //$answer = QuizAnswer::where('user_id', $request->user_id)->where('question_id', $value->id)->where('case_id', $request->case_id)->first();
-                    $answer = $value->answer;
-                    if(isset($answer->answer)){
-                        if($answer->answer == 'It doesn’t really bother me'){
-                            $a2 = 1;
-                        }else if($answer->answer == 'I do notice that it’s there but it doesn’t prevent me from doing day to day things'){
-                            $a2 = 2;
-                        }else if($answer->answer == 'It bothers me and I do feel self-conscious about it sometimes when other people point it out'){
-                            $a2 = 3;
-                        }else if($answer->answer == 'It bothers me a lot.I sometimes skip out on things because of my acne and it affects my self-esteem.'){
-                            $a2 = 4;
-                        }else if($answer->answer == 'My acne is the primary thing that holds back my confidence and I feel like it holds me back from enjoying things as much as I want to.'){
-                            $a2 = 5;
-                        }
-                    }
-                }
-                if($value->recommendation_product == 'recommendation_3'){
-                    //$a3 = 0;
-                    //$answer = QuizAnswer::where('user_id', $request->user_id)->where('question_id', $value->id)->where('case_id', $request->case_id)->first();
-
-                    $answer = $value->answer;
-
-                    if(isset($answer->answer)){
-                        if($answer->answer == 'My acne does not scar'){
-                            $a3 = 1;
-                        }else if($answer->answer == 'I tend to get dark marks and pigmentation from my acne'){
-                            $a3 = 2;
-                        }else if($answer->answer == 'My skin does tend to scar and causes depressions and bumps after my acne heals'){
-                            $a3 = 3;
-                        }
-                    }
-                }
-                if($value->recommendation_product == 'recommendation_4'){
-                    //$b1 = 0;
-                   // $answer = QuizAnswer::where('user_id', $request->user_id)->where('question_id', $value->id)->where('case_id', $request->case_id)->first();
-
-                    $answer = $value->answer;
-
-                    if(isset($answer->answer)){
-                        if($answer->answer == 'Yes'){
-                            $b1 = 1;
-                        }
-                    }
-                }
-                if($value->recommendation_product == 'recommendation_5'){
-                    //$b2 = null;
-                    //$answer = QuizAnswer::where('user_id', $request->user_id)->where('question_id', $value->id)->where('case_id', $request->case_id)->first();
-
-                    $answer = $value->answer;
-
-                    if(isset($answer->answer)){
-                        if($answer->answer == 'Topical Retinoids (Differin Retin-A Tazorac etc.)'){
-                            $b2 = 0;
-                        }else if($answer->answer == 'Topical Antibiotics (Benzaclin Duac etc.)'){
-                            $b2 = 0;
-                        }else if($answer->answer == 'Combination Antibiotic/Retinoid cream'){
-                            $b2 = 0;
-                        }else if($answer->answer == 'Oral Antibiotics (Doxycycline Minocycline Azythromycin etc.)'){
-                            $b2 = 0;
-                        }else if($answer->answer == 'Oral Contraceptives (Yaz Ortho Tri-Cyclen)'){
-                            $b2 = 1;
-                        }else if($answer->answer == 'Azelaic Acid (Azelex Finacea)'){
-                            $b2 = 1;
-                        }else if($answer->answer == 'Dapsone (Aczone)'){
-                            $b2 = 1;
-                        }else if($answer->answer == 'Accutane (Isotretinoin)'){
-                            $b2 = 1;
-                        }else if($answer->answer == 'Other'){
-                            $b2 = 1;
-                        }
-                    }
-                }
-                if($value->recommendation_product == 'recommendation_6'){
-                    //$c1 = 0;
-                    //$answer = QuizAnswer::where('user_id', $request->user_id)->where('question_id', $value->id)->where('case_id', $request->case_id)->first();
-
-                    $answer = $value->answer;
-
-                    if(isset($answer->answer)){
-                        if($answer->answer == 'Yes'){
-                            $c1 = 1;
-                        }
-                    }
-                }
-                if($value->recommendation_product == 'recommendation_7'){
-                    //$ts1 = 0;
-                    //$answer = QuizAnswer::where('user_id', $request->user_id)->where('question_id', $value->id)->where('case_id', $request->case_id)->first();
-
-                    $answer = $value->answer;
-
-                    if(isset($answer->answer)){
-                        if($answer->answer == 'Very Dry'){
-                            $ts1 = 1;
-                        }else if($answer->answer == 'Dry'){
-                            $ts1 = 1;
-                        }else if($answer->answer == 'Combination'){
-                            $ts1 = 2;
-                        }else if($answer->answer == 'Oily'){
-                            $ts1 = 2;
-                        }else if($answer->answer == 'Very Oily'){
-                            $ts1 = 2;
-                        }
-                    }
-                }
-                if($value->recommendation_product == 'recommendation_8'){
-                    //$ts2 = 0;
-                    //$answer = QuizAnswer::where('user_id', $request->user_id)->where('question_id', $value->id)->where('case_id', $request->case_id)->first();
-
-                    $answer = $value->answer;
-                    
-                    if(isset($answer->answer)){
-                        if($answer->answer == 'Yes'){
-                            $ts2 = 1;
-                        }else if($answer->answer == 'No'){
-                            $ts2 = 2;
-                        }
-                    }
-                }
-                
-                
-             } 
-             $a = $a1+$a2+$a3;
-             $b = $b1+$b2;
-             $c = $c1;
-
-             
-            /* if($c == 1){
-                $data = 'Accutane';
-             }else if($a+$b>12){
-                $data = 'Accutane';
-             }else{
-                if($ts1 == 1){
-                    $data = 'Topical_low';
-                }else if($ts1 == 2){
-                    $data = 'Topical_high';
-                }
-             }*/
-
-              if($a+$b>=11){
-                $data = 'Accutane';
-             }else if($c == 1){
-                $data = 'Accutane';
-             }else if($a+$b<11 && $c == 1){
-                 $data = 'Accutane';
-             }else{
-                if($ts1 == 1){
-                    $data = 'Topical_low';
-                }else if($ts1 == 2){
-                    $data = 'Topical_high';
-                }
-             }
+    $recommendation = json_decode($answer_data[0]['answer']);
 
 
-            return $this->sendResponse($data, 'Product recommendation successfully.');   
+    $a1 = 0;
+    $a2 = 0;
+    $a3 = 0;
+    $b1 = 0;
+    $b2 = 0;
+    $c1 = 0;
+    $ts1 = 0;
+    $ts2 = 0;
+    $data = 'accutane';
+    foreach ($recommendation as $key => $value) {
+
+        if($value->recommendation_product == 'recommendation_1'){
+            
+         $answer = $value->answer;
+
+         if(isset($answer->answer)){
+            if($answer->answer == 'Mild– just a few pimples here and there. Mostly whiteheads and blackheads with a few inflamed bumps here and there.'){
+                $a1 = 1;
+            }else if($answer->answer == 'Moderate– multiple inflamed pimples with new ones popping up. Acne is inflamed and red.'){
+                $a1 = 2;
+            }else if($answer->answer == 'Moderately Severe– a large number of inflamed pimples with some deeper, more painful nodules that do not come to a head. Acne leaves moderate scarring.'){
+                $a1 = 3;
+            }else if($answer->answer == 'Severe– a large number of inflamed pimples with multiple deep, painful nodules that do not come to a head. Acne leaves significant scarring.'){
+                $a1 = 4;
+            }
+        }
+    }
+    if($value->recommendation_product == 'recommendation_2'){
+        
+        $answer = $value->answer;
+        if(isset($answer->answer)){
+            if($answer->answer == 'It doesn’t really bother me'){
+                $a2 = 1;
+            }else if($answer->answer == 'I do notice that it’s there but it doesn’t prevent me from doing day to day things'){
+                $a2 = 2;
+            }else if($answer->answer == 'It bothers me and I do feel self-conscious about it sometimes when other people point it out'){
+                $a2 = 3;
+            }else if($answer->answer == 'It bothers me a lot.I sometimes skip out on things because of my acne and it affects my self-esteem.'){
+                $a2 = 4;
+            }else if($answer->answer == 'My acne is the primary thing that holds back my confidence and I feel like it holds me back from enjoying things as much as I want to.'){
+                $a2 = 5;
+            }
+        }
+    }
+    if($value->recommendation_product == 'recommendation_3'){
+        
+
+        $answer = $value->answer;
+
+        if(isset($answer->answer)){
+            if($answer->answer == 'My acne does not scar'){
+                $a3 = 1;
+            }else if($answer->answer == 'I tend to get dark marks and pigmentation from my acne'){
+                $a3 = 2;
+            }else if($answer->answer == 'My skin does tend to scar and causes depressions and bumps after my acne heals'){
+                $a3 = 3;
+            }
+        }
+    }
+    if($value->recommendation_product == 'recommendation_4'){
+        
+
+        $answer = $value->answer;
+
+        if(isset($answer->answer)){
+            if($answer->answer == 'Yes'){
+                $b1 = 1;
+            }
+        }
+    }
+    if($value->recommendation_product == 'recommendation_5'){
+
+        $answer = $value->answer;
+
+        if(isset($answer->answer)){
+            if($answer->answer == 'Topical Retinoids (Differin Retin-A Tazorac etc.)'){
+                $b2 = 0;
+            }else if($answer->answer == 'Topical Antibiotics (Benzaclin Duac etc.)'){
+                $b2 = 0;
+            }else if($answer->answer == 'Combination Antibiotic/Retinoid cream'){
+                $b2 = 0;
+            }else if($answer->answer == 'Oral Antibiotics (Doxycycline Minocycline Azythromycin etc.)'){
+                $b2 = 0;
+            }else if($answer->answer == 'Oral Contraceptives (Yaz Ortho Tri-Cyclen)'){
+                $b2 = 1;
+            }else if($answer->answer == 'Azelaic Acid (Azelex Finacea)'){
+                $b2 = 1;
+            }else if($answer->answer == 'Dapsone (Aczone)'){
+                $b2 = 1;
+            }else if($answer->answer == 'Accutane (Isotretinoin)'){
+                $b2 = 1;
+            }else if($answer->answer == 'Other'){
+                $b2 = 1;
+            }
+        }
+    }
+    if($value->recommendation_product == 'recommendation_6'){
+
+        $answer = $value->answer;
+
+        if(isset($answer->answer)){
+            if($answer->answer == 'Yes'){
+                $c1 = 1;
+            }
+        }
+    }
+    if($value->recommendation_product == 'recommendation_7'){
+
+        $answer = $value->answer;
+
+        if(isset($answer->answer)){
+            if($answer->answer == 'Very Dry'){
+                $ts1 = 1;
+            }else if($answer->answer == 'Dry'){
+                $ts1 = 1;
+            }else if($answer->answer == 'Combination'){
+                $ts1 = 2;
+            }else if($answer->answer == 'Oily'){
+                $ts1 = 2;
+            }else if($answer->answer == 'Very Oily'){
+                $ts1 = 2;
+            }
+        }
+    }
+    if($value->recommendation_product == 'recommendation_8'){
+       
+        $answer = $value->answer;
+
+        if(isset($answer->answer)){
+            if($answer->answer == 'Yes'){
+                $ts2 = 1;
+            }else if($answer->answer == 'No'){
+                $ts2 = 2;
+            }
+        }
+    }
+    
+    
+} 
+$a = $a1+$a2+$a3;
+$b = $b1+$b2;
+$c = $c1;
+
+
+
+
+if($a+$b>=11){
+    $data = 'Accutane';
+}else if($c == 1){
+    $data = 'Accutane';
+}else if($a+$b<11 && $c == 1){
+ $data = 'Accutane';
+}else{
+    if($ts1 == 1){
+        $data = 'Topical_low';
+    }else if($ts1 == 2){
+        $data = 'Topical_high';
+    }
+}
+
+
+return $this->sendResponse($data, 'Product recommendation successfully.');   
            // }
            // catch(\Exception $ex){
           //  return $this->sendError('Server error', array($ex->getMessage()));
         // }
-    
-        }
+
+}
 }

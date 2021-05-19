0126acbbacb3bf08712d3df497958dde955195fb
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-19 16:42:21 +0530

    Update index.blade.php

--- resources/views/ordermanagement/index.blade.php
+++ resources/views/ordermanagement/index.blade.php
@@ -9,7 +9,7 @@
         <p>{{ $message }}</p>
     </div>
     @endif
-    
+
 
     <div class="content-wrapper">
         <div class="content-header row">
@@ -73,7 +73,7 @@
 
                                             <td>
                                                 <div class="d-flex">
-            <a class="icons edit-icon" href="{{route('ordermanagement.show',$order_data->case_id) }}">
+            <a class="icons edit-icon" href="{{ route('casemanagement.show',$order_data->case_id) }}">
                                                         <i class="fa fa-eye"></i>
                                                     </a>
                                                     

e97487e41543f7608f52f6361bc494a240f7897f
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-19 18:31:13 +0530

    Update view.blade.php

--- resources/views/ordermanagement/view.blade.php
+++ resources/views/ordermanagement/view.blade.php
@@ -350,51 +350,7 @@
  @php
  Session::forget('que_current_tab');
  @endphp
- <div id="questionaire" class="tab-pane fade in">					    
- 	{{-- 	@foreach($que as $key => $loopdata)	 --}}
- 	<div class="row" style="padding: 10px;">
- 		<div class="col-md-12">
- 			<section class="card" >
- 				<ul class="nav nav-tabs" id="question-tab-menu">
-
- 					@foreach($category as $key => $data)
- 					{{-- <li><a class="btn @if($activeTab == 0) active @elseif($current_tab_id == 'home'.$key) active @endif" data-toggle="tab" href="#home{{$key}}">{{$data}}</a></li> --}}
-
- 					<li><a class="btn" data-toggle="tab" href="#home{{$key}}">{{$data}}</a></li>
- 					<?php //$activeTab++ ?> 
- 					@endforeach
- 				</ul>
- 				<div class="tab-content">
- 					<?php $i=0 ?>
- 					@foreach($category as $key => $data)
- 					<div id="home{{$key}}" class="tab-pane fade in @if($i== 0) active show @endif">					    
- 						@foreach($quiz as $key1 => $que)
- 						<div class="row" style="padding: 10px;">
- 							<div class="col-md-12">
- 								<?php 
- 								if($que['category_id']== $key){
-
- 									echo "<h4><strong>Que:".$que['question']."</strong></h4>";
- 									echo "<p>Ans:".$que['answer']."</p>";
- 								}
- 								?>
-
- 							</div>
- 						</div>
- 						<?php $i++ ?>
- 						@endforeach
- 					</div> 
- 					@endforeach 
- 					
-
- 				</div>
- 			</section>
- 		</div>
- 	</div>
- </div>  
-
- {{-- @endforeach --}}
-
+ 
  <!--end code tab2-->
 
  <!--tab3-->

35f80c6e6b771ef95fc701ccc0b03eae68dbf29a
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-19 18:09:42 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -47,7 +47,8 @@
 					<div class="breadcrumb-wrapper col-12 d-flex">
 						<ol class="breadcrumb">
 							<li class="breadcrumb-item"><a href="{{url('/admin/dashboard')}}">Dashboard</a></li>
-							<li class="breadcrumb-item active">All</li>
+							<li class="breadcrumb-item active">List</li>
+							<li class="breadcrumb-item active"><a href="{{route('casemanagement.index')}}">Case Management List</a></li>
 						</ol>
 					</div>
 				</div>

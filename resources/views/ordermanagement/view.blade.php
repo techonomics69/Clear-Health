d0bd617a0fe98ea89af74c3fcd58b8d0f150f3e8
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-20 11:04:27 +0530

    Update view.blade.php

--- resources/views/ordermanagement/view.blade.php
+++ resources/views/ordermanagement/view.blade.php
@@ -24,7 +24,7 @@
 	$activeTab = 1;
 	$active = 1 ;
 
-   // unset($products[$key]);
+	// unset($products[$key]);
 	@endphp
 	@else
 	@php
@@ -61,85 +61,82 @@
 						</div>
 					</div> --> 
 				</div>
-				<?php if("medication_type" == 1) { ?>
-				<div class="row">
-					<div class="col-lg-12">
-						<section class="card" >
-							<ul class="nav nav-tabs" id="casemanagement-tab-menu">
-								<li><a class="btn active " data-toggle="tab" href="#profile">Profile</a></li>
-								<li><a class="btn" data-toggle="tab" href="#order_summary">Order Summary</a></li>
-								<li><a class="btn" data-toggle="tab" href="#shipments_shipping_details">Shipments & shipping details</a></li>
-							</ul>
-</section>
-</div>
-</div>
+				<?php if("medication_type" == 2) { ?>
+					<div class="row">
+						<div class="col-lg-12">
+							<section class="card" >
+								<ul class="nav nav-tabs" id="casemanagement-tab-menu">
+									<li><a class="btn active " data-toggle="tab" href="#profile">Profile</a></li>
+									<li><a class="btn" data-toggle="tab" href="#order_summary">Order Summary</a></li>
+									<li><a class="btn" data-toggle="tab" href="#shipments_shipping_details">Shipments & shipping details</a></li>
+								</ul>
+							</section>
+						</div>
+					</div>
 
-<?php } else { ?> 
+				<?php } else { ?> 
 
-<div class="row">
-					<div class="col-lg-12">
-						<section class="card" >
-							<ul class="nav nav-tabs" id="casemanagement-tab-menu">
+					<div class="row">
+						<div class="col-lg-12">
+							<section class="card" >
+								<ul class="nav nav-tabs" id="casemanagement-tab-menu">
+									<li><a class="btn active " data-toggle="tab" href="#profile">Profile</a></li>
+									<li><a class="btn" data-toggle="tab" href="#questions">Questions</a></li>
+										<li><a class="btn" data-toggle="tab" href="#skincare_summary">Skincare Summary </a></li>
+											<li><a class="btn" data-toggle="tab" href="#action_items">Action Items </a></li>
+												<li><a class="btn" data-toggle="tab" href="#messages">Messages</a></li>
+												<li><a class="btn" data-toggle="tab" href="#photos">Photos </a></li>
+												<li><a class="btn" data-toggle="tab" href="#payments">Payments </a></li>
+												<li><a class="btn" data-toggle="tab" href="#shipments_shipping_details">Shipments & shipping details</a></li>
+											</ul>
+										</section>
+									</div>
+								</div>
+							<?php } ?>
+						</div>
+					</div>
+				
+					@endsection
 
+					@section('scriptsection')
 
+					<script>
+						$.noConflict();
+						jQuery( document ).ready(function( $ ) {
+							$('.c_profile').DataTable({
+								"dom": '<"top"if>rt<"bottom"lp><"clear">',
+								"oSearch": { "bSmart": false, "bRegex": true },
+								"scrollX": true,
+							});
+						});
 
-								<li><a class="btn active " data-toggle="tab" href="#profile">Profile</a></li>
-								<li><a class="btn" data-toggle="tab" href="#questions">Questions<a></li>
-									<li><a class="btn" data-toggle="tab" href="#skincare_summary">Skincare Summary <a></li>
-								<li><a class="btn" data-toggle="tab" href="#action_items">Action Items <a></li>
-								<li><a class="btn" data-toggle="tab" href="#messages">Messages</a></li>
-								<li><a class="btn" data-toggle="tab" href="#photos">Photos </a></li>
-								<li><a class="btn" data-toggle="tab" href="#payments">Payments </a></li>
-								<li><a class="btn" data-toggle="tab" href="#shipments_shipping_details">Shipments & shipping details</a></li>
-							</ul>
-</section>
-</div>
-</div>
-
-<?php } ?>
-</div>
-</div>
-@endsection
-
-@section('scriptsection')
-
-<script>
-	$.noConflict();
-	jQuery( document ).ready(function( $ ) {
-		$('.c_profile').DataTable({
-			"dom": '<"top"if>rt<"bottom"lp><"clear">',
-			"oSearch": { "bSmart": false, "bRegex": true },
-			"scrollX": true,
-		});
-	});
-
-	$(document).on('click', '#i_pledge_agreement', function() {
-    $('#i_pledge_agreement_form').submit();
-  });
-</script>
+						$(document).on('click', '#i_pledge_agreement', function() {
+							$('#i_pledge_agreement_form').submit();
+						});
+					</script>
 
 
-@if (count($errors) > 0)
-<script type="text/javascript">
-	$('html, body').animate({
-		scrollTop: $("#testreoprtdiv").offset().top
-	}, 2000);
-</script>
-@endif
+					@if (count($errors) > 0)
+					<script type="text/javascript">
+						$('html, body').animate({
+							scrollTop: $("#testreoprtdiv").offset().top
+						}, 2000);
+					</script>
+					@endif
 
-@endsection
-<style>
-	.tab-content h4{
-			font-size:16px;
-		}
-		.tab-content p{
-			font-size:16px;
-			margin-bottom:0;
-		}
-		.inner-section {
-				width: 100%;
-		
-			}
-	</style>
+					@endsection
+					<style>
+						.tab-content h4{
+							font-size:16px;
+						}
+						.tab-content p{
+							font-size:16px;
+							margin-bottom:0;
+						}
+						.inner-section {
+							width: 100%;
+
+						}
+					</style>
 
 

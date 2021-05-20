258a69b7378e81a9518917c6e628b3fb19f77cd9
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-20 12:06:47 +0530

    Update QuizAnswerController.php

--- app/Http/Controllers/API/QuizAnswerController.php
+++ app/Http/Controllers/API/QuizAnswerController.php
@@ -310,19 +310,6 @@ $a = $a1+$a2+$a3;
 $b = $b1+$b2;
 $c = $c1;
 
-echo "<pre>";
-print_r($a);
-echo "<pre>";
-
-
-echo "<pre>";
-print_r($b);
-echo "<pre>";
-
-
-echo "<pre>";
-print_r($c);
-echo "<pre>";
 
 
 

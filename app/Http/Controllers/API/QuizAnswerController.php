7adab0a78ec85e6517022eecf4e8e02718d2f10a
Deval Fullstackdeveloper <devalfullstackdeveloper@gmail.com>
2021-05-20 17:17:34 +0530

    product re

--- app/Http/Controllers/API/QuizAnswerController.php
+++ app/Http/Controllers/API/QuizAnswerController.php
@@ -323,7 +323,7 @@ if($a+$b>=11){
     }
 }
 
-$data = "a=>".$a." b=>".$b." c=>".$c." a1=>".$a1." a2=>".$a2." a3=>".$a3." b1=>".$b1." b2=>".json_encode($b2);
+$data = "a=>".$a." b=>".$b." c=>".$c." a1=>".$a1." a2=>".$a2." a3=>".$a3." b1=>".$b1." b2=>".json_encode(count($b2));
 return $this->sendResponse($data, 'Product recommendation successfully.');   
            // }
            // catch(\Exception $ex){

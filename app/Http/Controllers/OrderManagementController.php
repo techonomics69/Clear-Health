23b41951767905b7f66993d831f7a9eb39a67383
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-20 09:32:56 +0530

    Update OrderManagementController.php

--- app/Http/Controllers/OrderManagementController.php
+++ app/Http/Controllers/OrderManagementController.php
@@ -71,11 +71,11 @@ public function store(Request $request)
 public function show($id)
 {
    $user_case_management_data = CaseManagement::join('users','case_managements.user_id', '=', 'users.id')->select('case_managements.*','users.first_name','users.last_name','users.email')->where('case_managements.id',$id)->first();
-   $category = QuizCategory::pluck('name', 'id')->toArray();
+   /*$category = QuizCategory::pluck('name', 'id')->toArray();*/
         //foreach ($user_case_management_data as $key => $value) {
-   $quiz= QuizAnswer::join('quizzes','quiz_answers.question_id', '=', 'quizzes.id')->select('quiz_answers.*','quizzes.question','quizzes.category_id')->where('case_id', $user_case_management_data['id'])->OrderBy('id', 'ASC')->get();
+   /*$quiz= QuizAnswer::join('quizzes','quiz_answers.question_id', '=', 'quizzes.id')->select('quiz_answers.*','quizzes.question','quizzes.category_id')->where('case_id', $user_case_management_data['id'])->OrderBy('id', 'ASC')->get();*/
 
-   return view('ordermanagement.view',compact('user_case_management_data','category','quiz'));
+   return view('ordermanagement.view',compact('user_case_management_data'));
 }
 
 public function edit($id)

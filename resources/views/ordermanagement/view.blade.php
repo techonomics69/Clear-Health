fb4f065cf9a7e600347dbc2626ea05bd8c20c06f
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-20 16:45:27 +0530

    Update view.blade.php

--- resources/views/ordermanagement/view.blade.php
+++ resources/views/ordermanagement/view.blade.php
@@ -41,7 +41,7 @@
 					</div> --> 
 				</div>
 				
-				<?php if($order_data['medication_type'] == 2) { ?>
+				<?php if($order_non_prescribed['medication_type'] == 2) { ?>
 					<div class="row">
 						<div class="col-lg-12">
 							<section class="card" >
@@ -62,27 +62,27 @@
 															<!-- @foreach ($order_non_prescribed as $key => $order_data) -->
 															<div class="col-md-6  form-group">
 																<strong>FirstName:</strong>
-																{{$order_data->first_name}}
+																{{ $order_non_prescribed['first_name']}}
 															</div>
 
 															<div class="col-md-6 form-group">
 																<strong>LastName: </strong>
-																{{$order_data->last_name}}
+																{{ $order_non_prescribed['last_name']}}
 															</div>
 
 															<div class="col-md-6 form-group">
 																<strong>Email: </strong>
-																{{$order_data->email}}
+																{{ $order_non_prescribed['email']}}
 															</div>
 
 															<div class="col-md-6 form-group">
 																<strong>Email: </strong>
-																{{$order_data->mobile}}
+																{{ $order_non_prescribed['mobile']}}
 															</div>
 
 															<div class="col-md-6 form-group">
 																<strong>Email: </strong>
-																{{$order_data->address}}
+																{{ $order_non_prescribed['address']}}
 															</div>
 															
 														</div>

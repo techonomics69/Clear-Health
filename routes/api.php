ee59939e12a711baf0382e2f1b82b0f633da1a54
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-21 19:34:25 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -77,7 +77,7 @@ Route::post('forgot_password',[RegisterController::class, 'forgot_password']);
     Route::resource('caseManagement', CaseManagementController::class);
     Route::post('createOrder', [CheckoutController::class, 'store']);
     Route::post('addCheckoutAddress', [CheckoutController::class, 'addCheckoutAddress']);
-    Route::post('getCheckoutdetail', [CheckoutController::class, 'getCheckoutdetail']);
+    Route::post('getOrderDetail', [CheckoutController::class, 'getCheckoutdetail']);
     Route::post('getCheckoutAddress', [CheckoutController::class, 'getCheckoutAddress']);
     Route::get('getUpsellProductDetails', [ProductController::class, 'getUpsellProductDetails']);
     Route::get('getskincareplan', [ProductController::class, 'getskincareplan']);

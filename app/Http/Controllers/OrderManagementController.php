07add90b3e201fdcad77dafb6c53a42eba8dfda3
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-21 16:36:32 +0530

    Update OrderManagementController.php

--- app/Http/Controllers/OrderManagementController.php
+++ app/Http/Controllers/OrderManagementController.php
@@ -70,7 +70,7 @@ class OrderManagementController extends Controller
      ->join('carts','carts.id', '=', 'checkout.cart_id')
      ->join('checkout_address','checkout_address.id', '=', 'checkout.id')
      /* ->join('products', 'products.id', '=', 'carts.product_id')*/
-     ->select('checkout.*','users.email','checkout.case_id','checkout.created_at','checkout.order_id','checkout.medication_type','checkout.id','checkout.cart_id','carts.product_price','users.first_name','users.last_name','users.email','users.mobile','checkout_address.addressline1','checkout_address.addressline2','checkout_address.city','checkout_address.state','checkout_address.zipcode','carts.quantity')
+     ->select('checkout.*','users.email','checkout.case_id','checkout.created_at','checkout.order_id','checkout.medication_type','checkout.id','checkout.cart_id','carts.product_price','users.first_name','users.last_name','users.email','users.mobile','checkout_address.addressline1','checkout_address.addressline2','checkout_address.city','checkout_address.state','checkout_address.zipcode','carts.quantity')->orderBy('checkout.id', 'DESC')
      //'products.name AS product_name',
      //->where('checkout.id',$id)
      ->get();

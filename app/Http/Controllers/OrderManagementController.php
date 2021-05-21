04fedbcf8aa00503a84fd114cfcae7fc7b131e26
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-21 16:07:35 +0530

    Update OrderManagementController.php

--- app/Http/Controllers/OrderManagementController.php
+++ app/Http/Controllers/OrderManagementController.php
@@ -81,7 +81,7 @@ class OrderManagementController extends Controller
         $product_details  = Cart::join('products', 'products.id', '=', 'carts.product_id')->whereIn('carts.id', $cart_ids)->select('products.name AS product_name')->get();
 
         foreach($product_details as $product_key=>$product_value){
-           $product_name[] = $product_key['product_name'];  
+           $product_name[] = $product_value['product_name'];  
        }
        $order_non_prescribed[$key]->product_name = implode(',',$product_name);
            

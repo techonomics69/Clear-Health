dac50aef09e4d2d8dc01bbf95a9983cbfc41178d
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-21 17:13:14 +0530

    Update OrderManagementController.php

--- app/Http/Controllers/OrderManagementController.php
+++ app/Http/Controllers/OrderManagementController.php
@@ -45,71 +45,71 @@ class OrderManagementController extends Controller
             $product_details  = Cart::join('products', 'products.id', '=', 'carts.product_id')->whereIn('carts.id', $cart_ids)->select('products.name AS product_name')->get();
 
             foreach( $product_details as $k=>$v){
-             $product_name[] = $v['product_name'];  
-         }
-         $order[$key]->product_name = implode(',',$product_name);
-     }
+               $product_name[] = $v['product_name'];  
+           }
+           $order[$key]->product_name = implode(',',$product_name);
+       }
 
-     return view('ordermanagement.index',compact('order'));
- }
+       return view('ordermanagement.index',compact('order'));
+   }
 
- public function create()
- {
+   public function create()
+   {
 
- }
+   }
 
- public function store(Request $request)
- {   
+   public function store(Request $request)
+   {   
 
- }
+   }
 
- public function show($id)
- {
+   public function show($id)
+   {
 
-     $order_non_prescribed =  checkout::join('users', 'users.id', '=', 'checkout.user_id')
-     ->join('carts','carts.id', '=', 'checkout.cart_id')
-     ->join('checkout_address','checkout_address.id', '=', 'checkout.id')
-     /* ->join('products', 'products.id', '=', 'carts.product_id')*/
-     ->select('checkout.*','users.email','checkout.case_id','checkout.created_at','checkout.order_id','checkout.medication_type','checkout.id','checkout.cart_id','carts.product_price','users.first_name','users.last_name','users.email','users.mobile','checkout_address.addressline1','checkout_address.addressline2','checkout_address.city','checkout_address.state','checkout_address.zipcode','carts.quantity')->orderBy('checkout.id', 'DESC')
+       $order_non_prescribed =  checkout::join('users', 'users.id', '=', 'checkout.user_id')
+       ->join('carts','carts.id', '=', 'checkout.cart_id')
+       ->join('checkout_address','checkout_address.id', '=', 'checkout.id')
+       /* ->join('products', 'products.id', '=', 'carts.product_id')*/
+       ->select('checkout.*','users.email','checkout.case_id','checkout.created_at','checkout.order_id','checkout.medication_type','checkout.id','checkout.cart_id','carts.product_price','users.first_name','users.last_name','users.email','users.mobile','checkout_address.addressline1','checkout_address.addressline2','checkout_address.city','checkout_address.state','checkout_address.zipcode','carts.quantity')->orderBy('checkout.id', 'DESC')
      //'products.name AS product_name',
      //->where('checkout.id',$id)
-     ->get();
+       ->get();
 
 
-     foreach($order_non_prescribed as $key=>$val)
-     {
+       foreach($order_non_prescribed as $key=>$val)
+       {
         $cart_ids = explode(',', $val['cart_id']);
         $product_name = array();
-        $product_details  = Cart::join('products', 'products.id', '=', 'carts.product_id')->whereIn('carts.id', $cart_ids)->select('products.name AS product_name')->get();
+        $product_details  = Cart::join('products', 'products.id', '=', 'carts.product_id')->where('carts.id', $cart_ids)->select('products.name AS product_name')->get();
 
         foreach($product_details as $product_key=>$product_value){
-         $product_name[] = $product_value['product_name'];  
-     }
+           $product_name[] = $product_value['product_name'];  
+       }
 
      /*if(isset($val['medication_type']))
      {
         $order_non_prescribed[$key]['medication_type'] = $val['medication_type'];
      }else{
         $order_non_prescribed[$key]['medication_type']= '';
-     }*/
-     
-     $order_non_prescribed[$key]->product_name = implode(',',$product_name);    
- }
+    }*/
 
- $user_case_management_data = CaseManagement::join('users','case_managements.user_id', '=', 'users.id')
- ->select('case_managements.*','users.first_name','users.last_name','users.email','users.mobile','users.address')
- ->where('case_managements.id',$id)
- ->first();
+    $order_non_prescribed[$key]->product_name = implode(',',$product_name);    
+}
 
- $category = QuizCategory::pluck('name', 'id')->toArray();
+$user_case_management_data = CaseManagement::join('users','case_managements.user_id', '=', 'users.id')
+->select('case_managements.*','users.first_name','users.last_name','users.email','users.mobile','users.address')
+->where('case_managements.id',$id)
+->first();
 
- $quiz= QuizAnswer::join('quizzes','quiz_answers.question_id', '=', 'quizzes.id')
- ->select('quiz_answers.*','quizzes.question','quizzes.category_id')
- ->where('case_id', $user_case_management_data['id'])
- ->OrderBy('id', 'ASC')
- ->get();
+$category = QuizCategory::pluck('name', 'id')->toArray();
 
- return view('ordermanagement.view',compact('order_non_prescribed','user_case_management_data','category','quiz'));
+$quiz= QuizAnswer::join('quizzes','quiz_answers.question_id', '=', 'quizzes.id')
+->select('quiz_answers.*','quizzes.question','quizzes.category_id')
+->where('case_id', $user_case_management_data['id'])
+->OrderBy('id', 'ASC')
+->get();
+
+return view('ordermanagement.view',compact('order_non_prescribed','user_case_management_data','category','quiz'));
 }
 
 public function edit($id)

84f2d6529b78d2b66ab2a0cb729758fc7b3907cb
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-21 15:28:04 +0530

    Update OrderManagementController.php

--- app/Http/Controllers/OrderManagementController.php
+++ app/Http/Controllers/OrderManagementController.php
@@ -78,7 +78,7 @@ class OrderManagementController extends Controller
         {
             $cart_ids = explode(',', $val['cart_id']);
             $product_name = array();
-            $product_details  = Cart::join('products', 'products.id', '=', 'carts.product_id')->whereIn('carts.id', $cart_ids)->select('products.name AS product_name')->get()->toArray();
+            $product_details  = Cart::join('products', 'products.id', '=', 'carts.product_id')->whereIn('carts.id', $cart_ids)->select('products.name AS product_name')->get();
 
             foreach($product_details as $product_key=>$product_value){
              $product_name[] = $product_key['product_name'];  

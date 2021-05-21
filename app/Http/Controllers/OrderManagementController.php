4376adc2ee5e64e59337612f170e4c9a1d67ba60
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-21 16:19:27 +0530

    Update OrderManagementController.php

--- app/Http/Controllers/OrderManagementController.php
+++ app/Http/Controllers/OrderManagementController.php
@@ -76,12 +76,20 @@ class OrderManagementController extends Controller
      ->get();
      foreach($order_non_prescribed as $key=>$val)
      {
+
         $cart_ids = explode(',', $val['cart_id']);
         $product_name = array();
         $product_details  = Cart::join('products', 'products.id', '=', 'carts.product_id')->whereIn('carts.id', $cart_ids)->select('products.name AS product_name')->get();
 
         foreach($product_details as $product_key=>$product_value){
          $product_name[] = $product_value['product_name'];  
+     }
+
+     if(isset($val['medication_type']))
+     {
+        $order_non_prescribed[$key]['medication_type'] = $val['medication_type'];
+     }else{
+        $order_non_prescribed[$key]['medication_type']= '';
      }
      $order_non_prescribed[$key]->product_name = implode(',',$product_name);    
  }

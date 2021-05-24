cbe33b97f256412ee4f3c20ce9d7de2358716a2a
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-24 15:52:23 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -24,7 +24,7 @@
 	$activeTab = 1;
 	$active = 1 ;
 
-   // unset($products[$key]);
+	// unset($products[$key]);
 	@endphp
 	@else
 	@php
@@ -53,171 +53,163 @@
 					</div>
 				</div>
 			</div>
-				{{-- <div class="content-header-right col-md-6 col-12 mb-2">
-						<div class="pull-right">
-						@can('quiz-create')					
-							<a class="btn btn-secondry" href="{{ route('ipledgeimports.create') }}"><i class="fa fa-plus" aria-hidden="true"></i> Case Management </a>
-						@endcan	
-						</div>
-					</div> --}}
+			{{-- <div class="content-header-right col-md-6 col-12 mb-2">
+				<div class="pull-right">
+					@can('quiz-create')					
+					<a class="btn btn-secondry" href="{{ route('ipledgeimports.create') }}"><i class="fa fa-plus" aria-hidden="true"></i> Case Management </a>
+					@endcan	
 				</div>
-				<div class="row">
-					<div class="col-lg-12">
-						<section class="card" >
-							<ul class="nav nav-tabs" id="casemanagement-tab-menu">
-									<li><a class="btn active " data-toggle="tab" href="#profile">Profile</a></li>
-									<li><a class="btn" data-toggle="tab" href="#questions">Questions</a></li>
-									<li><a class="btn" data-toggle="tab" href="#skincare_summary">Skincare Summary </a></li>
-									<li><a class="btn" data-toggle="tab" href="#action_items">Action Items </a></li>
-									<li><a class="btn" data-toggle="tab" href="#messages">Messages</a></li>
-									<li><a class="btn" data-toggle="tab" href="#photos">Photos </a></li>
-									<li><a class="btn" data-toggle="tab" href="#payments">Payments </a></li>
-								</ul>
-							<div class="tab-content">
+			</div> --}}
+		</div>
+		<div class="row">
+			<div class="col-lg-12">
+				<section class="card" >
+					<ul class="nav nav-tabs" id="casemanagement-tab-menu">
+						<li><a class="btn active " data-toggle="tab" href="#profile">Profile</a></li>
+						<li><a class="btn" data-toggle="tab" href="#questions">Questions</a></li>
+						<li><a class="btn" data-toggle="tab" href="#skincare_summary">Skincare Summary </a></li>
+						<li><a class="btn" data-toggle="tab" href="#action_items">Action Items </a></li>
+						<li><a class="btn" data-toggle="tab" href="#messages">Messages</a></li>
+						<li><a class="btn" data-toggle="tab" href="#photos">Photos </a></li>
+						<li><a class="btn" data-toggle="tab" href="#payments">Payments </a></li>
+					</ul>
+					<div class="tab-content">
+						<!--start 1st tab-->
+						<div id="profile" class="tab-pane fade in active show">					    
+							<div class="row" style="padding: 20px;">
+								<div class="col-md-12">
+									<section class="card">
+										<div class="card-body">
+											<div class="box-block mtb32">
+												<h3 class="font-weight-bold"><span class="text-underline">Basic Information</span></h3>
+												<div class="col-md-6  form-group">
 
-								<div id="profile" class="tab-pane fade in active show">					    
+													<strong>FirstName:</strong>
+													{{$user_case_management_data['first_name']}}
 
-									<div class="row" style="padding: 20px;">
-										<div class="col-md-12">
-											<section class="card">
+												</div>
 
-												<div class="card-body">
+												<div class="col-md-6 form-group">
+													<strong>LastName: </strong>
+													{{$user_case_management_data['last_name']}}
 
-												<div class="box-block mtb32">
-													<h3 class="font-weight-bold"><span class="text-underline">Basic Information</span></h3>
-													<div class="col-md-6  form-group">
-														
-														<strong>FirstName:</strong>
-														{{$user_case_management_data['first_name']}}
-														
-													</div>
+												</div>
 
-													<div class="col-md-6 form-group">
-														<strong>LastName: </strong>
-														{{$user_case_management_data['last_name']}}
-														
-													</div>
+												<div class="col-md-6 form-group">
+													<strong>Email: </strong>
+													{{$user_case_management_data['email']}}
 
-													<div class="col-md-6 form-group">
-														<strong>Email: </strong>
-														{{$user_case_management_data['email']}}
-														
-													</div>
-													<div class="col-md-6 form-group">
-														<strong>Mobile No: </strong>
-														{{$user_case_management_data['email']}}
-							
-													</div>
-													<div class="col-md-6 form-group">
-														<strong>Gender : </strong>
-														{{$user_case_management_data['email']}}
-							
-													</div>
-													<div class="col-md-6 form-group">
-														<strong>Physician Details : </strong>
-														{{$user_case_management_data['email']}}
-							
-													</div>
+												</div>
+												<div class="col-md-6 form-group">
+													<strong>Mobile No: </strong>
+													{{$user_case_management_data['email']}}
+
+												</div>
+												<div class="col-md-6 form-group">
+													<strong>Gender : </strong>
+													{{$user_case_management_data['email']}}
+
+												</div>
+												<div class="col-md-6 form-group">
+													<strong>Physician Details : </strong>
+													{{$user_case_management_data['email']}}
+
+												</div>
+											</div>
+										</div>
+									</section>
+								</div>
+							</div>
+						</div>  
+<!--End 1st tab-->
+
+						<!--start 2nd tab-->
+
+
+						{{-- @if(session()->has('que_current_tab'))
+						@php
+						$current_tab_id = 'home'.session()->get('que_current_tab') ;
+						$activeTab = 1;
+						$active = 1 ;
+
+						// unset($products[$key]);
+						@endphp
+						@else
+						@php
+						$current_tab_id = "";
+						$activeTab = 0 ;
+						$active = 0 ;	
+						@endphp
+						@endif --}}
+
+						@php
+						Session::forget('que_current_tab');
+						@endphp
+						<div id="questions" class="tab-pane fade in">					    
+							{{-- 	@foreach($que as $key => $loopdata)	 --}}
+							<div class="row" style="padding: 10px;">
+								<div class="col-md-12">
+									<section class="card" >
+										<ul class="nav nav-tabs" id="question-tab-menu">
+
+											@foreach($category as $key => $data)
+											{{-- <li><a class="btn @if($activeTab == 0) active @elseif($current_tab_id == 'home'.$key) active @endif" data-toggle="tab" href="#home{{$key}}">{{$data}}</a></li> --}}
+
+											<li><a class="btn" data-toggle="tab" href="#home{{$key}}">{{$data}}</a></li>
+											<?php //$activeTab++ ?> 
+											@endforeach
+										</ul>
+										<div class="tab-content">
+											<?php $i=0 ?>
+											@foreach($category as $key => $data)
+											<div id="home{{$key}}" class="tab-pane fade in @if($i== 0) active show @endif">					    
+												@foreach($quiz as $key1 => $que)
+												<div class="row" style="padding: 10px;">
+													<div class="col-md-12">
+														<?php 
+														if($que['category_id']== $key){
+
+															echo "<h4><strong>Que:".$que['question']."</strong></h4>";
+															echo "<p>Ans:".$que['answer']."</p>";
+														}
+														?>
+
 													</div>
 												</div>
-											</section>
-											
+												<?php $i++ ?>
+												@endforeach
+											</div> 
+											@endforeach 
+
+
 										</div>
-									</div>
-								</div>  
+									</section>
+								</div>
+							</div>
+						</div>  
 
+						{{-- @endforeach --}}
 
-								<!--start code tab2-->
+						<!--end 2nd tab-->
 
-
-{{-- @if(session()->has('que_current_tab'))
- @php
-    $current_tab_id = 'home'.session()->get('que_current_tab') ;
-    $activeTab = 1;
-    $active = 1 ;
-   
-   // unset($products[$key]);
-    @endphp
- @else
- @php
- $current_tab_id = "";
- $activeTab = 0 ;
- $active = 0 ;	
- @endphp
- @endif --}}
-
- @php
- Session::forget('que_current_tab');
- @endphp
- <div id="questions" class="tab-pane fade in">					    
- 	{{-- 	@foreach($que as $key => $loopdata)	 --}}
- 	<div class="row" style="padding: 10px;">
- 		<div class="col-md-12">
- 			<section class="card" >
- 				<ul class="nav nav-tabs" id="question-tab-menu">
-
- 					@foreach($category as $key => $data)
- 					{{-- <li><a class="btn @if($activeTab == 0) active @elseif($current_tab_id == 'home'.$key) active @endif" data-toggle="tab" href="#home{{$key}}">{{$data}}</a></li> --}}
-
- 					<li><a class="btn" data-toggle="tab" href="#home{{$key}}">{{$data}}</a></li>
- 					<?php //$activeTab++ ?> 
- 					@endforeach
- 				</ul>
- 				<div class="tab-content">
- 					<?php $i=0 ?>
- 					@foreach($category as $key => $data)
- 					<div id="home{{$key}}" class="tab-pane fade in @if($i== 0) active show @endif">					    
- 						@foreach($quiz as $key1 => $que)
- 						<div class="row" style="padding: 10px;">
- 							<div class="col-md-12">
- 								<?php 
- 								if($que['category_id']== $key){
-
- 									echo "<h4><strong>Que:".$que['question']."</strong></h4>";
- 									echo "<p>Ans:".$que['answer']."</p>";
- 								}
- 								?>
-
- 							</div>
- 						</div>
- 						<?php $i++ ?>
- 						@endforeach
- 					</div> 
- 					@endforeach 
- 					
-
- 				</div>
- 			</section>
- 		</div>
- 	</div>
- </div>  
-
- {{-- @endforeach --}}
-
- <!--end code tab2-->
-
- <!--tab3-->
- <div id="skincare_summary" class="tab-pane fade in">
- 	<div class="row" style="padding: 20px;">
-										<div class="col-md-12">
-											<section class="card">
-
-												<div class="card-body">
-
-												<div class="box-block mtb32">
-													<h3 class="font-weight-bold"><span class="text-underline">Skincare Summary</span></h3>
-													</div>
-												</div>
-											</section>
-											
+						<!--start 3rd tab-->
+						<div id="skincare_summary" class="tab-pane fade in">
+							<div class="row" style="padding: 20px;">
+								<div class="col-md-12">
+									<section class="card">
+										<div class="card-body">
+											<div class="box-block mtb32">
+												<h3 class="font-weight-bold"><span class="text-underline">Skincare Summary</span></h3>
+											</div>
 										</div>
-									</div>
+									</section>
+
+								</div>
+							</div>
 						</div> 
+						<!--end 3rd tab-->
 
-
-						<!--end of tab3-->
-
-						<!--tab4-->
+						<!--start 4th tab-->
 						<div id="action_items" class="tab-pane fade in ">
 							<div class="row" style="padding: 20px;">
 								<div class="col-md-12">
@@ -225,43 +217,41 @@
 								</div>
 							</div>
 						</div> 
+						<!--end 4th tab-->
 
-
-						<!--end of tab4-->
-						<!--tab5-->
+						<!--start 5th tab-->
 						<div id="messages" class="tab-pane fade in ">
 							<div class="row" style="padding: 20px;">
 								<div class="col-md-12">
 
 									messages goes here
-								{{-- <table class="table table-responsive-md table-striped table-bordered ipledgeList" style="width:100%">
-									<thead>
-										<tr>
-											<th width="60px">Files</th>
-											<th width="60px">Imported By</th>
-											<th width="200px">Imported date</th>
-										</tr>
-									</thead>
-									<tbody>					
-										@foreach($ipledgehistory_data as $key1 => $data1)
-										<tr>
-											<td><a class="text-dark" href="{{route('IpledgefileDownload',$data1['id'])}}" target="_blank"><i class="fa fa-download" aria-hidden="true"></i>  {{$data1['files']}}</a></td>
-											<td>{{$data1['imported_by']}}</td>
-											<td>{{$data1['crated_at']}}</td>
-										</tr>
-										@endforeach
-									</tbody>
-								</table> --}}
+									{{-- <table class="table table-responsive-md table-striped table-bordered ipledgeList" style="width:100%">
+										<thead>
+											<tr>
+												<th width="60px">Files</th>
+												<th width="60px">Imported By</th>
+												<th width="200px">Imported date</th>
+											</tr>
+										</thead>
+										<tbody>					
+											@foreach($ipledgehistory_data as $key1 => $data1)
+											<tr>
+												<td><a class="text-dark" href="{{route('IpledgefileDownload',$data1['id'])}}" target="_blank"><i class="fa fa-download" aria-hidden="true"></i>  {{$data1['files']}}</a></td>
+												<td>{{$data1['imported_by']}}</td>
+												<td>{{$data1['crated_at']}}</td>
+											</tr>
+											@endforeach
+										</tbody>
+									</table> --}}
+								</div>
 							</div>
-						</div>
-					</div> 
+						</div> 
+					</div>
+					<!--end 5th tab-->
 				</div>
-
-				<!--end of tab5-->
-			</div>
-		</section>
+			</section>
+		</div>
 	</div>
-</div>
 </div>
 </div>
 @endsection
@@ -279,8 +269,8 @@
 	});
 
 	$(document).on('click', '#i_pledge_agreement', function() {
-    $('#i_pledge_agreement_form').submit();
-  });
+		$('#i_pledge_agreement_form').submit();
+	});
 </script>
 
 
@@ -295,16 +285,16 @@
 @endsection
 <style>
 	.tab-content h4{
-			font-size:16px;
-		}
-		.tab-content p{
-			font-size:16px;
-			margin-bottom:0;
-		}
-		.inner-section {
-				width: 100%;
+		font-size:16px;
+	}
+	.tab-content p{
+		font-size:16px;
+		margin-bottom:0;
+	}
+	.inner-section {
+		width: 100%;
 		
-			}
-	</style>
+	}
+</style>
 
 

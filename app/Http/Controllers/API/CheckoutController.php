3e5e598cc40c7ffacc990a6eeb5e264a0ea4aa7b
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-24 11:43:08 +0530

    Update CheckoutController.php

--- app/Http/Controllers/API/CheckoutController.php
+++ app/Http/Controllers/API/CheckoutController.php
@@ -33,7 +33,7 @@ class CheckoutController extends BaseController
      ->select('checkout.id', 'checkout.md_status','checkout.status','checkout.created_at','checkout.updated_at')->where('checkout.user_id',$request->user_id)->OrderBy('id', 'desc')->first();
 
      if(!empty($orderlist)){
-         return $this->sendResponse( $orderlist. 'Order data retrieved successfully.');
+         return $this->sendResponse($orderlist, 'Order data retrieved successfully.');
      }else{
         return $this->sendResponse( $orderlist =array(), 'No Data Found.');
     }

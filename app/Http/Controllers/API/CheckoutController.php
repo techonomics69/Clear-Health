5fd2d09a7709b8c8a0567f9f0d71c8223f06dee6
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-24 14:50:39 +0530

    Update CheckoutController.php

--- app/Http/Controllers/API/CheckoutController.php
+++ app/Http/Controllers/API/CheckoutController.php
@@ -16,27 +16,25 @@ class CheckoutController extends BaseController
 {
     public function index()
     {
-      /*$order = checkout::join('users', 'users.id', '=', 'checkout.user_id')
-      ->join('carts','carts.id', '=', 'checkout.cart_id')
-      ->join('products', 'products.id', '=', 'carts.product_id')
-      ->select('users.name', 'users.mobile', 'products.name AS product_name' , 'carts.product_price', 'checkout.total_amount','checkout.case_id','checkout.created_at')->get();
 
-      return $this->sendResponse($order, 'Order retrieved successfully.');*/
-  }
+    }
 
 
   public function orderList(Request $request)
   {
 
     try{
-     $orderlist = checkout::join('users', 'users.id', '=', 'checkout.user_id')
-     ->join('carts','carts.id', '=', 'checkout.cart_id')
-      ->join('products', 'products.id', '=', 'carts.product_id')
-     ->select('checkout.id', 'checkout.md_status','checkout.status','checkout.created_at','checkout.updated_at','carts.order_type')->where('checkout.user_id',$request->user_id)->OrderBy('id', 'desc')->get();
+       $orderlist = checkout::join('users', 'users.id', '=', 'checkout.user_id')
+       ->join('carts','carts.id', '=', 'checkout.cart_id')
+       ->join('products', 'products.id', '=', 'carts.product_id')
+       ->select('checkout.id', 'checkout.md_status','checkout.status','checkout.created_at','checkout.updated_at','carts.order_type')
+       ->where('checkout.user_id',$request->user_id)
+       ->OrderBy('id', 'desc')
+       ->get();
 
-     if(!empty($orderlist)){
-         return $this->sendResponse($orderlist, 'Order data retrieved successfully.');
-     }else{
+       if(!empty($orderlist)){
+           return $this->sendResponse($orderlist, 'Order data retrieved successfully.');
+       }else{
         return $this->sendResponse( $orderlist =array(), 'No Data Found.');
     }
 
@@ -205,8 +203,8 @@ try{
             $checkout_data = Checkout::where('user_id', $request['user_id'])->where('case_id', $request['case_id'])->OrderBy('id', 'desc')->first();
             //$checkout_data = Checkout::where('user_id', $request->user_id)->where('cart_id', $request->cart_id)->first();
             if(!empty($checkout_data)){
-             return $this->sendResponse($checkout_data, 'Checkout data retrieved successfully.');
-         }else{
+               return $this->sendResponse($checkout_data, 'Checkout data retrieved successfully.');
+           }else{
             return $this->sendResponse($checkout_data =array(), 'No Data Found.');
         }
 
@@ -222,8 +220,8 @@ public function getCheckoutAddress(Request $request)
         $checkout_data = Checkoutaddress::where('user_id', $request->user_id)->OrderBy('id', 'desc')->first();
             //$checkout_data = Checkout::where('user_id', $request->user_id)->where('cart_id', $request->cart_id)->first();
         if(!empty($checkout_data)){
-         return $this->sendResponse($checkout_data, 'Checkout Address data retrieved successfully.');
-     }else{
+           return $this->sendResponse($checkout_data, 'Checkout Address data retrieved successfully.');
+       }else{
         return $this->sendResponse($checkout_data =array(), 'No Data Found.');
     }
 

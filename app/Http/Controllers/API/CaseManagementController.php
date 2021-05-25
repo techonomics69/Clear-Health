241411f2c416ba5fbdcefa0a745551de04762928
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-25 13:06:39 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/API/CaseManagementController.php
+++ app/Http/Controllers/API/CaseManagementController.php
@@ -857,6 +857,12 @@ public function CreateCase(Request $request){
       $no_substitutions = "true";
       $pharmacy_notes =  "This medication might not be suitable for people with... ";
       $quantity = $accutan_strength;
+
+
+      echo "<pre>";
+      print_r('https://api.mdintegrations.xyz/v1/partner/medications/select?name='.$product_name.'&strength='.$quantity.' mg');
+      echo "<pre>";
+      exit();
       
       $curl = curl_init();
 
@@ -883,6 +889,10 @@ public function CreateCase(Request $request){
 
       $medications = json_decode($medications);
 
+      echo "<pre>";
+      print_r($medications);
+      echo "<pre>";
+      exit();
 
       $DispensUnitId = $medications[0]->dispense_unit_id;
       $dosespot_medication_id = $medications[0]->dosespot_medication_id;

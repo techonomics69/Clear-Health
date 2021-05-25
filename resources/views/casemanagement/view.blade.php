e6b0883ddef241245bc01df60132dd6ccc829aa7
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-25 16:47:29 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -132,6 +132,7 @@
 							<div class="row" style="padding: 10px;">
 								<div class="col-md-12">
 									@foreach($answers as $key => $ans)
+									<?php echo $ans; ?>
 									<h4><strong>Que:{{ $ans->question }}</strong></h4>
 									<p>Ans:{{ $ans->answer }}</p>
 									@endforeach

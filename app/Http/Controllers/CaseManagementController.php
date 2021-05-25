f36e817fec9c700f689a72796e93788a4fba6eec
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-25 11:20:35 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/CaseManagementController.php
+++ app/Http/Controllers/CaseManagementController.php
@@ -58,7 +58,7 @@ class CaseManagementController extends Controller
       $category = QuizCategory::pluck('name', 'id')->toArray();
         //foreach ($user_case_management_data as $key => $value) {
 
-      $quiz = Answers::where('case_id',506)->where('user_id',1123)->select('answer')->get()->OrderBy('id','ASC')->get();
+      $quiz = Answers::where('case_id',506)->where('user_id',1123)->select('answer')->get()->OrderBy('id','ASC');
       //$q= json_decode($quiz,true);
       echo "<pre>";
       print_r($quiz);

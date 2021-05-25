f09dd8e759e1dea1fbc6e8afc73e342248d68092
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-25 10:42:16 +0530

    Update CheckoutController.php

--- app/Http/Controllers/API/CheckoutController.php
+++ app/Http/Controllers/API/CheckoutController.php
@@ -26,9 +26,8 @@ class CheckoutController extends BaseController
         try{
          $orderlist = checkout::join('users', 'users.id', '=', 'checkout.user_id')
          ->join('carts','carts.id', '=', 'checkout.cart_id')
-         ->select('checkout.id','checkout.order_id','checkout.md_status','checkout.status','checkout.created_at','checkout.updated_at','carts.order_type','checkout.cart_id')
+         ->select('checkout.id','checkout.order_id','checkout.md_status','checkout.status','checkout.created_at','checkout.updated_at','carts.order_type','checkout.cart_id','checkout.case_id')
          ->where('checkout.user_id',$request->user_id)
-         ->where('checkout.case_id',$request->case_id)
          ->OrderBy('id', 'DESC')
          ->get();
 

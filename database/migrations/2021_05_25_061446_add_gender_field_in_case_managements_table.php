671e64ce2efc1deb2b8ea4b7954740632ca792dd
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-25 15:42:55 +0530

    Update 2021_05_25_061446_add_gender_field_in_case_managements_table.php

--- database/migrations/2021_05_25_061446_add_gender_field_in_case_managements_table.php
+++ database/migrations/2021_05_25_061446_add_gender_field_in_case_managements_table.php
@@ -14,7 +14,7 @@ class AddGenderFieldInCaseManagementsTable extends Migration
     public function up()
     {
         Schema::table('case_managements', function (Blueprint $table) {
-            //
+            $table->enum('gender')->default('0')->after('user_id')->comment='0 = Not known,1 = Male,2 = Female,9 = Not applicable';
         });
     }
 
@@ -25,8 +25,8 @@ class AddGenderFieldInCaseManagementsTable extends Migration
      */
     public function down()
     {
-        Schema::table('case_managements', function (Blueprint $table) {
+        /*Schema::table('case_managements', function (Blueprint $table) {
             //
-        });
+        });*/
     }
 }

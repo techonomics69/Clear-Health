d4a1afb77314cca55b29d8e41199310ff6c0059b
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-25 13:08:20 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/CaseManagementController.php
+++ app/Http/Controllers/CaseManagementController.php
@@ -68,11 +68,15 @@ class CaseManagementController extends Controller
 
 
 foreach ($answer as $key => $value) {
-echo "<pre>";
+/*echo "<pre>";
 print_r($value);
 echo "</pre>";
 die();
+*/
 
+echo $value['question'];
+echo $value['answer'];
+die();
 }
       /*$quiz= QuizAnswer::join('quizzes','quiz_answers.question_id', '=', 'quizzes.id')->select('quiz_answers.*','quizzes.question','quizzes.category_id')->where('case_id', $user_case_management_data['id'])->OrderBy('id', 'ASC')->get();*/
 

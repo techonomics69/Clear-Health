92c0fabb15dc702d8ff9bd467649d7f953620891
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-25 14:39:34 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/CaseManagementController.php
+++ app/Http/Controllers/CaseManagementController.php
@@ -70,9 +70,8 @@ class CaseManagementController extends Controller
  
 foreach ($answers as $key => $value) {
 echo "<pre>";
-//print_r('Question:- ' .$value->question.'<br>');
+print_r('Question:- ' .$value->question.'<br>');
 print_r('Answers:- ' .$value->answer);
-//echo $value['answer'];
 echo "</pre>";
 echo "<br>";
 }

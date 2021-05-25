bdec6a73009b2c60af8ab6a18e64323d9e6a938e
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-25 11:30:11 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/CaseManagementController.php
+++ app/Http/Controllers/CaseManagementController.php
@@ -59,8 +59,8 @@ class CaseManagementController extends Controller
         //foreach ($user_case_management_data as $key => $value) {
 
       $quiz = Answers::where('case_id',506)->where('user_id',1123)->select('answer')->get();
-      $q= explode(" { ", ($quiz, "answer"));
-      //$q= json_decode($quiz,"answer");
+      //$q= explode(" { ", ($quiz, "answer"));
+      $q= json_decode($quiz,"answer");
       echo "<pre>";
       print_r($q);
       echo "</pre>";

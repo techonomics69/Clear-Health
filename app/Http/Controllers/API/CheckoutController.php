a7ec057ae6ec7702d25e1f93a55c748511c55e8f
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-26 18:41:10 +0530

    Update CheckoutController.php

--- app/Http/Controllers/API/CheckoutController.php
+++ app/Http/Controllers/API/CheckoutController.php
@@ -110,7 +110,8 @@ class CheckoutController extends BaseController
         }
         $checkoutdata = Checkout::create($data);
         $checkcout_address = Checkoutaddress::where('user_id', $data['user_id'])->OrderBy('id','DESC')->first();
-        $ordeID= $checkout_address['order_id'];
+        $ordeID['order_id'] = $checkout_address['order_id'];
+
         if(!empty($checkcout_address))
         {
          $orderIdUpdate = $checkcout_address->where('order_id', $ordeID)->update($ordeID);

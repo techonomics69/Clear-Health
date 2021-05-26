3d96b9191b43dd8c9669c9a6b99b5ed4b762e430
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-26 09:44:31 +0530

    Update CheckoutController.php

--- app/Http/Controllers/API/CheckoutController.php
+++ app/Http/Controllers/API/CheckoutController.php
@@ -230,7 +230,7 @@ try{
    
 
             $product_name = array();
-            $product_details  = Cart::join('products', 'products.id', '=', 'carts.product_id')->whereIn('carts.id', $cart_ids)->select('products.name AS product_name')->get()->toArray();
+            $product_details  = Cart::join('products', 'products.id', '=', 'carts.product_id')->whereIn('carts.id', $cart_ids)->select('products.name AS product_name'.'products.price')->get()->toArray();
             foreach($product_details as $product_key=>$product_value){
              $product_name[] = $product_value['product_name'];  
          }

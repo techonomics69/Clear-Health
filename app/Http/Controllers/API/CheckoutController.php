668b2dc36714d486623ccdeada7bda9891daf514
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-26 17:10:29 +0530

    Update CheckoutController.php

--- app/Http/Controllers/API/CheckoutController.php
+++ app/Http/Controllers/API/CheckoutController.php
@@ -247,7 +247,7 @@ try{
      ->join('carts','carts.id', '=', 'checkout.cart_id')
      ->join('checkout_address', 'checkout_address.user_id', '=', 'checkout.user_id')
      ->select('checkout.id','users.first_name','users.last_name','checkout.order_id','carts.quantity','carts.order_type','checkout.cart_id','checkout_address.addressline1','checkout_address.addressline2','checkout_address.city','checkout_address.state','checkout_address.zipcode')
-     ->where('checkout.order_id',$request->order_id)
+     ->where('checkout.id',$request->id)
      ->OrderBy('id', 'DESC')
      ->get();
 

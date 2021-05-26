dd20e0918432a0efb4aab2ec5404fa83578e76d5
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-26 09:46:14 +0530

    Update CheckoutController.php

--- app/Http/Controllers/API/CheckoutController.php
+++ app/Http/Controllers/API/CheckoutController.php
@@ -230,9 +230,10 @@ try{
    
 
             $product_name = array();
-            $product_details  = Cart::join('products', 'products.id', '=', 'carts.product_id')->whereIn('carts.id', $cart_ids)->select('products.name AS product_name'.'products.price')->get()->toArray();
+            $product_details  = Cart::join('products', 'products.id', '=', 'carts.product_id')->whereIn('carts.id', $cart_ids)->select('products.name AS product_name','products.price')->get()->toArray();
             foreach($product_details as $product_key=>$product_value){
-             $product_name[] = $product_value['product_name'];  
+             $product_name[] = $product_value['product_name'];
+             $price[]=$product_value['price'];  
          }
          $orderlist[$key]->product_name = implode(', ' ,$product_name); 
          echo "<pre>";

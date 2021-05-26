a011c26b1af3beed3a74d9fd5653f26860e1ae80
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-26 12:01:41 +0530

    Update CheckoutController.php

--- app/Http/Controllers/API/CheckoutController.php
+++ app/Http/Controllers/API/CheckoutController.php
@@ -219,31 +219,6 @@ try{
            ->where('checkout.order_id',$request->order_id)
            ->OrderBy('id', 'DESC')
            ->get();
-$superheroes = array(
-    "spider-man" => array(
-        "name" => "Peter Parker",
-        "email" => "peterparker@mail.com",
-    ),
-    "super-man" => array(
-        "name" => "Clark Kent",
-        "email" => "clarkkent@mail.com",
-    ),
-    "iron-man" => array(
-        "name" => "Harry Potter",
-        "email" => "harrypotter@mail.com",
-    )
-);
- 
-// Printing all the keys and values one by one
-$keys = array_keys($superheroes);
-for($i = 0; $i < count($superheroes); $i++) {
-    echo $keys[$i] . "{<br>";
-    foreach($superheroes[$keys[$i]] as $key => $value) {
-        echo $key . " : " . $value . "<br>";
-    }
-    echo "}<br>";
-}
-
             
 
            foreach($orderlist as $key=>$val)

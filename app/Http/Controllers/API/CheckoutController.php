49754f6ab917e08936900013555b52878a180af0
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-26 13:38:30 +0530

    Update CheckoutController.php

--- app/Http/Controllers/API/CheckoutController.php
+++ app/Http/Controllers/API/CheckoutController.php
@@ -263,7 +263,7 @@ try{
            $products[$product_key]['order_type'] =$product_value['order_type'];
            $products[$product_key]['pharmacy_pickup']='';
 
-           if(isset($product_value['pharmacy_pickup']) && $product_value['pharmacy_pickup'] != '' && $product_value['order_type'] == 'Prescribed'){
+           if(isset($product_value['pharmacy_pickup']) && $product_value['pharmacy_pickup'] != '' && $product_value['order_type'] = 'Prescribed'){
 
             if($product_value['pharmacy_pickup'] != "cash"){
                 $r = $this->get_token();
@@ -288,15 +288,14 @@ try{
                 ));
 
                 $response = curl_exec($curl);
-
                 curl_close($curl);
 
             }else{
 
-             $products[$product_key]['pharmacy_pickup']= 'cash';
+             $products[$product_key]['pharmacy_pickup'] = 'cash';
          }
 
-         $products[$product_key]['pharmacy_pickup']= '';
+         $products[$product_key]['pharmacy_pickup'] = '';
      }
 
  }

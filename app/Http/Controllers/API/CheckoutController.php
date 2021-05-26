d1c46d94a669f83b4b6946286981c6e9b30ce1e8
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-26 11:16:42 +0530

    Update CheckoutController.php

--- app/Http/Controllers/API/CheckoutController.php
+++ app/Http/Controllers/API/CheckoutController.php
@@ -233,9 +233,9 @@ try{
             foreach($product_details as $product_key => $product_value)
             {
                 $orderlist[$key]->product_name = $product_value['product_name'];
-               /*$product_name[] = $product_value['product_name'];
+               //$product_name[] = $product_value['product_name'];
                $price[] = $product_value['price'];
-               $image[] = $product_value['image'];*/
+               $image[] = $product_value['image'];
 
             }
 

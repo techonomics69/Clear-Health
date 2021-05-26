c489c37b2d4da4000a935f7e31a25ca273050e0c
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-26 16:37:20 +0530

    Update CheckoutController.php

--- app/Http/Controllers/API/CheckoutController.php
+++ app/Http/Controllers/API/CheckoutController.php
@@ -294,19 +294,17 @@ try{
 
                 $response = curl_exec($curl);
                 $response1 = json_decode($response);
-                print_r( $response1);
-                die();
+                
                 $products[$product_key]['pharmacy_pickup'] =  $response1->name;
                 /*$products[$product_key] =  $response1->address1;
                 $products[$product_key] =  $response1->address2;
                  $products[$product_key] =  $response1->city;
                  $products[$product_key]=  $response1->state;
                   $products[$product_key] =  $response1->zip_code;*/
-print_r($products);
+            print_r($products);
                 die();
-
-                curl_close($curl);
                 
+                curl_close($curl);
             }else{
 
              $products[$product_key]['pharmacy_pickup'] = 'cash';

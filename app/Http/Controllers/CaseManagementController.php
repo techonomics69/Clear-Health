c5f540b5abdb2e7298445d1d4cc3c9a67e549182
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-27 12:26:33 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/CaseManagementController.php
+++ app/Http/Controllers/CaseManagementController.php
@@ -55,7 +55,7 @@ class CaseManagementController extends Controller
     public function show($id)
     {
       $user_case_management_data = CaseManagement::join('users','case_managements.user_id', '=', 'users.id')->select('case_managements.*','users.first_name','users.last_name','users.email','users.mobile','users.gender')->where('case_managements.id',$id)->first();
-      
+
       $category = QuizCategory::pluck('name', 'id')->toArray();
 
       $answers_data = Answers::where('case_id',$user_case_management_data['id'])->where('user_id',$user_case_management_data['user_id'])->get();
@@ -65,14 +65,6 @@ class CaseManagementController extends Controller
       $answers = json_decode($answers_data[0]['answer']);
 
       }
-
-     /* foreach ($answers as $key => $value) {
-          $questions = $value->question;
-          if(isset($value->answer)){
-           $answer =  $value->answer;
-          }
-
-      }*/
 
       return view('casemanagement.view',compact('user_case_management_data','category','answers'));
     }

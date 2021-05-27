9a2c12ed3a52997accbb15a4c6c89d8e131c8ebc
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-27 10:08:50 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -172,11 +172,13 @@
 														$questions = $ans->question;
 														if(isset($ans->answer)){
 															$answer =  $ans->answer;
+
+															$ans1 = implode('( ) ' ,$answer)
 														}
 														?>
 														<h4><strong>Que: <?php echo $questions; ?></strong></h4>
 
-														<p>Ans: <?php print_r($answer); ?> </p>
+														<p>Ans: <?php print_r($ans1); ?> </p>
 
 
 													</div>

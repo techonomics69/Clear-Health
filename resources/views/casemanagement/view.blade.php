92a4a1a10b0e6c4b7a99aec303ade0c4fc1c4808
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-27 18:22:22 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -170,15 +170,22 @@
 												
 @foreach($general as $key => $general_qa)
 <div class="row" style="padding: 10px;">
-											<div class="col-md-12">
-<?php  echo "<h4><strong>Que:".$general_qa['question']."</strong></h4>";
-	  echo "<p>Ans:".$general_qa['answer']."</p>"; ?>
+	<div class="col-md-12">
+<?php  
+
+$questions = $general_qa->question;
+	if(isset($general_qa->answer)){
+	$answer =  (array)$general_qa->answer;
+	$getanswer= implode(" " , $answer);
+	} ?>
+<h4><strong>Que: <?php echo $questions; ?></strong></h4>
+<p>Ans: <?php echo $getanswer; ?> ?>
 	</div>
 </div>
 @endforeach
 @foreach($accutane as $key => $accutane_qa)
 <div class="row" style="padding: 10px;">
-											<div class="col-md-12">
+	<div class="col-md-12">
 <?php  echo "<h4><strong>Que:".$accutane_qa['question']."</strong></h4>";
 	   echo "<p>Ans:".$accutane_qa['answer']."</p>"; ?>
 	</div>
@@ -186,7 +193,7 @@
 @endforeach
 @foreach($topical as $key => $topical_qa)
 <div class="row" style="padding: 10px;">
-											<div class="col-md-12">
+	<div class="col-md-12">
 <?php  echo "<h4><strong>Que:".$accutane_qa['question']."</strong></h4>";
 	  echo "<p>Ans:".$accutane_qa['answer']."</p>"; ?>
 	</div>

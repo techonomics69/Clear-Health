921f7f591f0b9c87b8d45142c31fecd8943f3b87
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-27 11:19:40 +0530

    Update AnswerController.php

--- app/Http/Controllers/API/AnswerController.php
+++ app/Http/Controllers/API/AnswerController.php
@@ -19,7 +19,7 @@ class AnswerController extends BaseController
     public function getAnswer(Request $request)
     {
         try{
-            $answer = Answers::where('user_id', $request->user_id)->where('case_id', $request->case_id)->first();
+            $answer = Answers::where('user_id', $request->user_id)->where('case_id', $request->case_id)->where('category_id', $request->category_id)->first();
           
                  return $this->sendResponse($answer, 'Answer retrieved successfully.');
         }catch(\Exception $ex){
@@ -39,7 +39,7 @@ class AnswerController extends BaseController
             if($validator->fails()){
                 return $this->sendError('Validation Error.', $validator->errors()->all());       
             }
-            $answer = Answers::where('user_id', $data['user_id'])->where('case_id', $data['case_id'])->first();
+            $answer = Answers::where('user_id', $data['user_id'])->where('case_id', $data['case_id'])->where('category_id', $data['category_id'])->first();
             if(isset($answer)){
                 $answerUpdate = Answers::where('id',$answer->id)->update($data);
                 return $this->sendResponse(array(), 'Answer Updated Successfully');

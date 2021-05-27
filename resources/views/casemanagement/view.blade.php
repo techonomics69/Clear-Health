387ca1296af7f4ddff46fde86b6a468f8b260f30
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-27 18:32:28 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -172,19 +172,16 @@
 <div class="row" style="padding: 10px;">
 	<div class="col-md-12">
 <?php  
-echo"<pre>";
-print_r($general_qa->question);
-echo"</pre>";
-
-$questions = $general_qa->question;
-
+print_r($general_qa);
+die();
+$getquestions = $general_qa->question;
 
 	if(isset($general_qa->answer)){
 	$answer =  (array)$general_qa->answer;
 	$getanswer= implode(" " , $answer);
 	} ?>
-<h4><strong>Que: <?php //echo $questions; ?></strong></h4>
-<p>Ans: <?php //echo $getanswer; ?> 
+<h4><strong>Que: <?php echo $getquestions; ?></strong></h4>
+<p>Ans: <?php echo $getanswer; ?> 
 	</div>
 </div>
 @endforeach

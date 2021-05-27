3eeff34c0b2bb1710db03cd4a6d22b335bec5ae9
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-27 15:23:08 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/CaseManagementController.php
+++ app/Http/Controllers/CaseManagementController.php
@@ -63,7 +63,7 @@ class CaseManagementController extends Controller
       // $quiz= QuizAnswer::join('quizzes','quiz_answers.question_id', '=', 'quizzes.id')->select('quiz_answers.*','quizzes.question','quizzes.category_id')->where('case_id', $user_case_management_data['id'])->OrderBy('id', 'ASC')->get();
 
       //if(!empty($answers_data)){
-
+      $$answers_data=[];
         $answers = json_decode($answers_data[0]['answer']);
 
 

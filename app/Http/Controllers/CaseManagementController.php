5027fe943359b60de73aa9ff612b23567e9eeaa1
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-27 12:36:26 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/CaseManagementController.php
+++ app/Http/Controllers/CaseManagementController.php
@@ -58,7 +58,9 @@ class CaseManagementController extends Controller
 
       $category = QuizCategory::pluck('name', 'id')->toArray();
 
-      $answers_data = Answers::where('case_id',$user_case_management_data['id'])->where('user_id',$user_case_management_data['user_id'])->get();
+      $answers_data = Answers::where('case_id',$user_case_management_data['id'])->where('user_id',$user_case_management_data['user_id'])->where('category_id',$user_case_management_data['category_id'])->get();
+
+      // $quiz= QuizAnswer::join('quizzes','quiz_answers.question_id', '=', 'quizzes.id')->select('quiz_answers.*','quizzes.question','quizzes.category_id')->where('case_id', $user_case_management_data['id'])->OrderBy('id', 'ASC')->get();
 
     if(!empty($answers_data)){
 

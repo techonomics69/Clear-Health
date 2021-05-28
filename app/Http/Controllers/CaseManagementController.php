012928df51ac661ed73179039b81a6a032f8fd82
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-28 18:51:19 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/CaseManagementController.php
+++ app/Http/Controllers/CaseManagementController.php
@@ -65,7 +65,7 @@ class CaseManagementController extends Controller
       $general_que = [];
     }
 echo"<pre>";
-print_r($general);
+print_r($general_que);
 echo"</pre>";
 die();
       $accutane = Answers::where('case_id',$user_case_management_data['id'])->where('user_id',$user_case_management_data['user_id'])->where('category_id',8)->get();

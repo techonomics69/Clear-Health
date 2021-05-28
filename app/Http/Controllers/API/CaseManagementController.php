84deb9c690e458937c480cc65088456335d9eddd
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-28 16:31:23 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/API/CaseManagementController.php
+++ app/Http/Controllers/API/CaseManagementController.php
@@ -185,7 +185,6 @@ public function create_patient(Request $request)
   create_patient();
 }
 
-
   public function searchStateDetail(Request $request){
     $r = get_token();
     $token_data = json_decode($r);
@@ -213,8 +212,6 @@ public function create_patient(Request $request)
     $response = curl_exec($curl);
 
     curl_close($curl);
-    //echo $response;
-
     return $this->sendResponse(json_decode($response),'State data recieved successfully');
   }
 

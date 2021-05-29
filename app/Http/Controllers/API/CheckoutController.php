2c5e4bb2061fce2581c2184f95833fcf9247d340
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-29 17:14:52 +0530

    Update CheckoutController.php

--- app/Http/Controllers/API/CheckoutController.php
+++ app/Http/Controllers/API/CheckoutController.php
@@ -253,7 +253,16 @@ try{
      ->OrderBy('id', 'DESC')
      ->first();
 
+  $shipping_address = Checkoutaddress::select('checkout_address.patient_firstname','checkout_address.patient_lastname','checkout_address.addressline1','checkout_address.addressline2','checkout_address.city','checkout_address.state','checkout_address.zipcode','checkout_address.email','checkout_address.phone')
+     ->where('checkout_address.order_id',$orderlist['order_id'])
+     ->where('checkout_address.address_type',1)
+     ->OrderBy('id', 'DESC')
+     ->first();
 
+echo "<pre>";
+print_r($shipping_address);
+echo "<pre>";
+exit();
     // foreach($orderlist as $key=>$val)
      //{
         $cart_ids = explode(',', $orderlist['cart_id']);

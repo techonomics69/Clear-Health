d9bfaa7b85919538aa38712e59dc2a8956c6bfbe
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-29 18:47:28 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/API/CaseManagementController.php
+++ app/Http/Controllers/API/CaseManagementController.php
@@ -539,11 +539,7 @@ public function detach_file_from_case(Request $request){
 
         $postfields = json_encode($postfields);
 
-        echo "<pre>";
-        print_r($postfields);
-        echo "<pre>";
-        exit();
-
+       
         $curl = curl_init();
 
         curl_setopt_array($curl, array(

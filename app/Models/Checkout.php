8213edd3a91b407ff12353963a4d631d3b710e79
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-29 15:33:26 +0530

    Update Checkout.php

--- app/Models/Checkout.php
+++ app/Models/Checkout.php
@@ -9,9 +9,7 @@ class Checkout extends Model
 {
     use HasFactory;
     protected $table = "checkout";
-    protected $fillable = ['id','user_id', 'order_id', 'cart_id', 'case_id' ,'cart_amount', 'total_amount', 'patient_firstname','patient_lastname','email', 'shipping_fee','shipping_method','shipping_addreess_id','billing_address_id','card_number','card_name','address_type','pharmacy_detail','medication_type','plan_id','plan_quantity'];
-
-
+    protected $fillable = ['id','user_id', 'order_id', 'cart_id', 'case_id' ,'cart_amount', 'total_amount', 'patient_firstname','patient_lastname','email', 'shipping_fee','shipping_method','shipping_addreess_id','billing_address_id','card_number','card_name','address_type','pharmacy_detail','medication_type','plan_id','plan_quantity','ipladege_id','delivery_date','md_case_id','md_status','telemedicine_fee','handling_fee','tax','status','order_detail_id'];
 
      public function shipping_addreess()
     {

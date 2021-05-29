31231c3816f2f26ab2a585b2de414969a04b69e3
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-29 18:25:09 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/CaseManagementController.php
+++ app/Http/Controllers/CaseManagementController.php
@@ -75,6 +75,11 @@ class CaseManagementController extends Controller
 
 $product_details  = Cart::join('products', 'products.id', '=', 'carts.product_id')->whereIn('carts.id', $cart_ids)->select('products.name AS product_name','products.used_for_plan','carts.quantity','carts.order_type','carts.pharmacy_pickup','carts.product_price as price')->get()->toArray();
 
+foreach($product_details as $product_key => $product_value)
+        {
+           
+         $product_name[] = $product_value['product_name'];
+         }
 $skincare_summary['product_name'] = implode(', ' ,$product_details);
 
 echo"<pre>";

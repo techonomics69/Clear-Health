e00f7e6528ffb2a4af5737a42f0cbc99f6e212d4
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-31 18:52:29 +0530

    Update index.blade.php

--- resources/views/ordermanagement/index.blade.php
+++ resources/views/ordermanagement/index.blade.php
@@ -70,15 +70,14 @@
                                             } ?></td>
 
                                            <td>
-                                            
                                                 <div class="d-flex">
-                                                    <a class="icons edit-icon" href="{{ route('ordermanagement.show',$order_data->id) $order_data->case_id }}">
+                                                    <a class="icons edit-icon" href="{{ route('ordermanagement.show',$order_data->id)}}">
                                                         <i class="fa fa-eye"></i>
                                                     </a> 
                                                 </div>  
                                             </td>
                                             </tr>
-                                            
+
                                             @endforeach
                                         </tbody>
                                     </table>

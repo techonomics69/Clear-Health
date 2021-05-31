3a1c5b0ce8ebfc283c136cc1bafacc77549c211c
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-31 10:32:05 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/CaseManagementController.php
+++ app/Http/Controllers/CaseManagementController.php
@@ -85,6 +85,8 @@ if(isset($product_details['pharmacy_pickup']) && $product_details['pharmacy_pick
 print_r($pharmacy_id);
 echo "</pre>";
 die();*/
+
+
                 $curl = curl_init();
                 curl_setopt_array($curl, array(
                     CURLOPT_URL => 'https://api.mdintegrations.xyz/v1/partner/pharmacies/'.$pharmacy_id,

f3fc2378f57ab74c79684907f3e0104c262da387
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-31 12:40:52 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/CaseManagementController.php
+++ app/Http/Controllers/CaseManagementController.php
@@ -57,35 +57,7 @@ class CaseManagementController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-public function get_token(){
-      $curl = curl_init();
 
-      curl_setopt_array($curl, array(
-        CURLOPT_URL => 'https://api.mdintegrations.xyz/v1/partner/auth/token',
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_POSTFIELDS =>'{
-          "grant_type": "client_credentials",
-          "client_id": "c7a20a90-4db9-42e4-860a-7f41c2a8a0b1",
-          "client_secret": "xBsQsgLFhYIFNlKwhJW3wClOmNuJ4WQDX0n8475C",
-          "scope": "*"
-      }',
-      CURLOPT_HTTPHEADER => array(
-          'Content-Type: application/json',
-          'Cookie: __cfduid=db3bdfa9cd5de377331fced06a838a4421617781226'
-      ),
-  ));
-
-      $response = curl_exec($curl);
-
-      curl_close($curl);
-      return $response;
-  }
     
     public function show($id)
     {

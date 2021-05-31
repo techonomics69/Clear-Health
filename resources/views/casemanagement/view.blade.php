84d2c492f46c226424f5826f8df0f24449195fd2
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-31 13:21:09 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -339,7 +339,9 @@
 											<div class="box-block mtb32">
 												<h3 class="font-weight-bold"><span class="text-underline">Pharmacy Preference</span></h3>
 												<div class="col-md-6  form-group">
+
 												{{$skincare_summary['pharmacy_pickup'] }}
+												
 												</div>
 												
 											</div>

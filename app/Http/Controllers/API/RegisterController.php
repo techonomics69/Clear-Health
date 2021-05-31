d0926e828b13f8f7d27b6b36f5b8d455958160bf
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-31 16:50:57 +0530

    Update RegisterController.php

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -103,7 +103,8 @@ class RegisterController extends BaseController
                 $success['email'] =  $user->email;
                 $success['user_id'] =  $user->id;
 
-       $case_status =  CaseManagement::where("user_id",$request['user_id'])->where("case_status" , 'completed')->first();      
+       $case_status =  CaseManagement::where("user_id",$request['user_id'])->OrderBy("user_id" , "DESC")->first(); 
+
        echo"<pre>";
        print_r($case_status);
        echo"</pre>";

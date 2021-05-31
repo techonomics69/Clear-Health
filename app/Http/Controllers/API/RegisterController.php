c056f60791537f34f7ef364ebb272cba51293918
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-05-31 17:23:22 +0530

    Update RegisterController.php

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -110,7 +110,7 @@ $complete = true;
 
 if($case_status->case_status == "completed")
 {
-    $complete;
+   echo  $complete;
 }
 //$acnebother = false;
 /*if($case_status->case_status == "/acnebother")

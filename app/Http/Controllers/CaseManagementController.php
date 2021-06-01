343be819c19877fa377e179d2fdb1858462db8ac
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-01 18:35:35 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/CaseManagementController.php
+++ app/Http/Controllers/CaseManagementController.php
@@ -153,10 +153,10 @@ if(isset($accutane) || array_key_exists("answer",$accutane))
    $accutane_que = [];
 }
 
-/*echo "<pre>";
+echo "<pre>";
 print_r($accutane_que);
 echo "</pre>";
-die();*/
+die();
 
 $topical = Answers::where('case_id',$user_case_management_data['id'])->where('user_id',$user_case_management_data['user_id'])->where('category_id',9)->first();
 

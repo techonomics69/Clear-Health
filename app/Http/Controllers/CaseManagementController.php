8dfa41242101fca3f1adc204747cf5055ecff05e
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-01 15:26:50 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/CaseManagementController.php
+++ app/Http/Controllers/CaseManagementController.php
@@ -130,6 +130,12 @@ class CaseManagementController extends Controller
    $category = QuizCategory::pluck('name', 'id')->toArray();
 
    $general = Answers::where('case_id',$user_case_management_data['id'])->where('user_id',$user_case_management_data['user_id'])->where('category_id',7)->get();
+
+echo "<pre>";
+print_r($general);
+echo "</pre>";
+die();
+
    if(!empty($general[0])){
     $general_que=json_decode($general[0]["answer"]);
   } else {

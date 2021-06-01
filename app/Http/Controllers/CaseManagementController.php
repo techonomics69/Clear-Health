219707733aa7ee3c71fe6f7819739455476341bf
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-01 19:01:11 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/CaseManagementController.php
+++ app/Http/Controllers/CaseManagementController.php
@@ -155,7 +155,7 @@ if(isset($accutane))
 echo "<pre>";
 print_r($accutane);
 echo "</pre>";
-
+die();
 $topical = Answers::where('case_id',$user_case_management_data['id'])->where('user_id',$user_case_management_data['user_id'])->where('category_id',9)->first();
 
 /*$topical_que=json_decode($topical->answer);*/

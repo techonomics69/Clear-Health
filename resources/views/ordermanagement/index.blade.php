9c300f2604fe2cb9561bed8c45d9a5b7582fd787
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-01 14:40:22 +0530

    Update index.blade.php

--- resources/views/ordermanagement/index.blade.php
+++ resources/views/ordermanagement/index.blade.php
@@ -84,7 +84,7 @@
                                            <?php if(isset($order_data->case_id) && $order_data->case_id == ''){ ?>
                                              <td> 
                                                 <div class="d-flex">
-                                                    <a class="icons edit-icon" href="{{ route('casemanagement.show',$order_data->case_id)}}">
+                                                    <a class="icons edit-icon" href="#">
                                                         <i class="fa fa-eye"></i>
                                                     </a> 
                                                 </div>  

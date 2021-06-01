83030587fffb95c4b48acf3d979e0432dd357e9f
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-01 16:49:40 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/CaseManagementController.php
+++ app/Http/Controllers/CaseManagementController.php
@@ -146,15 +146,16 @@ die();*/
 
 $accutane_que=json_decode($accutane->answer); 
 
+echo "<pre>";
+print_r($accutane_que);
+echo "</pre>";
+die();
   if (isset($accutane->answer)) {
    $accutane_que=json_decode($accutane->answer);
  }else{
   $accutane_que = [];
 }
-/*echo "<pre>";
-print_r($accutane_que);
-echo "</pre>";
-die();*/
+
 
 $topical = Answers::where('case_id',$user_case_management_data['id'])->where('user_id',$user_case_management_data['user_id'])->where('category_id',9)->first();
 

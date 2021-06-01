f2ccb441a0a38da551154204ca02f2b49131ad11
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-01 12:22:04 +0530

    Update index.blade.php

--- resources/views/ordermanagement/index.blade.php
+++ resources/views/ordermanagement/index.blade.php
@@ -84,16 +84,16 @@ die();
                                                 </div>  
                                             </td>
                                         <?php }else{ ?>
-
+<?php if(isset($order_data->case_id == '') && $order_data->case_id){ ?>
                                            <td> 
                                             <div class="d-flex">
-                                                <a class="icons edit-icon" href="#">
+                                                <a class="icons edit-icon" href="{{ route('casemanagement.show',$order_data->case_id)}}">
                                                     <i class="fa fa-eye"></i>
                                                 </a> 
                                             </div>  
                                         </td>
 
-                                    <?php } ?>
+                                    <?php} } ?>
                                 </tr>
                                 @endforeach
 

7be9581e5c8ba2a885846c5c90cf620acc288ca5
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-02 09:46:10 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/CaseManagementController.php
+++ app/Http/Controllers/CaseManagementController.php
@@ -132,7 +132,12 @@ class CaseManagementController extends Controller
    $general = Answers::where('case_id',$user_case_management_data['id'])->where('user_id',$user_case_management_data['user_id'])->where('category_id',7)->first();
 
    //$general_que=json_decode($general->answer);
-
+foreach ($general_que as $key => $value) {
+echo "<pre>";
+print_r($value);
+echo "</pre>";
+die();
+}
    if(isset($general))
    {
      $general_que = json_decode($general->answer); 
@@ -140,10 +145,6 @@ class CaseManagementController extends Controller
     $general_que = [];
    }
 
-/*echo "<pre>";
-print_r($general);
-echo "</pre>";
-die();*/
 $accutane = Answers::where('case_id',$user_case_management_data['id'])->where('user_id',$user_case_management_data['user_id'])->where('category_id',8)->first();
 
 //$accutane_que=json_decode($accutane->answer); 

ed3e2afe29d52a1e25463ab203184d5efcd816c9
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-02 10:15:00 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/CaseManagementController.php
+++ app/Http/Controllers/CaseManagementController.php
@@ -143,6 +143,7 @@ foreach ($general_que as $key => $value) {
   if(isset($value) && $value->question='Hey there! First, we need to know your legal name.'){
 
 $value->answer = $user_case_management_data->first_name;
+$value->answer = $user_case_management_data->last_name;
 echo "<pre>";
 print_r($value->question);
 echo "<br>";

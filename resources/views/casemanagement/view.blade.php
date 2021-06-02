13cd1a5037a7039f760cf835a08bbd1db99e71be
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-02 11:25:51 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -172,8 +172,8 @@
 															<?php 
 															if(isset($general->question) && $general->question == 'Hey there! First, we need to know your legal name.')
 															{ 
-																$first_name = $value->answer = $user_case_management_data->first_name;
-																$last_name =  $value->answer = $user_case_management_data->last_name;
+																/*$first_name = $value->answer = $user_case_management_data->first_name;
+																$last_name =  $value->answer = $user_case_management_data->last_name;*/
 																?>
 																<h4><strong>Que <?php echo ++$j; ?> : <?php 
 																/*if(isset($general) && $general->question="Hey there! First, we need to know your legal name.") {*/

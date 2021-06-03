5b715e54cef8b93f208ef726983f27800ea9b5e0
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-03 12:00:58 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
-  
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-  
+
 use App\Http\Controllers\API\RegisterController;
 use App\Http\Controllers\API\ProductController;
 use App\Http\Controllers\API\BlogController;
@@ -20,7 +20,7 @@ use App\Http\Controllers\API\CaseManagementController;
 use App\Http\Controllers\API\CheckoutController;
 use App\Http\Controllers\API\AnswerController;
 use App\Http\Controllers\API\TreatmentGuidesController;
-  
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -31,72 +31,72 @@ use App\Http\Controllers\API\TreatmentGuidesController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-  
+
 Route::post('register', [RegisterController::class, 'register']);
 Route::post('login', [RegisterController::class, 'login']);
 Route::post('forgot_password',[RegisterController::class, 'forgot_password']);
-     
+
 //Route::middleware('auth:api')->group( function () {
-    Route::resource('checkout',CheckoutController::class);
-    Route::resource('products', ProductController::class);
-    Route::resource('blogs', BlogController::class);
-    Route::get('getalltags',[BlogController::class, 'getAllTags']);
-    Route::get('getmostpopular',[BlogController::class, 'most_popular']);
+Route::resource('checkout',CheckoutController::class);
+Route::resource('products', ProductController::class);
+Route::resource('blogs', BlogController::class);
+Route::get('getalltags',[BlogController::class, 'getAllTags']);
+Route::get('getmostpopular',[BlogController::class, 'most_popular']);
 
 //Treatment Guides
-    Route::resource('treatmentGuides',TreatmentGuidesController::class);
-     Route::get('treatmentGuides/{id}', [TreatmentGuidesController::class, 'show']);
+Route::resource('treatmentGuides',TreatmentGuidesController::class);
+Route::get('treatmentGuides/{id}', [TreatmentGuidesController::class, 'show']);
 
-   
-    Route::resource('category', CategoryController::class);
-    Route::resource('cms', CmsController::class);
-    Route::get('cmspages/{pagenameid}',[CmsController::class,'show']);
-    Route::get('faqlist/{category}', [FaqController::class, 'index']);
-    Route::get('faq_category', [FaqController::class ,'getFaqCategoryList']);
-    Route::resource('testimonial', TestimonialController::class);
-    Route::resource('quiz', QuizController::class);
-    Route::resource('quizAnswer', QuizAnswerController::class);
-    Route::get('quizAnswer/user/{id}', [QuizAnswerController::class, 'getAnswerByUser']);
-    Route::post('questionByOrderAndCategory', [QuizController::class, 'getQuizByOrderAndCategory']);
-    Route::get('getQuestionListOfGeneral', [QuizController::class, 'getQuestionListOfGeneral']);
-    Route::get('quizAnswer/question/{id}', [QuizAnswerController::class, 'getAnswerByQuestion']);
-    Route::post('getQuizAnswer', [QuizAnswerController::class, 'getAnswerByUserQuestionCaseID']);
-    Route::get('quizCategory', [QuizController::class ,'getQuizCategoryList']);
-	Route::post('user/update/{id}', [UserController::class, 'update_user']);
-    Route::get('user/{id}', [UserController::class, 'show']);
-    Route::get('userlist', [UserController::class, 'userlist']);
-    Route::post('sendmail', [UserController::class, 'sendmail']); 
-    Route::post('addParentdetails', [UserController::class, 'addParentdetails']);
-    Route::post('getParentdetails', [UserController::class, 'getParentdetails']);
-    
+
+Route::resource('category', CategoryController::class);
+Route::resource('cms', CmsController::class);
+Route::get('cmspages/{pagenameid}',[CmsController::class,'show']);
+Route::get('faqlist/{category}', [FaqController::class, 'index']);
+Route::get('faq_category', [FaqController::class ,'getFaqCategoryList']);
+Route::resource('testimonial', TestimonialController::class);
+Route::resource('quiz', QuizController::class);
+Route::resource('quizAnswer', QuizAnswerController::class);
+Route::get('quizAnswer/user/{id}', [QuizAnswerController::class, 'getAnswerByUser']);
+Route::post('questionByOrderAndCategory', [QuizController::class, 'getQuizByOrderAndCategory']);
+Route::get('getQuestionListOfGeneral', [QuizController::class, 'getQuestionListOfGeneral']);
+Route::get('quizAnswer/question/{id}', [QuizAnswerController::class, 'getAnswerByQuestion']);
+Route::post('getQuizAnswer', [QuizAnswerController::class, 'getAnswerByUserQuestionCaseID']);
+Route::get('quizCategory', [QuizController::class ,'getQuizCategoryList']);
+Route::post('user/update/{id}', [UserController::class, 'update_user']);
+Route::get('user/{id}', [UserController::class, 'show']);
+Route::get('userlist', [UserController::class, 'userlist']);
+Route::post('sendmail', [UserController::class, 'sendmail']); 
+Route::post('addParentdetails', [UserController::class, 'addParentdetails']);
+Route::post('getParentdetails', [UserController::class, 'getParentdetails']);
+
     //Cart
-    Route::resource('cart', CartController::class);
-    Route::get('cart/item/{id}',[CartController::class, 'getCartByUser']);
-    Route::get('cart/prescribeditem/{id}',[CartController::class, 'getCartByUserPrescribed']);
-    Route::get('cart/addonitem/{id}',[CartController::class, 'getCartByUserAddOn']);
-    Route::post('cart/addonItemUpdate/{id}', [CartController::class, 'addonItemUpdate']);
-    Route::post('cart/cartRemove/{id}', [CartController::class, 'cartRemove']);
-    Route::post('cart/prescribedItemUpdate', [CartController::class, 'prescribedItemUpdate']);
+Route::resource('cart', CartController::class);
+Route::get('cart/item/{id}',[CartController::class, 'getCartByUser']);
+Route::get('cart/prescribeditem/{id}',[CartController::class, 'getCartByUserPrescribed']);
+Route::get('cart/addonitem/{id}',[CartController::class, 'getCartByUserAddOn']);
+Route::post('cart/addonItemUpdate/{id}', [CartController::class, 'addonItemUpdate']);
+Route::post('cart/cartRemove/{id}', [CartController::class, 'cartRemove']);
+Route::post('cart/prescribedItemUpdate', [CartController::class, 'prescribedItemUpdate']);
 
 
-    Route::post('getFees',[FeeController::class, 'getFees']);
-    Route::resource('caseManagement', CaseManagementController::class);
-    Route::post('createOrder', [CheckoutController::class, 'store']);
-    Route::post('addCheckoutAddress', [CheckoutController::class, 'addCheckoutAddress']);
-    Route::post('orderList', [CheckoutController::class, 'orderList']);
-    Route::post('getOrderDetail', [CheckoutController::class, 'getCheckoutdetail']);
-    Route::post('getCheckoutAddress', [CheckoutController::class, 'getCheckoutAddress']);
-    Route::get('getUpsellProductDetails', [ProductController::class, 'getUpsellProductDetails']);
-    Route::get('getskincareplan', [ProductController::class, 'getskincareplan']);
-    Route::post('ProductRecommend',[QuizAnswerController::class, 'ProductRecommend']);
-    Route::post('ProductRecommendBasedONTretinoinFormula',[QuizAnswerController::class, 'ProductRecommendBasedONTretinoinFormula']);
-    Route::post('ProductActive',[ProductController::class, 'ProductActive']);
+Route::post('getFees',[FeeController::class, 'getFees']);
+Route::resource('caseManagement', CaseManagementController::class);
+Route::post('createOrder', [CheckoutController::class, 'store']);
+Route::post('addCheckoutAddress', [CheckoutController::class, 'addCheckoutAddress']);
+Route::post('orderList', [CheckoutController::class, 'orderList']);
+Route::post('getOrderDetail', [CheckoutController::class, 'getCheckoutdetail']);
+Route::post('getCheckoutAddress', [CheckoutController::class, 'getCheckoutAddress']);
+Route::get('getUpsellProductDetails', [ProductController::class, 'getUpsellProductDetails']);
+Route::get('getskincareplan', [ProductController::class, 'getskincareplan']);
+Route::post('ProductRecommend',[QuizAnswerController::class, 'ProductRecommend']);
+Route::post('ProductRecommendBasedONTretinoinFormula',[QuizAnswerController::class, 'ProductRecommendBasedONTretinoinFormula']);
+Route::post('ProductActive',[ProductController::class, 'ProductActive']);
 
     //MD API
 
-    Route::post('create_patient', [CaseManagementController::class, 'create_patient']);
-    Route::post('demo', [CaseManagementController::class, 'demo']);
-    Route::post('getToken', [CaseManagementController::class, 'get_token']);
+Route::post('create_patient', [CaseManagementController::class, 'create_patient']);
+Route::post('demo', [CaseManagementController::class, 'demo']);
+Route::post('getToken', [CaseManagementController::class, 'get_token']);
     Route::post('searchStateDetail', [CaseManagementController::class, 'searchStateDetail']);//search state data
     Route::get('getAllStates', [CaseManagementController::class, 'getAllStates']);
     Route::post('getCitiesFromGivenState', [CaseManagementController::class, 'getCitiesFromGivenState']);

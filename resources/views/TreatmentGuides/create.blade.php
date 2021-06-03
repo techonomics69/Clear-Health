cc8e9c410f586423fc6de12f0f65e1f7a4bb6e7d
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-03 13:07:53 +0530

    Update create.blade.php

--- resources/views/TreatmentGuides/create.blade.php
+++ resources/views/TreatmentGuides/create.blade.php
@@ -42,7 +42,7 @@
                     </div>
                 </header>  -->
                 <div class="card-body">
-                {!! Form::open(array('route' => 'products.store','method'=>'POST', 'enctype'=>"multipart/form-data")) !!}
+                {!! Form::open(array('route' => 'treatmentGuides.store','method'=>'POST', 'enctype'=>"multipart/form-data")) !!}
                     <div class="row">
                         <div class="col-md-4 col-sm-6 col-xs-12">
                                     <div class="form-group">

69c685e4ec36a1097c24aa27b4c1cdbc80337675
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-03 13:10:20 +0530

    Update TreatmentGuidesController.php

--- app/Http/Controllers/TreatmentGuidesController.php
+++ app/Http/Controllers/TreatmentGuidesController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-    
+
 use App\Models\TreatmentGuides;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
@@ -16,37 +16,71 @@ class TreatmentGuidesController extends Controller
      * @return \Illuminate\Http\Response
      */
 
+
+    public function index()
+    {
+      
+    }
+    public function create()
+    {
+     
+
+    }
+
+
     public function store(Request $request)
     {
-        $regex = '/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/';
-        $this->validate($request, [
-            
-            'title' => 'required|unique:products,name|regex:/^[\pL\s\-]+$/u',
-            'sub_title' => 'required',
-            'status' => 'required|not_in:0',
-            'guides_image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:5000',
-            'detail' => 'required',
-        ]);
-        $data = $request->all();
+      $regex = '/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/';
+      $this->validate($request, [
+        
+        'title' => 'required|unique:products,name|regex:/^[\pL\s\-]+$/u',
+        'sub_title' => 'required',
+        'status' => 'required|not_in:0',
+        'guides_image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:5000',
+        'detail' => 'required',
+      ]);
+      $data = $request->all();
 
-        if(!empty($request->guides_image)):
-            $imageName = time().'.'.$request->guides_image->extension();
+      if(!empty($request->guides_image)):
+        $imageName = time().'.'.$request->guides_image->extension();
 
-            $path = public_path().'/images/TreatmentGuides';
+        $path = public_path().'/images/TreatmentGuides';
 
-            if (! File::exists($path)) {
-                File::makeDirectory($path, $mode = 0777, true, true);
-            }
+        if (! File::exists($path)) {
+          File::makeDirectory($path, $mode = 0777, true, true);
+        }
 
-            $request->guides_image->move(public_path('images/TreatmentGuides'), $imageName);
-            $data['guides_image'] = $imageName;
-        endif;
+        $request->guides_image->move(public_path('images/TreatmentGuides'), $imageName);
+        $data['guides_image'] = $imageName;
+      endif;
 
-        $treat_guides = TreatmentGuides::create($data);
-                
-        toastr()->success('Treatment Guides created successfully');
+      $treat_guides = TreatmentGuides::create($data);
+      
+      toastr()->success('Treatment Guides created successfully');
 
-        return redirect()->route('treatmentGuides.index');
+      return redirect()->route('treatmentGuides.index');
+    }
+
+    public function show($id)
+    {
+      
+    }
+
+    public function edit($id)
+    {
+      
     }
     
-}
+
+    public function update(Request $request, $id)
+    {
+      
+    } 
+    public function destroy($id)
+    {
+
+    }
+
+    
+    
+  }

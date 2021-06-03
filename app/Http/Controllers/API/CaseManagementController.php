f28ed8c3e3d2795e346bcf28cc168f046da22988
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-03 15:56:49 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/API/CaseManagementController.php
+++ app/Http/Controllers/API/CaseManagementController.php
@@ -993,7 +993,7 @@ public function createMessage(Request $request){
      	$message_data[$key]['messageStatus'] = $messageStatus;
 
      	if($value['file_path']!=''){
-     		$message_data[$key]['file_path'] = assets($value['file_path']);
+     		$message_data[$key]['file_path'] = assets(''.$value['file_path'].'');
      	}else{
      		$message_data[$key]['file_path'] = null;
      	}

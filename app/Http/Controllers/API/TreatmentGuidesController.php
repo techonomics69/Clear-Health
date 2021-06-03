400e103669840995a45ecb3c17e31f743cdca7b9
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-03 12:19:40 +0530

    Update TreatmentGuidesController.php

--- app/Http/Controllers/API/TreatmentGuidesController.php
+++ app/Http/Controllers/API/TreatmentGuidesController.php
@@ -89,7 +89,7 @@ class TreatmentGuidesController extends BaseController
 //$products = Product::where('product_active', '1')->get();
         foreach ($array as $key => $value) {
            //$product = Product::find($id);
-          echo $value["id"]."<br />";
+          /*echo $value["id"]."<br />";*/
 
       }
 

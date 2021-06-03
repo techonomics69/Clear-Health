49866a9c3227f49f530b48f6f5b8015bc3e05ea6
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-03 11:47:40 +0530

    Update TreatmentGuidesController.php

--- app/Http/Controllers/API/TreatmentGuidesController.php
+++ app/Http/Controllers/API/TreatmentGuidesController.php
@@ -21,24 +21,27 @@ class TreatmentGuidesController extends BaseController
     {
 
         $array = array(
+            [
             "id" =>"1",
             "title" => "abc",
             "sub_title" => "abc1",
             "status" =>"1",
             "guides_image" => "abcimg",
-            array(
-               "id" =>"2",
+           ],
+              [
+                "id" =>"2",
                "title" => "pqr",
                "sub_title" => "pqr1",
                "status" =>"1",
                "guides_image" => "pqrimg",
-               array(
+               ],
+               [
               "id" =>"3",
                "title" => "xyz",
                "sub_title" => "xyz1",
                "status" =>"1",
                "guides_image" => "xyzimg",
-            )));
+           ]);
 /*array(
                "id" =>"2",
                "title" => "pqr",

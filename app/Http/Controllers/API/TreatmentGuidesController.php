032f155d3ac9b92d15e0f48cf4b234d2740986b0
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-03 14:44:58 +0530

    Update TreatmentGuidesController.php

--- app/Http/Controllers/API/TreatmentGuidesController.php
+++ app/Http/Controllers/API/TreatmentGuidesController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\API\BaseController as BaseController;
 use Illuminate\Http\Request;
+use App\Models\TreatmentGuides;
 
 use Validator;
 use Exception;
@@ -20,93 +21,18 @@ class TreatmentGuidesController extends BaseController
     public function index()
     {
 
-        $array = array(
-            [
-                "id" =>"1",
-                "title" => "abc",
-                "sub_title" => "abc1",
-                "status" =>"1",
-                "detail" => "AAAAAAA",
-                "guides_image" => "abcimg",
-            ],
-            [
-                "id" =>"2",
-                "title" => "pqr",
-                "sub_title" => "pqr1",
-                "status" =>"1",
-                "detail" => "BBBBBBB",
-                "guides_image" => "pqrimg",
-            ],
-            [
-              "id" =>"3",
-              "title" => "xyz",
-              "sub_title" => "xyz1",
-              "status" =>"1",
-              "detail" => "CCCCCCCC",
-              "guides_image" => "xyzimg",
-          ],
-          [
-              "id" =>"4",
-              "title" => "test",
-              "sub_title" => "test1",
-              "status" =>"1",
-              "detail" => "DDDDDDD",
-              "guides_image" => "testimg",
-          ]);
-
-        return $this->sendResponse($array,'Treatement Guides retrieved successfully.');
+      $TreatmentGuides = TreatmentGuides::all();
+        return $this->sendResponse($TreatmentGuides,'Treatement Guides retrieved successfully.');
     }
 
     public function show($id)
     {
-        $Treatment = array(
-            [
-                "id" =>"1",
-                "title" => "abc",
-                "sub_title" => "abc1",
-                "status" =>"1",
-                "guides_image" => "abcimg",
-            ],
-            [
-                "id" =>"2",
-                "title" => "pqr",
-                "sub_title" => "pqr1",
-                "status" =>"1",
-                "guides_image" => "pqrimg",
-            ],
-            [
-              "id" =>"3",
-              "title" => "xyz",
-              "sub_title" => "xyz1",
-              "status" =>"1",
-              "guides_image" => "xyzimg",
-          ],
-          [
-              "id" =>"4",
-              "title" => "test",
-              "sub_title" => "test1",
-              "status" =>"1",
-              "guides_image" => "testimg",
-          ]);
-
-
-        //$Treatment = $array::find($id);
-        /*echo "<pre>";
-        print_r($Treatment);
-        echo "</pre>";*/
-        //die();
-
-        //$cms = Cms::find($id);
-        //foreach ($array as $key => $value) {
-           //$product = Product::find($id);
-          /*echo $value["id"]."<br />";*/
-
-      //}
-
-      if (is_null($Treatment)) {
-        return $this->sendError('Blog not found.');
-    }
-
-    return $this->sendResponse($Treatment, 'Cms retrieved successfully.');
+        $TreatmentGuides = TreatmentGuides::find($id);
+  
+        if (is_null($TreatmentGuides)) {
+            return $this->sendError('Treatment Guides not found.');
+        }
+   
+        return $this->sendResponse($TreatmentGuides, 'Treatment Guides retrieved successfully.');
 }
 }

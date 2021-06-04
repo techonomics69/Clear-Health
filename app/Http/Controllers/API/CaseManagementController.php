574c538e72d4d93441907e835d0f53e6a59600f0
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-04 15:02:38 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/API/CaseManagementController.php
+++ app/Http/Controllers/API/CaseManagementController.php
@@ -1080,7 +1080,7 @@ public function createMessage(Request $request){
     $destinationPath = public_path('/Message_files');
 
 
-    $messagefiles_details = Messages::join('message_files', 'messages.id', '=', 'message_files.msg_id')->select(' messages.id as message_id','message_files.*')->where('user_id', '=',$user_id)->first();
+    $messagefiles_details = Messages::join('message_files', 'messages.id', '=', 'message_files.msg_id')->select('messages.id as message_id','message_files.*')->where('user_id', '=',$user_id)->first();
 
     $file_name = str_replace("public/Message_files/","",$messagefiles_details['system_file']);
     

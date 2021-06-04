cd02c28a69ca0583de2230661f14016b79f1c104
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-04 10:47:35 +0530

    Update index.blade.php

--- resources/views/treatmentGuides/index.blade.php
+++ resources/views/treatmentGuides/index.blade.php
@@ -10,14 +10,7 @@
     </div>
     @endif
     <!DOCTYPE html>
-    <html>
-    <head>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    </head>
-    <body>
-
-    </body>
-    </html>
+   
 
     <div class="content-wrapper">
         <div class="content-header row">

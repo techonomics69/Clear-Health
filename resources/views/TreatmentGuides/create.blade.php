80cff3be125c5655a2a486fb98680819eb571e90
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-04 09:37:19 +0530

    Update create.blade.php

--- resources/views/TreatmentGuides/create.blade.php
+++ resources/views/TreatmentGuides/create.blade.php
@@ -20,7 +20,7 @@
                     <div class="breadcrumb-wrapper col-12 d-flex">
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item"><a href="{{url('/admin/dashboard')}}">Dashboard</a></li>
-                            <li class="breadcrumb-item active">Create Guides</li>
+                            <li class="breadcrumb-item active">Create Treatment Guides</li>
                         </ol>
                     </div>
                 </div>

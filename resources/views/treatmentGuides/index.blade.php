5e197d54f7b1eb2bc0f105f320463efd6bca4dfb
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-04 10:02:27 +0530

    Update index.blade.php

--- resources/views/treatmentGuides/index.blade.php
+++ resources/views/treatmentGuides/index.blade.php
@@ -69,24 +69,26 @@
                                             <span class="badge badge-danger">Out Of Stock</span>
                                             @endif
                                         </td>
-                                        <td>
-                                            <a class="icons edit-icon" href="{{ route('treatmentGuides.show',$guides->id) }}">
-                                                <i class="fa fa-eye"></i>
-                                            </a>        
-                                            @can('treatmentGuides-edit')                     
-                                            <a class="icons edit-icon" href="{{ route('treatmentGuides.edit',$guides->id) }}">
-                                                <i class="fa fa-edit"></i>
-                                            </a>  
-                                            @endcan                                                                                                                                                                                    
-                                            @can('treatmentGuides-delete')                          
-                                            {!! Form::open(['method' => 'DELETE','route' => ['treatmentGuides.destroy', $guides->id],'style'=>'display:inline']) !!}
-                                            <a class="icons edit-icon product_delete" href="#" id="{{$guides->id}}" onclick="deleteGuides({{$guides->id}})">
-                                                <i class="fa fa-trash" aria-hidden="true"></i>
-                                            </a>
-                                            <button type="submit" class="btn btn-danger btn_delete{{$guides->id}}" style="display:none;">Delete</button>
-                                            {!! Form::close() !!}    
-                                            @endcan                       
-                                        </td>
+                                       <td>
+                                    <div class="d-flex">
+                                    <a class="icons edit-icon" href="{{ route('deleteGuides.show',$guides->id) }}">
+                                        <i class="fa fa-eye"></i>
+                                    </a> 
+                                    @can('treatmentGuides-edit')    
+                                    <a class="icons edit-icon" href="{{ route('treatmentGuides.edit',$guides->id) }}">
+                                        <i class="fa fa-edit"></i>
+                                    </a>
+                                    @endcan
+                                    @can('treatmentGuides-delete')                          
+                                    {!! Form::open(['method' => 'DELETE','route' => ['treatmentGuides.destroy', $guides->id],'style'=>'display:inline']) !!}
+                                        <a class="icons edit-icon treatmentGuides_delete" href="#" id="{{$guides->id}}" onclick="deleteGuides({{$guides->id}})">
+                                            <i class="fa fa-trash" aria-hidden="true"></i>
+                                        </a>
+                                        <button type="submit" class="btn btn-danger btn_delete{{$guides->id}}" style="display:none;">Delete</button>
+                                    {!! Form::close() !!}
+                                    @endcan 
+                                </div>                      
+                                </td>
                                     </tr>
                                     @endforeach
                                 </tbody>

308e85adf296a015e091ea0096d0613db571842d
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-05 15:33:16 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -122,8 +122,6 @@ Route::post('getToken', [CaseManagementController::class, 'get_token']);
     Route::post('sendMessageNonMedical', [CaseManagementController::class, 'sendMessageNonMedical']);
     Route::post('getMessagesNonMedical', [CaseManagementController::class, 'getMessagesNonMedical']);
 
-
-
     Route::post('user/update_vouch_status', [UserController::class, 'updateVerifiedByVouch']);
     Route::get('user/vouch_details/{id}', [UserController::class, 'getVouchedDetails']);
     Route::post('add_recomeended_product', [CaseManagementController::class, 'add_recomeended_product']);

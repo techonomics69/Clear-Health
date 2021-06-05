94d3300991adc2cd3fb403f36c3b86839c62e18c
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-05 14:41:37 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -602,7 +602,7 @@
 
                             <div class="last-typing-section">
                                 <div class="camera lastimg">
-                                    <img src="publi/images/camera.png" alt="">
+                                    <img src="public/images/camera.png" alt="">
                                 </div>
                                 <div class="attachment lastimg">
                                     <img src="assets/images/paperclip.png" alt="">

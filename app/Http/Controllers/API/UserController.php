2e1d8fe6f9d36e6110979bacb0c2de943b8cfd0b
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-05 15:31:40 +0530

    Update UserController.php

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -145,51 +145,13 @@ public function show($id)
     return $this->sendResponse($user,'user Retrived successfully');
 }
 
-public function updateVerifiedByVouch(Request $request){
-    //try{
+public function updateVerifiedByVouch(Request $request, $id){
+    try{
+        $input = $request->all();
 
-        //$input = $request->all();
+        $data = User::whereId($id)->update($input);
 
-        $user_id = $request['user_id'];
-        $case_id = $request['case_id'];
-        $order_id = $request['order_id'];
-
-        $data = User::where('id',$user_id)->update(['verified_by_vouch' => $request['verified_by_vouch']]);
-
-        //$user = User::find($id);
-
-         $orderdata = checkout::where('checkout.order_id',$order_id)->where('checkout.case_id',$case_id)->where('checkout.user_id',$user_id)->first();
-
-
-        //code gor md create case
-        if($data['medication_type'] == 1){
-
-            //call create patient api
-                $patient_id = create_patient($data['user_id'],$data['case_id']);
-            //end of code create patient api
-
-
-            if($patient_id != ''){
-
-               $cart_ids = explode(',', $data['cart_id']);
-
-               $pharmacy_data  =  Cart::select('pharmacy_pickup')->where('user_id',$data['user_id'])->whereIn('id',$cart_ids)->where('order_type', '!=', 'AddOn')->first();
-
-               $preferred_pharmacy_id = $pharmacy_data['pharmacy_pickup'];
-
-               $response = CreateCase($data['user_id'],$data['case_id'],$preferred_pharmacy_id,$patient_id);
-
-               $response = json_decode($response);
-
-               if(!empty($response)){
-
-                $md_response = $response;
-               
-                 return $this->sendResponse($md_response, 'Order Created Successfully');
-                }
-            }else{
-                 return $this->sendResponse(array(), 'something went wrong!');
-            }
+        $user = User::find($id);
 
         if($data == 1){
             return $this->sendResponse($user, 'User Status Updated Successfully');
@@ -198,10 +160,9 @@ public function updateVerifiedByVouch(Request $request){
           return $this->sendResponse($user, 'Something went wrong!');  
       }
 
-  }
-  /*}catch(\Exception $ex){
-     return $this->sendError('Server error',array($ex->getMessage()));
-  }*/
+  }catch(\Exception $ex){
+   return $this->sendError('Server error',array($ex->getMessage()));
+}
 }
 
 public function getVouchedDetails(Request $request, $id){

26546a1e0477c3f4511046c1df8437915dfb16fa
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-07 10:49:30 +0530

    Update ChangePasswordController.php

--- app/Http/Controllers/API/ChangePasswordController.php
+++ app/Http/Controllers/API/ChangePasswordController.php
@@ -7,6 +7,7 @@ use App\Rules\MatchOldPassword;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Validator;
 use Exception;
 

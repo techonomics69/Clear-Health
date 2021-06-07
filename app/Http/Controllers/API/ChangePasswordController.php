7bcac72cbe7e47798feb83ca1f4dcbf15261e63c
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-07 10:47:29 +0530

    Update ChangePasswordController.php

--- app/Http/Controllers/API/ChangePasswordController.php
+++ app/Http/Controllers/API/ChangePasswordController.php
@@ -29,6 +29,57 @@ class ChangePasswordController extends BaseController
     }
 
 
+
+public function changePassword(Request $request)
+{
+    $input = $request->all();
+    $userid = Auth::guard('api')->user()->id;
+
+        $request->validate([
+            'email' => ['required'],
+            'current_password' => ['required'],
+            'new_password' => ['required'],
+            'new_confirm_password' => ['same:new_password'],
+        ]);
+
+
+
+   /* $rules = array(
+        'old_password' => 'required',
+        'new_password' => 'required|min:6',
+        'confirm_password' => 'required|same:new_password',
+    );*/
+    $validator = Validator::make($input, $request);
+
+     echo"<pre>";
+   print_r($validator);
+   echo"</pre>";
+   die();
+    if ($validator->fails()) {
+        $arr = array("status" => 400, "message" => $validator->errors()->first(), "data" => array());
+    } else {
+        try {
+            if ((Hash::check(request('old_password'), Auth::user()->password)) == false) {
+                $arr = array("status" => 400, "message" => "Check your old password.", "data" => array());
+            } else if ((Hash::check(request('new_password'), Auth::user()->password)) == true) {
+                $arr = array("status" => 400, "message" => "Please enter a password which is not similar then current password.", "data" => array());
+            } else {
+                User::where('id', $userid)->update(['password' => Hash::make($input['new_password'])]);
+                $arr = array("status" => 200, "message" => "Password updated successfully.", "data" => array());
+            }
+        } catch (\Exception $ex) {
+            if (isset($ex->errorInfo[2])) {
+                $msg = $ex->errorInfo[2];
+            } else {
+                $msg = $ex->getMessage();
+            }
+            $arr = array("status" => 400, "message" => $msg, "data" => array());
+        }
+    }
+     return $this->sendResponse($arr, 'Password Change Successfully');
+    //return \Response::json($arr);
+}
+
     /*public function changePassword(Request $request)
     {
       

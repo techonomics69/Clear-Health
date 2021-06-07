aa884af28ccc1fb52f3f7017633af14fab519bf9
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-07 12:52:00 +0530

    Update index.blade.php

--- resources/views/ordermanagement/index.blade.php
+++ resources/views/ordermanagement/index.blade.php
@@ -81,7 +81,18 @@
                                                 </div>  
                                             </td>
                                         <?php }else { ?>
-                                             
+                                            <?php if(isset($order_data->case_id)){?>
+
+                                           <td> 
+                                                <div class="d-flex">
+                                                    <a class="icons edit-icon" href="{{ route('casemanagement.show',$order_data->case_id)}}">
+                                                        <i class="fa fa-eye"></i> 
+                                                    </a> 
+                                                </div>  
+                                            </td> 
+
+
+                                        <?php }?> 
                                     <?php  }?>
                                     </tr>
                                     @endforeach

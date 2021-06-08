a6e30bc56405abfd44b0340162cd3818931c7dfb
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-08 17:26:49 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/CaseManagementController.php
+++ app/Http/Controllers/CaseManagementController.php
@@ -397,7 +397,7 @@ $sender = "admin";
 //toastr()->success('Message send');
 //return redirect()->back();
 
-return Redirect::route('casemanagement.view')->with( ['message_data' => $message_data] );
+return Redirect('sendMessageNonMedical')->back()->with('message_data');
 //return redirect('sendMessageNonMedical')->with(['message_data' => $message_data] );
 //return redirect('sendMessageNonMedical',compact('message_data'));
 

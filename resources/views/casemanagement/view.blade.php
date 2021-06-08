723cc6ecda52547ab2a687aefd7ff4a5e52dad4a
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-08 18:35:29 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -593,7 +593,9 @@
 {!! Form::open(array('route' => 'sendMessageNonMedical','method'=>'POST', 'enctype'=>"multipart/form-data")) !!}
 
 <?php 
-//echo $message_data; 
+$test =Session::get('message_data')
+echo "TEStttttttttttt";
+print_r($test);
 //echo $q;
 ?>
 

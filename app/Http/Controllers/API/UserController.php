655900c43c72b02bb09b3aabaec260c5fcfd6300
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-08 14:15:55 +0530

    Update UserController.php

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -312,18 +312,17 @@ public function addUserPic(Request $request)
   $userpic=UserPics::where('user_id',$request['user_id'])->where('case_id',$request['case_id'])->first();
 
   if(isset($userpic)){
-    $userpicUpdate = UserPics::where('id',$userpic->id)->update([ 'left_pic'=>$user_left_pic,'straight_pic'=>$user_straight_pic,'right_pic'=>$user_right_pic,'other_pic' => $user_other_pic]
-      /*'left_pic'=>$user_left_pic,
+    $userpicUpdate = UserPics::where('id',$userpic->id)->update(array(
+      'left_pic'=>$user_left_pic,
       'straight_pic'=>$user_straight_pic,
       'right_pic'=>$user_right_pic,
-      'other_pic' => $user_other_pic,*/
+      'other_pic' => $user_other_pic,
 
       /*'left_pic'=>$file_name_left_pic,
       'straight_pic'=>$file_name_straight_pic,
       'right_pic'=>$file_name_right_pic,
       'other_pic' => $file_name_other_pic,*/
-   // )
-  );
+    ));
     return $this->sendResponse(array(), 'User picture update successfully');
   }else{
 

a2f0f36a2a6bde50783f75af6103ae0a3bd24fdb
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-08 17:48:34 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -579,12 +579,7 @@
                             <p>Brandon Koh,MD</p>
                         </div> -->
 
- {{ session()->get( 'message_data' ) }}                     
-<?php 
-
-$q= Session::get('message_data');
-//echo $q; 
-print_r($q); ?>
+ 
 
 {!! Form::open(array('route' => 'sendMessageNonMedical','method'=>'POST', 'enctype'=>"multipart/form-data")) !!}
 

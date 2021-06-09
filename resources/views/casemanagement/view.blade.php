89da198679a50d8f7eaa34b39b226a0c15cf8025
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-09 10:49:56 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -577,6 +577,7 @@ $msg_tab = 0;
 											</ul>
 											<div class="tab-content">
 												<div id="tab1" class="tab-pane fade in @if($msg_tab == 1) active@endif">	
+													
 													<div class="row" style="padding: 10px;">
 														<div class="col-md-12">
 															<div class="box-block mtb32" id="tab1">

14113426499cf810f22ed1572fb40150704f3281
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-09 14:49:02 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -627,7 +627,7 @@
                         <div class="chating-section">
                         	@foreach ($message_details as $key => $message1)
 <?php  
-print_r($message_details);
+print_r($message1['sender']);
 //print_r($message_details['sender']); ?>
 @endforeach
                         	<ul><?php if(isset($message_data)) {?>

b16651bb5b09272b1a7611fd9ea901d8b7592d5c
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-09 17:31:55 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -632,9 +632,10 @@
 ?>
 
 
-<img src="{{ asset('public/images/'.$message['file_name']) }}" alt='Girl in a jacket' width='100'>
+<img src="{{ asset('public/'.$message['file_name']) }}" alt='Girl in a jacket' width='100'>
 <!-- http://103.101.59.95/dev.clearhealth/public/images/Message_files/1623238722-camera.png
-http://103.101.59.95/dev.clearhealth/public/Message_files/1623238668-telegram.png -->
+http://103.101.59.95/dev.clearhealth/public/Message_files/1623238668-telegram.png
+http://103.101.59.95/dev.clearhealth/public/images/1623238722-camera.png -->
 <?php
 
 														

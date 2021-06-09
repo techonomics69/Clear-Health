2bfc02c40ad3103a1ff6eb0d3dcaa2efab7719ae
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-09 11:14:47 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -576,7 +576,7 @@ $msg_tab = 0;
 												<li><a class="btn" data-toggle="tab" href="#tab2">Non-Medical Messgaes</a></li>
 											</ul>
 											<div class="tab-content">
-												<div id="tab1" class="tab-pane fade in @if($msg_tab == 1) active show @endif">	
+												<div id="tab1" class="tab-pane fade in @if($msg_tab == 1) active @endif">	
 													<div class="row" style="padding: 10px;">
 														<div class="col-md-12">
 															<div class="box-block mtb32" id="tab1">

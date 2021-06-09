b58f9249bbbe031db89d66d044682e5db1e2be8a
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-09 11:25:54 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -573,7 +573,7 @@ $msg_tab = 0;
 										<section class="card">
 											<ul class="nav nav-tabs" id="messages-tab-menu">
 												<li><a class="btn  @if($msg_tab != 1) active @endif" data-toggle="tab" href="#tab1">Medical Messgaes</a></li>
-												<li><a class="btn" data-toggle="tab" href="#tab2">Non-Medical Messgaes</a></li>
+												<li><a class="btn  @if($msg_tab != 1) active @endif" data-toggle="tab" href="#tab2">Non-Medical Messgaes</a></li>
 											</ul>
 											<div class="tab-content">
 												<div id="tab1" class="tab-pane fade in @if($msg_tab == 1) active @endif">	

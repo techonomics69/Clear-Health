8469014d843ddafb401ca314fe4a9c287fa174cf
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-09 17:04:17 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -619,14 +619,8 @@
 							<div class="right-cht">
 								{!! Form::open(array('route' => 'sendMessageNonMedical','method'=>'POST', 'enctype'=>"multipart/form-data")) !!}
 								<div class="chating-section">
-<?php //echo "<pre>"; print_r($message_data); echo "</pre>"; die(); ?>
-
-<?php //echo "<pre>"; print_r($message_details); echo "</pre>"; die(); ?>
 									<ul><?php if(isset($message_data)) {?>
-										
 											@foreach ($message_data as $key => $message)
-
-
 											<li>
 												<p>
 													<?php 
@@ -645,7 +639,6 @@
 										
 									<?php }?>
 								</ul>
-
 							</div>
 																<div class="last-typing-section">
                         	<!-- <div class="camera lastimg">

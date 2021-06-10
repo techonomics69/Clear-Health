6993313cc2422a5a3df95f2592abdf6f62ee4e16
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-10 12:17:46 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -789,12 +789,7 @@ width: 100%;
 @section('scriptsection')
 <script>
 $(document).ready(function() {
-    alert("I am an alert box!");
-});	
-</script>
-<script>
-$(document).ready(function() {
-	alert('safsfsdf');
+	 alert("I am an alert box!");
 	$('#btnsubmit').on('click', function() {
 		alert('hii');
 		$("#btnsubmit").attr("disabled", "disabled");

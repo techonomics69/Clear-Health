fbacf23515656b9aa54fc5db3cc0ab07db10fd64
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-10 18:50:12 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/CaseManagementController.php
+++ app/Http/Controllers/CaseManagementController.php
@@ -211,7 +211,10 @@ class CaseManagementController extends Controller
     //$case_message_id = $request['system_case_id'];
     $channel = 'patient';
 
-
+echo "<pre>";
+  print_r( $case_id );
+  echo "</pre>";
+  die();
 
     $curl = curl_init();
 
@@ -274,10 +277,10 @@ class CaseManagementController extends Controller
 
 
 
-echo "<pre>";
+/*echo "<pre>";
   print_r($msg_history);
   echo "</pre>";
-  die();
+  die();*/
 
   return view('casemanagement.view',compact('user_case_management_data','category','general_que','accutane_que','topical_que','skincare_summary','message_data','message_details'));
 

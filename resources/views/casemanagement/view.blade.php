f6c35b1123ea0ce13231ad4982ea2355f613ec54
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-10 15:44:43 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -796,7 +796,7 @@
 				//cache: false,
 				success: function(response){
 					//myVar = response;
-					 //alert(myVar);
+					alert(response);
 					 $(".removemsg").removeClass("active") ; 
 					 $(".nonmedicalmsg").addClass("active") ; 
 					 location.reload();

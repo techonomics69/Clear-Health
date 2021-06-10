1e40eb7c9bcf10780de2aba5979b1fc6c7097100
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-10 17:39:19 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/CaseManagementController.php
+++ app/Http/Controllers/CaseManagementController.php
@@ -425,7 +425,7 @@ $sender = "admin";
 
     //$message_data['date'] = date('M j', $message_data['created_at']);
 echo "<pre>";
-  print_r($message_data['created_at']);
+  print_r($message_data['created_at']->date;
   echo "</pre>";
   die();
     $message_file_data = array();

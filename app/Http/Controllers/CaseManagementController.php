7aeec2012d2e9e809c23d02abab7c6ef725c21b7
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-11 16:16:36 +0530

    non msg

--- app/Http/Controllers/CaseManagementController.php
+++ app/Http/Controllers/CaseManagementController.php
@@ -203,7 +203,7 @@ class CaseManagementController extends Controller
 
 // Medical msg
 
-    $r = get_token();
+    /*$r = get_token();
     $token_data = json_decode($r);
     $token = $token_data->access_token;
     $case_id = $user_case_management_data['md_case_id'];
@@ -252,7 +252,7 @@ class CaseManagementController extends Controller
       }
 
       $i++;
-    }
+    }*/
 
    /* if(!empty($msg_history) && count($msg_history)>0 ){
       return $this->sendResponse($msg_history,'Message retrieved successfully');
@@ -269,7 +269,7 @@ class CaseManagementController extends Controller
   echo "</pre>";
   die();*/
 
-  return view('casemanagement.view',compact('user_case_management_data','category','general_que','accutane_que','topical_que','skincare_summary','message_data','message_details','msg_history'));
+  return view('casemanagement.view',compact('user_case_management_data','category','general_que','accutane_que','topical_que','skincare_summary','message_data','message_details'));
 
 }
 

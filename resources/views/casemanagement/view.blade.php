01d63edf0c4fe5f37c21b31850600a423875a58a
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-11 13:10:38 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -756,9 +756,9 @@
 			event.preventDefault();
 			var file_data = $('#file').prop('files')[0];
 			var file_data = $('#text').val();
-			var file_data = $('#user_id').val();
+			//var file_data = $('#user_id').val();
 			//var file_data = $('#case_id').val();
-			//var form_data = new FormData();
+			var form_data = new FormData();
         	form_data.append('file', file_data);
 
 

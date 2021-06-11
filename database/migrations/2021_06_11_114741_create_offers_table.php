c7536987f4f635565882704418147634f37f12e5
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-11 17:27:12 +0530

    Create 2021_06_11_114741_create_offers_table.php

---
+++ database/migrations/2021_06_11_114741_create_offers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOffersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->id();
+            $table->date('from_date');
+            $table->date('to_date');
+            $table->string('promocode');
+            $table->text('description');
+            $table->string('promocode_type'); 
+            $table->string('promocode_value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offers');
+    }
+}

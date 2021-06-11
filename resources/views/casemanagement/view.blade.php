e1e9f471d59ff00e7bbdd56fa6e08a812cfd7f55
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-11 18:15:49 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -774,7 +774,7 @@
       			processData: false,
       			contentType: false,
 				success: function(response){
-					if(response.text == null){
+					if(response == null){
 						"1;"
 						//$(".chating-section ul").append("<li>"+"<p>"+"<img src={{URL('/')}}/public/Message_files/" +response.file_name+ ">"+"</p>"+"<h5>"+response.msg_date+"<h5>"+"</li>");
 					/*alert(response.text);*/

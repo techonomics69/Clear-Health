b406347fe7ab5ddfbabc567ac682ecb5cceb58ef
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-11 17:32:12 +0530

    Update Offers.php

--- app/Models/Offers.php
+++ app/Models/Offers.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Offers extends Model
 {
 	protected $table = 'offers';
-	protected $fillable = ['id','title','from_date','to_date','description','offer_type','percentage','amount','gift','addon','vehicle','promocode','promocode_type','promocode_value','promocode_description'];
+	protected $fillable = ['id','from_date','to_date','description','promocode','promocode_type','promocode_value'];
 }

22becd00c255537153475ff443a56b69a85860d5
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-11 17:00:07 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -776,7 +776,7 @@
 				success: function(response){
 					/*alert(response.file_path);*/
 					//$(".chating-section ul").append("<li>"+"<p>"+response.text+"</p>"+"<h5>"+response.msg_date+"<h5>"+"</li>");			
-					$(".chating-section ul").append("<li>"+"<p>"+"<img src='public/Message_files/'"+response.file_name+">"+"</p>"+"<h5>"+response.msg_date+"<h5>"+"</li>");						
+					$(".chating-section ul").append("<li>"+"<p>"+"<img src=public/Message_files/"+response.file_name+">"+"</p>"+"<h5>"+response.msg_date+"<h5>"+"</li>");						
 				}
 			});
 		});

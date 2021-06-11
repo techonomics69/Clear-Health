6adbaabffe59245c4f971ecc5556caafeb9879d4
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-11 18:10:11 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -774,12 +774,13 @@
       			processData: false,
       			contentType: false,
 				success: function(response){
-					if(response.text == null){
+					if(response != null){
+						$(".chating-section ul").append("<li>"+"<p>"+response.text+"</p>"+"<h5>"+response.msg_date+"<h5>"+"</li>");
 					/*alert(response.text);*/
 					//$(".chating-section ul").append("<li>"+"<p>"+response.text+"</p>"+"<h5>"+response.msg_date+"<h5>"+"</li>");			
-					$(".chating-section ul").append("<li>"+"<p>"+"<img src={{URL('/')}}/public/Message_files/" +response.file_name+ ">"+"</p>"+"<h5>"+response.msg_date+"<h5>"+"</li>");
+					
 				}else{
-					$(".chating-section ul").append("<li>"+"<p>"+response.text+"</p>"+"<h5>"+response.msg_date+"<h5>"+"</li>");
+					$(".chating-section ul").append("<li>"+"<p>"+"<img src={{URL('/')}}/public/Message_files/" +response.file_name+ ">"+"</p>"+"<h5>"+response.msg_date+"<h5>"+"</li>");
 				}
 			});
 		});

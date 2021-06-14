e4d4dc278cbafd6e7c4c8b624b6082c98a96b2e0
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-14 12:32:09 +0530

    Update create.blade.php

--- resources/views/offers/create.blade.php
+++ resources/views/offers/create.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 @section('title', 'clearHealth | Offers & Promotions')
 @section('content')
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js" type="text/javascript"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"
+type="text/javascript"></script>
+<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css"
+rel="Stylesheet"type="text/css"/>
 <div class="app-content content"> 
     @if (count($errors) > 0)
     <div class="alert alert-danger">

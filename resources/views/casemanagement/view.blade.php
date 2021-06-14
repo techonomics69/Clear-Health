24233b14426aa2fb7319beffdefe58780f0163cf
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-14 16:49:28 +0530

    get msg

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -603,6 +603,9 @@
 							<div class="right-cht">
 								{!! Form::open(array('method'=>'POST', 'enctype'=>"multipart/form-data", 'id'=>"msgForm")) !!}
 								<div class="chating-section">
+									@foreach ($message_details as $key => $message_data)
+									<?php print_r($message_data); ?>
+									@endforeach
 									<ul><?php if(isset($message_data)) {?>
 										@foreach ($message_data as $key => $message)
 										<li>

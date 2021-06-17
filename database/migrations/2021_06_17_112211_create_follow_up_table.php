b8ad823a965d94d217565f7d02549d3f9cbb5da9
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-17 17:01:52 +0530

    Create 2021_06_17_112211_create_follow_up_table.php

---
+++ database/migrations/2021_06_17_112211_create_follow_up_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFollowUpTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('follow_up', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('case_id');
+            $table->string('answer');
+            $table->string('left_face');
+            $table->string('right_face');
+            $table->string('center_face');
+            $table->string('back_photo');
+            $table->string('chest_photo');
+            $table->string('follow_up_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('follow_up');
+    }
+}

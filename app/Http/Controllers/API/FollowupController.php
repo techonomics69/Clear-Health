b50f110d01434a3baa4605881b14083e182593b6
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-18 12:31:58 +0530

    Update FollowupController.php

--- app/Http/Controllers/API/FollowupController.php
+++ app/Http/Controllers/API/FollowupController.php
@@ -129,7 +129,7 @@ class FollowupController extends BaseController
             return $this->sendError('Validation Error.', $validator->errors()->all());       
         }
 
-        $followUpAns = FollowUp::where('user_id', $data['user_id'])->where('case_id', $data['case_id'])->->where('follow_up_status','!=','completed')->first();
+        $followUpAns = FollowUp::where('user_id', $data['user_id'])->where('case_id', $data['case_id'])->where('follow_up_status','!=','completed')->first();
         if(!empty($followUpAns)):
            $followUpAns = $followUpAns->update($data);
        endif;

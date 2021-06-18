d333e05e3891a1ec0ebb6fcd555de2b612ff12c2
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-18 13:52:16 +0530

    Update FollowupController.php

--- app/Http/Controllers/API/FollowupController.php
+++ app/Http/Controllers/API/FollowupController.php
@@ -253,7 +253,7 @@ class FollowupController extends BaseController
 
         }
 
-        $followUpAns = FollowUp::where([['user_id', $user_id],['case_id', $case_id],['follow_up_status','<>','completed'],['follow_up_no',$data['follow_up_no']]])->first();
+        $followUpAns = FollowUp::where([['user_id', $user_id],['case_id', $case_id],['follow_up_no',$data['follow_up_no']]])->first();//['follow_up_status','<>','completed'],
 
         echo "<pre>";
         print_r($followUpAns);

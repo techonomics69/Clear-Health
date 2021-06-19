f5b296704d220052ce507e32efb18a271ce73bf9
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-19 12:56:50 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -638,7 +638,7 @@
 										
 		<li class = <?php if($message['sender'] == 'admin') { ?>"right"<?php }else{ ?>
 		"left" <?php } ?>>
-											<p id="<?php if($key == count($message_data) - 1) echo 'bottomDivMsg{{$message['id']}}' ?>">
+											<p id="<?php if($key == count($message_data) - 1) echo 'bottomDivMsg'.$message['id'] ?>">
 												<?php 
 												if(isset($message['message']) && $message['message']!=''){
 													echo $message['message'];

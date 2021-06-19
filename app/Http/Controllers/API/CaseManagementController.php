753e7e0c3e779dc9462710e13b76b12287b4f4ba
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-19 15:57:15 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/API/CaseManagementController.php
+++ app/Http/Controllers/API/CaseManagementController.php
@@ -1145,6 +1145,12 @@ public function createMessage(Request $request){
   $case_id = $request['case_id'];
   $md_case_id = $request['md_case_id'];
 
+  $gender = User::where('id',$user_id)->pluck('gender');
+  echo "<pre>";
+  print_r($gender);
+  echo "<pre>";
+  exit(); 
+
   $update_data = array();
 
   if(isset($request['abstinence_form'])){

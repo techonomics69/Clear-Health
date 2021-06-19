fe67922049e19efdfec66d01d57840ad5ec99089
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-19 17:32:32 +0530

    again put removed migration files

---
+++ database/migrations/2021_06_17_112211_create_follow_up_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFollowUpTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('follow_up', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('case_id');
+            $table->string('md_case_id')->nullable();
+            $table->longtext('answer')->nullable();
+            $table->string('left_face')->nullable();
+            $table->string('right_face')->nullable();
+            $table->string('center_face')->nullable();
+            $table->string('back_photo')->nullable();
+            $table->string('chest_photo')->nullable();
+            $table->string('follow_up_no')->nullable();
+            $table->text('pregnancy_test')->nullable();
+            $table->string('follow_up_status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('follow_up');
+    }
+}

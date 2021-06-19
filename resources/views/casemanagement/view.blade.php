e42f02b2b3ff1578edbe493e9e441851b7f237c3
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-19 12:18:07 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -790,9 +790,9 @@
 					$('#text').val('');
 					$('#file').val('');
 					if(response.text == null){ 
-						$(".chating-section ul").append("<li class='right'>"+"<p id='bottomDivMsg'>"+"<img width='100' src={{URL('/')}}/public/Message_files/" +response.file_name+ ">"+ "<a target='_blank' download='' href={{URL('/')}}/public/Message_files/"+response.file_name+">" + " Download" + "</a>"+"</p>"+"<h5>"+response.msg_date+"<h5>"+"</li>");
+						$(".chating-section ul").append("<li class='right'>"+"<p>"+"<img width='100' src={{URL('/')}}/public/Message_files/" +response.file_name+ ">"+ "<a target='_blank' download='' href={{URL('/')}}/public/Message_files/"+response.file_name+">" + " Download" + "</a>"+"</p>"+"<h5>"+response.msg_date+"<h5>"+"</li>");
 					}else{
-						$(".chating-section ul").append("<li class='right'>"+"<p id='bottomDivMsg'>"+response.text+"</p>"+"<h5>"+response.msg_date+"<h5>"+"</li>");	
+						$(".chating-section ul").append("<li class='right'>"+"<p>"+response.text+"</p>"+"<h5>"+response.msg_date+"<h5>"+"</li>");	
 					}
 					$("#chating-section").scrollTop($("#chating-section").get(0).scrollHeight);
 				}

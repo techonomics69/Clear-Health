7ef383eebff4011ae26755a64f0a9ac41438d6ab
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-21 13:01:26 +0530

    Update helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -823,6 +823,10 @@ if(!empty($Patient_data)){
 
     function getAssignedIpledgeIdToUser($user_id,$case_id,$md_case_id){
          $assigned_ipledge_id = CaseManagement::select('ipledge_id')->where([['id',$case_id],['user_id',$user_id],['md_case_id',$md_case_id]])->toSql();
+         echo "<pre>";
+         print_r($assigned_ipledge_id);
+         echo "<pre>";
+         exit();
          $assigned_ipledge_id = $assigned_ipledge_id['ipledge_id'];
 
          return $assigned_ipledge_id;

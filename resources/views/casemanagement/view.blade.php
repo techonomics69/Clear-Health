c2f1716f741d8578d96cbfd267c231e3769ffa1b
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-21 17:31:09 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -854,43 +854,46 @@
 	var exts = ['jpg','jpeg','png'];
 	
 	var fname = event.target.files[0].name;
-	var fileNameExt = fileName.substr(fileName.lastIndexOf('.') + 1);
+	var fileNameExt = fileName.substr(fname.lastIndexOf('.') + 1);
 	var reader = new FileReader();
 	if ($.inArray(fileNameExt, exts) == -1){
 		reader.onload = function(){
       	var output = document.getElementById('blah');
       	output.src = reader.result;
     	};
+		reader.readAsDataURL(event.target.files[0]);
 	}else if(fileNameExt == 'doc'){
 		reader.onload = function(){
       	var output = document.getElementById('blah');
       	output.src = "{{ asset('public/images/msgs/doc.png') }}";
     	};
+		reader.readAsDataURL(event.target.files[0]);
 	}else if(fileNameExt == 'pdf'){
 		reader.onload = function(){
       	var output = document.getElementById('blah');
       	output.src = "{{ asset('public/images/msgs/pdf.png') }}";
     	};
+		reader.readAsDataURL(event.target.files[0]);
 	}else if(fileNameExt == 'txt'){
 		reader.onload = function(){
       	var output = document.getElementById('blah');
       	output.src = "{{ asset('public/images/msgs/txt.png') }}";
     	};
+		reader.readAsDataURL(event.target.files[0]);
 	}else if(fileNameExt == 'xls'){
 		reader.onload = function(){
       	var output = document.getElementById('blah');
       	output.src = "{{ asset('public/images/msgs/xls.png') }}";
     	};
+		reader.readAsDataURL(event.target.files[0]);
 	}else{
 		reader.onload = function(){
       	var output = document.getElementById('blah');
       	output.src = "{{ asset('public/images/msgs/file.png') }}";
     	};
+		reader.readAsDataURL(event.target.files[0]);
 	}	
-		
-    
-    
-    reader.readAsDataURL(event.target.files[0]);
+	
 	$("#blah").show();
   };
 

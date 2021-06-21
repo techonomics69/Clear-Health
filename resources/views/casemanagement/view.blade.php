6cc1ec170e461e7d01f8439c2b9b3e898f46b758
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-21 15:50:45 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -666,8 +666,13 @@
 								</ul>
 
 							</div>
-							<img id="blah" src="#" alt="your image" style="display: none; height: 120px;
-				width: 250px;" />
+							<div class="row">
+								<div class="col-lg-12">
+									<img id="blah" src="#" alt="your image" style="display: none; height: 120px;
+									width: 250px;" />
+								</div>
+							</div>
+							
 							<div id="last-typing-section" class="last-typing-section">
 <!-- <div class="camera lastimg">
 <img src="{{asset('public/images/camera.png')}}" alt="">

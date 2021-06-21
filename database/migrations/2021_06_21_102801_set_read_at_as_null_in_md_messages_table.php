e1d3ebd72f8e03f78a1615107568609ddf9ec127
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-21 15:59:19 +0530

    update migration

--- database/migrations/2021_06_21_102801_set_read_at_as_null_in_md_messages_table.php
+++ database/migrations/2021_06_21_102801_set_read_at_as_null_in_md_messages_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class SetReadAtAsNullInCheckoutField extends Migration
+class SetReadAtAsNullInMdMessagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class SetReadAtAsNullInCheckoutField extends Migration
      */
     public function up()
     {
-        Schema::table('checkout_field', function (Blueprint $table) {
-            //
+        Schema::table('md_messages', function (Blueprint $table) {
+             $table->datetime('read_at')->nullable()->change();
         });
     }
 
@@ -25,7 +25,7 @@ class SetReadAtAsNullInCheckoutField extends Migration
      */
     public function down()
     {
-        Schema::table('checkout_field', function (Blueprint $table) {
+        Schema::table('md_messages', function (Blueprint $table) {
             //
         });
     }

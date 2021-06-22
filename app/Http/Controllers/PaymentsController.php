6d9ef579c645cbbac6468bcf64c3e0854b826a32
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-22 17:10:23 +0530

    Update PaymentsController.php

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -49,13 +49,9 @@ class PaymentsController extends Controller
         ]);
 
         /** I have hard coded amount. You may fetch the amount based on customers order or anything */
-        $amount     = 30;
+        $amount     = 1 * 100;
         $currency   = 'usd';
 
-       /* echo "<pre>";
-        print_r(request('stripeToken'));
-        echo "<pre>";
-        exit();*/
         if (empty(request()->get('stripeToken'))) {
             session()->flash('error', 'Some error while making the payment. Please try again');
             return back()->withInput();
@@ -92,11 +88,6 @@ class PaymentsController extends Controller
             if (empty($apiError) && $charge) {
                 // Retrieve charge details 
                 $paymentDetails = $charge->jsonSerialize();
-
-                echo "<pre>";
-                print_r($paymentDetails);
-                echo "<pre>";
-                exit();
                 if ($paymentDetails['amount_refunded'] == 0 && empty($paymentDetails['failure_code']) && $paymentDetails['paid'] == 1 && $paymentDetails['captured'] == 1) {
                     /** You need to create model and other implementations */
                     /*

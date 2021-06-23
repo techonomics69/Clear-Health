04f5b4be65d809bacf7fd1ba57472755e0ca5b0b
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-23 13:01:50 +0530

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -928,7 +928,7 @@ data-open="click" data-menu="vertical-menu" data-col="2-columns">
                 <div class="container-fluid p-0">
                   @yield('content')
                 </div>
-                <div class="loader">Loading...</div>      
+                 
                 <footer class="footer footer-static footer-light navbar-border">
                   <p class="clearfix blue-grey lighten-2 text-sm-center text-center mb-0 px-2">
                     <span class=" d-block d-md-inline-block">Copyright &copy; <?php echo date("Y"); ?> <a class="text-bold-800 grey darken-2" href="{{ route('home.dashboard') }}"

adf1d762cddce298736fd44a13b2feda24150332
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-23 12:46:54 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -631,7 +631,7 @@
 							@endif
 							<div class="right-cht">
 								{!! Form::open(array('method'=>'POST', 'enctype'=>"multipart/form-data", 'id'=>"msgForm")) !!}
-								<div class="chating-section nonmedicalmessages" id="chating-section" style="overflow: scroll;">
+								<div class="chating-section nonmedicalmessages" id="chating-section" >
 									<ul><?php
 										if(isset($message_data)) {?>
 										@foreach ($message_data as $key => $message)

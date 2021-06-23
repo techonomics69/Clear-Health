5b9d803f6e62da580f51de9aa2229ba20490cb7d
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-23 11:01:06 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -923,7 +923,12 @@
 		if (bytes === 0) return 'n/a'
 		const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10)
 		if (i === 0) return `${bytes} ${sizes[i]})`
-		return `${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]}`
+		var sizeArr = {
+			size : (bytes / (1024 ** i)).toFixed(1),
+			sizeIn :  sizes[i]
+		};
+		return $sizeArr;
+		// return `${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]}`
 	}
 
 	var loadFile = function(event) {

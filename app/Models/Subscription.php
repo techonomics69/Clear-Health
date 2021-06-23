7779743fbbcc513d7619d7037ea4e3bbd7e5f7ce
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-23 17:43:26 +0530

    Update Subscription.php

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Subscription extends Model
 {
 	protected $table = 'subscription';
-	protected $fillable = ['id','user_id','case_id','md_case_id','subscr_id','customer','plan_id','plan_amount','plan_currency','plan_interval','plan_interval_count','created','current_period_start','current_period_end','status'];
+	protected $fillable = ['id','user_id','case_id','md_case_id','subscr_id','customer','plan_id','plan_amount','plan_currency','plan_interval','plan_interval_count','created','current_period_start','current_period_end','status','product_id'];
 }

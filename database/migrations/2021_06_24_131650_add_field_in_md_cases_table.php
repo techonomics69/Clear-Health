c89afe28955fb1a64106149c619f4d8fc1403d0b
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-24 19:05:02 +0530

    Create 2021_06_24_131650_add_field_in_md_cases_table.php

---
+++ database/migrations/2021_06_24_131650_add_field_in_md_cases_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFieldInMdCasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('md_cases', function (Blueprint $table) {
+            $table->string('case_status_reason')->after('status');
+            $table->string('case_status_updated_at')->after('case_status_reason');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('md_cases', function (Blueprint $table) {
+            //
+        });
+    }
+}

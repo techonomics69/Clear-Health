0fc8483e71140776e89d9e82f96c878a41d54b6c
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-25 16:10:45 +0530

    Update QuizController.php

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -31,12 +31,14 @@ class QuizController extends Controller
     public function index(Request $request)
     {
         $category = QuizCategory::pluck('name', 'id')->toArray();
-        $request->session()->put('activequiz', 'questions');
+        
         foreach ($category as $key => $value) {
             $quiz[$key][] = Quiz::where('category_id', $key)->OrderBy('id', 'ASC')->get();
             $quizOrder[$key] = Quiz::where('category_id', $key)->OrderBy('id', 'ASC')->get()->pluck('order')->toArray();
         }
+        $request->session()->put('activequiz', $category[0]);
         // dd(session('activequiz'));
+
         return view('quiz.index', compact('quiz','category','quizOrder'))->with('i', ($request->input('page', 1) -1) * 5);
     }
 

438a8c769544a32502641f2e9cad7a56dd6bc25b
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-25 15:25:42 +0530

    Update create.blade.php

--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -75,13 +75,13 @@
                                  <div class="col-lg-4 col-md-6 col-sm-6">
                                     <div class="form-group">
                                         <label>Password</label><span class="required">*</span>
-                                        {!! Form::text('passwords', null, array('type'=>'password','placeholder' => 'Password','class' => 'form-control', 'maxlength' => "15","id"=>"password")) !!}
+                                        {!! Form::password('passwords', null, array('placeholder' => 'Password','class' => 'form-control', 'maxlength' => "15","id"=>"password")) !!}
                                     </div>
                                 </div>
                                 <div class="col-lg-4 col-md-6 col-sm-6">
                                     <div class="form-group">
                                         <label>Confirm Password</label><span class="required">*</span>
-                                        {!! Form::text('confirm-password', null, array('type'=>'password','placeholder' => 'Password','class' => 'form-control', 'maxlength' => "15","id"=>"confirm_password")) !!}
+                                        {!! Form::password('confirm-password', null, array('placeholder' => 'Password','class' => 'form-control', 'maxlength' => "15","id"=>"confirm_password")) !!}
                                     </div>
                                 </div>
                             </div>

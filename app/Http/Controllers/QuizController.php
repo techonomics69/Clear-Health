34864a4de30d0a96b55bc46e09153d62c982834b
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-25 16:29:27 +0530

    Update QuizController.php

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -61,6 +61,7 @@ class QuizController extends Controller
                 $request->session()->put('activequiz', session('activequiz'));
             }
         }
+        echo session('activequiz');
     }
 
     /**

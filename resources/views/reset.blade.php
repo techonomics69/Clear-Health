ce9276b153270cb9adca31c816fc3b3e91d912b8
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-25 17:42:13 +0530

    Update reset.blade.php

--- resources/views/reset.blade.php
+++ resources/views/reset.blade.php
@@ -123,6 +123,7 @@ data-open="click" data-menu="vertical-menu" data-col="1-column">
 </div>
 </div>
 </div>
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 <script>
    $(document).on('click', '#userSubmit', function() {
       alert();

28ccd8f2ea208609b89e440a15a3c4bd99b98d7c
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-25 10:59:29 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -190,7 +190,7 @@
 											@foreach($category as $key => $data)
 											{{-- <li><a class="btn @if($key == 0) active @elseif($current_tab_id == 'home'.$key) active @endif" data-toggle="tab" href="#home{{$key}}">{{$data}}</a></li> --}}
 
-											<li><a class="btn active " click="setheadertab()"  data-toggle="tab" href="#home{{$key}}">{{$data}}</a></li>
+											<li><a class="btn @if($key == 0) active @elseif($current_tab_id == 'home'.$key) active @endif" data-toggle="tab" href="#home{{$key}}">{{$data}}</a></li>
 											<?php //$activeTab++ ?> 
 											@endforeach
 										</ul>
@@ -1134,10 +1134,6 @@
 	
 	$("#blah").show();
   };
-
-  function setheadertab(){
-	  alert();
-  }
 
 </script>
 

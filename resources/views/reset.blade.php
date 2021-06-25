a187b09a27e4e88397a3ad4da6d32d3130604697
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-25 17:54:34 +0530

    Update reset.blade.php

--- resources/views/reset.blade.php
+++ resources/views/reset.blade.php
@@ -70,7 +70,7 @@ data-open="click" data-menu="vertical-menu" data-col="1-column">
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right" style="color: #fff">E-Mail Address</label>
                           <div class="col-md-6">
-                                <input id="email" input="resetValue('email')" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" autocomplete="email" autofocus>
+                                <input id="email" oninput="resetValue('email')" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -87,7 +87,7 @@ data-open="click" data-menu="vertical-menu" data-col="1-column">
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right" style="color: #fff">Password</label>
                             <div class="col-md-6">
-                                <input id="password" input="resetValue('password')" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="new-password">
+                                <input id="password" oninput="resetValue('password')" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="new-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -109,7 +109,7 @@ data-open="click" data-menu="vertical-menu" data-col="1-column">
                       <div class="form-group row">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right" style="color: #fff">Confirm Password</label>
                             <div class="col-md-6">
-                                <input id="password-confirm" input="resetValue('confirmpass')" type="password" class="form-control" name="password_confirmation" autocomplete="new-password">
+                                <input id="password-confirm" oninput="resetValue('confirmpass')" type="password" class="form-control" name="password_confirmation" autocomplete="new-password">
                                 @error('password_confirmation')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>

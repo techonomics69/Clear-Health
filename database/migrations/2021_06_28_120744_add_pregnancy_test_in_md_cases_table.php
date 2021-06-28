46431baf0a8f5ed3576be7e44567722a9533fe8f
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-28 17:40:31 +0530

    Create 2021_06_28_120744_add_pregnancy_test_in_md_cases_table.php

---
+++ database/migrations/2021_06_28_120744_add_pregnancy_test_in_md_cases_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPregnancyTestInMdCasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('md_cases', function (Blueprint $table) {
+            $table->string('pregnancy_test')->default('0')->after('case_type')->comment = '0=pending,1=verified';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('md_cases', function (Blueprint $table) {
+            //
+        });
+    }
+}

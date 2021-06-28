84ca458717144ac9822969e59b8fca292cca49c7
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-28 13:41:20 +0530

    Update CaseStatusUpdateGetPrescriptionController.php

--- app/Http/Controllers/CaseStatusUpdateGetPrescriptionController.php
+++ app/Http/Controllers/CaseStatusUpdateGetPrescriptionController.php
@@ -458,14 +458,7 @@ class CaseStatusUpdateGetPrescriptionController extends Controller
             $input_prescription['user_id'] = $user_id;
             $input_prescription['system_case_id'] = $system_case_id;
 
-
-
-              echo "<pre>";
-              print_r($input_prescription);
-              echo "<pre>";
-              exit();
-
-            //$CasePrescription_data = CasePrescriptions::create($input_prescription);
+            $CasePrescription_data = CasePrescriptions::create($input_prescription);
 
             if(isset($prescription->medication) && !empty($prescription->medication)){
               $input_medication['case_prescription_id'] = $CasePrescription_data->id;

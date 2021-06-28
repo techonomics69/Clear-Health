0f20b945ad175097214fc19444cabc7a054ce9a5
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-28 18:58:07 +0530

    Added helper

---
+++ app/Helper/shipStationHelper.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Helper;
+
+class shipStationHelper {
+
+
+    public static function createOrder($orderData){
+
+    }
+
+    public static function getOrderData($orderId){
+        
+    }
+
+}
+
+?>
--- config/shipstation.php
+++ config/shipstation.php
@@ -2,7 +2,7 @@
 
 return [
     'apiURL'        => env('SS_URL', 'https://ssapi.shipstation.com'),
-    'apiKey'        => env('SS_KEY', ''),
+    'apiKey'        => env('SS_KEY', 'fe52233fee7a48f0899535e50ea6336c'),
     'partnerApiKey' => env('SS_PARTNER_KEY', ''),
-    'apiSecret'     => env('SS_SECRET', ''),
+    'apiSecret'     => env('SS_SECRET', '5370fac2587a433c9e12bc2a91d85c2a'),
 ];

a1cb3702f880f21cc1900897a2d096fae5fff863
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-29 15:41:37 +0530

    Update MDWebhooksController.php

--- app/Http/Controllers/API/MDWebhooksController.php
+++ app/Http/Controllers/API/MDWebhooksController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\API;
 use Illuminate\Http\Request;
 use App\Http\Controllers\API\BaseController as BaseController;
 use Validator;
-
    
 class MdwebhooksController extends BaseController
 {

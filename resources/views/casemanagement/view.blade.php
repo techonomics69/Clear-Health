37592b1f4f511531e329bf789b13a34928edf12b
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-29 13:12:53 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -1270,7 +1270,7 @@
 			"oSearch": { "bSmart": false, "bRegex": true },
 			"scrollX": true,
 		});
-		console.log('window.location.pathname', window.location.hash);
+		console.log('window.location.pathname', window.location.pathname);
 		$('#action_item').trigger('click')
 	});
 

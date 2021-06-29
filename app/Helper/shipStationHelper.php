3720b4ba469a10a57f9bb409e09a8884a249c756
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-29 13:59:49 +0530

    Update shipStationHelper.php

--- app/Helper/shipStationHelper.php
+++ app/Helper/shipStationHelper.php
@@ -15,7 +15,7 @@ class shipStationHelper {
     }
         
     public static function createOrder($orderData){
-        $InitializeHelper = $this->InitializeHelper();
+        $InitializeHelper = shipStationHelper::InitializeHelper();
         return (isset($testm)) ? $testm : 'none';
     }
 

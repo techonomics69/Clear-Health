35ffb56425d4b748ab28a94905223fc7fa4f7b4b
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-29 18:26:57 +0530

    Update shipStationHelper.php

--- app/Helper/shipStationHelper.php
+++ app/Helper/shipStationHelper.php
@@ -48,7 +48,7 @@ class shipStationHelper {
                         // array_push($getitems, $arr1);
                         $item->name = $getproducts[0]->name;
                         $item->quantity = $getproducts[0]->prod_qty;
-                        $item->unitPrice  = $getproducts[0]->prod_price;
+                        $item->unitPrice  = ($getproducts[0]->prod_price!='') ? $getproducts[0]->prod_price : '0';
                         $item->warehouseLocation = 'Nefaire 141 Post Road East Westport, CT 06880';
                         $item->imageUrl = asset(config('filesystems.products.imageurl').''.$getproducts[0]->image_detail);
                     }

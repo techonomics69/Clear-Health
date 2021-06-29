3ac6ce23b5d90a1dbcc32a696fc7f9e2335e0025
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-29 19:43:15 +0530

    Update shipStationController.php

--- app/Http/Controllers/shipStationController.php
+++ app/Http/Controllers/shipStationController.php
@@ -7,5 +7,10 @@ use App\Helper\shipStationHelper;
 
 class shipStationController extends Controller
 {
+    public function getShiprates(Request $request){
+        $getRates = shipStationHelper::getRates($request->all());
+        return $getRates;
+    }
+
     
 }

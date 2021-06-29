037cb9abf27c100b8304e365935678fcd5f9f659
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-29 15:49:56 +0530

    Create MdwebhooksController.php

---
+++ app/Http/Controllers/API/MdwebhooksController.php
@@ -0,0 +1,25 @@
+<?php
+   
+namespace App\Http\Controllers\API;
+   
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use Validator;
+   
+class MdwebhooksController extends BaseController
+{
+    public function index()
+    {
+        die("innn");
+    }
+
+    public function show()
+    {
+       //
+    }
+    
+    public function store()
+    {
+       //
+    }
+}

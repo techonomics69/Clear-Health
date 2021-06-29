9272c27d55b64d57b8df7c7297a94d62755f3acf
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-29 14:55:03 +0530

    Update shipStationHelper.php

--- app/Helper/shipStationHelper.php
+++ app/Helper/shipStationHelper.php
@@ -16,6 +16,42 @@ class shipStationHelper {
         
     public static function createOrder($orderData){
         $InitializeHelper = shipStationHelper::InitializeHelper();
+
+        $address = new LaravelShipStation\Models\Address();
+	    
+        $address->name = $orderData['patient_firstname']." ".$orderData['patient_lastname'];
+	    $address->street1 = $orderData['addressline1']." ".$orderData['addressline2'];
+	    $address->city = $orderData['city'];
+	    $address->state = $orderData['state'];
+	    $address->postalCode = $orderData['zipcode'];
+	    $address->country = "US";
+	    $address->phone = $orderData['phone'];
+
+        $products = $orderData['cart_id'];
+
+        
+
+        // $item = new LaravelShipStation\Models\OrderItem();
+
+	    // $item->lineItemKey = '1';
+	    // $item->sku = '580123456';
+	    // $item->name = "Awesome sweater.";
+	    // $item->quantity = '1';
+	    // $item->unitPrice  = '29.99';
+	    // $item->warehouseLocation = 'Warehouse A';
+
+        // $order = new LaravelShipStation\Models\Order();
+	    // $order->orderNumber = $orderData['order_id'];
+	    // $order->orderDate = date("Y-m-d");
+	    // $order->orderStatus = 'awaiting_shipment';
+	    // $order->amountPaid = $orderData['total_amount'];
+	    // $order->taxAmount = $orderData['tax'];
+	    // $order->shippingAmount = $orderData['shipping_fee'];
+	    // $order->internalNotes = '';
+	    // $order->billTo = $address;
+    	// $order->shipTo = $address;
+    	// $order->items[] = $item;
+
         return (isset($testm)) ? $testm : 'none';
     }
 

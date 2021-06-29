7c7a46acd6bbd81255b9fe91b6c0b0e1773c7693
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-29 12:52:09 +0530

    Update shipStationHelper.php

--- app/Helper/shipStationHelper.php
+++ app/Helper/shipStationHelper.php
@@ -1,16 +1,14 @@
 <?php
 namespace App\Helper;
-
 use GuzzleHttp\Guzzle;
 use LaravelShipStation;
 use LaravelShipStation\ShipStation;
 use Illuminate\Support\Facades\App;
 
 class shipStationHelper {
-
-
-    public static function createOrder($orderData){
         
+    public static function createOrder($orderData){
+
     }
 
     public static function getOrderData($orderId){

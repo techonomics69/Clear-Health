a0c8412b2c483576dd8c48a60678783721f81ee4
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-29 15:22:14 +0530

    updated code

--- app/Http/Controllers/API/MDWebhooksController.php
+++ app/Http/Controllers/API/MDWebhooksController.php
@@ -1,57 +1,26 @@
 <?php
-
+   
 namespace App\Http\Controllers\API;
-
-use App\Http\Controllers\API\BaseController as BaseController;
+   
 use Illuminate\Http\Request;
-use App\Models\CaseManagement;
-use App\Models\Mdpatient;
+use App\Http\Controllers\API\BaseController as BaseController;
 use Validator;
-use Exception;
-use App\Models\User;
-use App\Models\Mdcases;
-use App\Models\Mdmanagement;
-use App\Models\CaseFiles;
-use App\Models\MdMessages;
-use App\Models\MdMessageFiles;
-use App\Models\MessageFiles;
-use App\Models\Messages;
-use App\Models\FollowUp;
-use Carbon\Carbon;
 
+   
 class MdwebhooksController extends BaseController
 {
-   
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        echo "<pre>";
-        print_r('hello');
-        echo "<pre>";
-        exit();
+        die("innn");
     }
 
-    public function create()
+    public function show()
     {
-        //
-    }
-
-    public function store()
-    {
-
-    }
-
-     public function show()
-    {
-
-    }
-
-    public function edit()
-    {
-        //
-    }
-
-    public function update()
-    {
-        //
+       //
     }
 }

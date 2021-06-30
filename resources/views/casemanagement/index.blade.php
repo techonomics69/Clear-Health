267d04776541fed3671d0f7b9bdd48f5d9d3e2ce
SEFALI <deval>
2021-06-30 17:30:27 +0530

    changes

--- resources/views/casemanagement/index.blade.php
+++ resources/views/casemanagement/index.blade.php
@@ -162,7 +162,7 @@
   jQuery(document).ready(function($) {
     $('#CaseManagementList').DataTable({
       "dom": '<"top"if>rt<"bottom"lp><"clear">',
-      fixedHeader: true
+  
     });
 
     /*$.ajax({

7e8a65769ee90c4ebd501bcdda4365e4e9599021
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-30 11:55:00 +0530

    update

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -568,25 +568,26 @@
 														<p class="heading_text ">iPledge Credentials</p>
 
 
-														<form action="{{ route('saveipledge') }}" method="POST" enctype="multipart/form-data" id="storeCustomer">
+														<form action="{{ route('saveipledge') }}" method="POST">
 															@csrf
 															<div class="form-group row">
 																<label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
 																<div class="col-sm-10">
-																	<input type="email" class="form-control" id="inputEmail3">
+																	<input type="email" class="form-control" name="email">
 																</div>
 															</div>
 
 															<div class="form-group row">
 																<label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
 																<div class="col-sm-10">
-																	<input type="password" class="form-control" id="inputPassword3">
+																	<input type="password" class="form-control" name="password">
+																	<input type="hidden" name="case_id" value="">
 																</div>
 															</div>
 
 														</form>
 														<div class="ipledge_button">
-															<a class="btn btn-secondry " href=""> Save</a>
+															<button class="btn btn-secondry" type="submit"> Save</button>
 															<a class="btn btn-primary " href=""> Send SMS</a>
 														</div>
 													</div>

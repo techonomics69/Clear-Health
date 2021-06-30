261cd86c3e80cf705b7658e83b16ea2c29e8b8cf
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-30 16:18:20 +0530

    Update shipStationHelper.php

--- app/Helper/shipStationHelper.php
+++ app/Helper/shipStationHelper.php
@@ -66,9 +66,10 @@ class shipStationHelper {
         $order->advancedOptions = array('storeId'=>'457183');
 
         $newOrder = $shipStation->orders->create($order);
+        $getOrder = json_decode($newOrder);
         //$updateOrder = DB::table('checkout')->where('id',$order['checkoutOrderId'])->update(['shipstation_order_id'=>$newOrder->orderId]);
 
-        return (isset($newOrder)) ? $newOrder['orderId'] : 'none';
+        return (isset($getOrder)) ? $getOrder : 'none';
     }
 
     public static function createOrder_prescribed($orderData){

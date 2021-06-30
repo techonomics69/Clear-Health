b9a65e404b6faa85219e0d22bc790a8b198ab83a
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-30 18:45:31 +0530

    Update Mdcases.php

--- app/Models/Mdcases.php
+++ app/Models/Mdcases.php
@@ -11,7 +11,7 @@ class Mdcases extends Model
     protected $table = 'md_cases';
     protected $fillable = ['prioritized_at', 'prioritized_reason', 'cancelled_at', 'md_created_at', 'support_reason', 'case_id', 'status','case_status_reason','case_status_updated_at','user_id','system_case_id'];
 
-    public function post()
+    public function caseManagement()
 	{
 	    return $this->belongsTo('App\Models\CaseManagement','system_case_id');
 	}

c971e72991f7630bf917e15c36f556de11007346
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-06-30 18:51:27 +0530

    add relation between models

--- app/Models/Mdcases.php
+++ app/Models/Mdcases.php
@@ -11,8 +11,8 @@ class Mdcases extends Model
     protected $table = 'md_cases';
     protected $fillable = ['prioritized_at', 'prioritized_reason', 'cancelled_at', 'md_created_at', 'support_reason', 'case_id', 'status','case_status_reason','case_status_updated_at','user_id','system_case_id'];
 
-    public function caseManagement()
+    /*public function caseManagement()
 	{
 	    return $this->belongsTo('App\Models\CaseManagement','system_case_id');
-	}
+	}*/
 }

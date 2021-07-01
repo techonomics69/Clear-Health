066153c8eb5f2eec236aba2536c76c0a009290c8
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-01 16:14:13 +0530

    Update FollowupController.php

--- app/Http/Controllers/API/FollowupController.php
+++ app/Http/Controllers/API/FollowupController.php
@@ -108,7 +108,9 @@ class FollowupController extends BaseController
       $destinationPath = public_path('/images/Users');
       $userGender = User::find($user_id)->gender;
       $followUpAns = FollowUp::where([['user_id', $user_id], ['case_id', $case_id], ['follow_up_no', $data['follow_up_no']], ['follow_up_status', '<>', 'completed']])->first();
-      dd($followUpAns);
+      echo '<pre>';
+      print_r($followUpAns);
+      die;
       if ($request['left_face'] != '') {
         $left_face = $request['left_face'];
         $left_face_file_name =  $user_id . '_left_face_' . time() . '.jpeg';

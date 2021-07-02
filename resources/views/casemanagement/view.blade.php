22cd24957baae18139839e268ada4b1213b28c37
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-02 15:11:43 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -785,11 +785,11 @@
 
 													@if($user_case_management_data->verify_prior_auth && $user_case_management_data->ipledge_items):
 													<div class="ipledge_button">
-														<button class="btn btn-secondry" type="submit"> Verify</button>
+														<button class="btn btn-secondry" type="button"> Verified</button>
 													</div>
 													@else
 													<div class="ipledge_button">
-														<button class="btn btn-secondry" type="button"> Verified</button>
+														<button class="btn btn-secondry" type="submit"> Verify</button>
 													</div>
 													@endif
 													{!! Form::close() !!}

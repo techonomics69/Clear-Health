e862d491f830af1ab9ef23913d3c71fc56d4d5b1
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-02 18:56:03 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/CaseManagementController.php
+++ app/Http/Controllers/CaseManagementController.php
@@ -158,7 +158,10 @@ class CaseManagementController extends Controller
           $response = curl_exec($curl);
           curl_close($curl);
           $response1 = json_decode($response);
-          $skincare_summary['pharmacy_pickup'] =  $response1->name;
+          if($response1>0){
+            $skincare_summary['pharmacy_pickup'] =  $response1->name;  
+          }
+          // $skincare_summary['pharmacy_pickup'] =  $response1->name;
         } else {
           $skincare_summary['pharmacy_pickup'] = 'Clear Health Pharmacy Network';
         }

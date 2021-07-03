04b3a276e5476a704614562935d305eb90202554
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-03 13:45:29 +0530

    Update index.blade.php

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -60,7 +60,7 @@
             </div> -->
             <div class="row" style="padding: 20px;">
                 <div class="col-md-12">
-            <table class="table  table-responsive-md table-striped table-bordered" style="width:100%" id="productsList">
+            <table class="table  table-responsive table-striped table-bordered" style="width:100%" id="productsList">
                 <thead>
                 <tr>
                     <th width="60px">No</th>

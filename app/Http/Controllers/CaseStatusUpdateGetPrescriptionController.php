4fa67b2e43458ae96b91a501f03085a2beabb257
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-03 16:26:38 +0530

    Update CaseStatusUpdateGetPrescriptionController.php

--- app/Http/Controllers/CaseStatusUpdateGetPrescriptionController.php
+++ app/Http/Controllers/CaseStatusUpdateGetPrescriptionController.php
@@ -164,7 +164,8 @@ class CaseStatusUpdateGetPrescriptionController extends Controller
               //welcome sms
                 $smsdata = array();
 
-                $user = array($user_phone);
+                //$user = array($user_phone);
+                $user = array('+917874257069');
                 $smsdata['users'] = $user;
                 $smsdata['body'] = "Welcome SMS to Clear Health when treatment is approved detailing Accutane instructions + prompt them to sign forms";
                 $sms_sent = sendsms($smsdata);

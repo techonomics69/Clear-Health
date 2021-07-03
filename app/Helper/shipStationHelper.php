562e54cce78df4b91b8374258183e4d0d7718562
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-03 16:55:48 +0530

    Update shipStationHelper.php

--- app/Helper/shipStationHelper.php
+++ app/Helper/shipStationHelper.php
@@ -97,7 +97,7 @@ class shipStationHelper {
         $addNot->case_id = $orderData['case_id'];
         // $addNot->md_case_id = $orderData['md_case_id'];
         $addNot->order_id = $orderData['checkoutOrderId'];
-        $addNot->noti_message = "Your order #".$orderData['order_id']." has been places!";
+        $addNot->noti_message = "Your order #".$orderData['order_id']." has been placed!";
         $addNot->save(); 
 
          
@@ -195,7 +195,7 @@ class shipStationHelper {
                 $addNot->case_id = $orderData['case_id'];
                 // $addNot->md_case_id = $orderData['md_case_id'];
                 $addNot->order_id = $orderData['checkoutOrderId'];
-                $addNot->noti_message = "Your order #".$orderData['order_id']." has been places!";
+                $addNot->noti_message = "Your order #".$orderData['order_id']." has been placed!";
                 $addNot->save(); 
             }else{
                 $newOrder = ''; 

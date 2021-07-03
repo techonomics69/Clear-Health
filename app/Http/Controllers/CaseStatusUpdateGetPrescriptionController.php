5e90a6ab8cb83d22a6ec73b5bc067efe67cc749a
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-03 13:59:22 +0530

    Update CaseStatusUpdateGetPrescriptionController.php

--- app/Http/Controllers/CaseStatusUpdateGetPrescriptionController.php
+++ app/Http/Controllers/CaseStatusUpdateGetPrescriptionController.php
@@ -558,7 +558,7 @@ class CaseStatusUpdateGetPrescriptionController extends Controller
     public function save_prescription_response($response,$user_id,$case_id,$system_case_id){
       $prescription_data = json_decode($response);
 
-      foreach($prescription_data as $key=>$prescription){
+      foreach($response as $key=>$prescription){
 
          
             $input_prescription['dosespot_prescription_id'] = (isset($prescription->dosespot_prescription_id)) ? $prescription->dosespot_prescription_id : NULL;

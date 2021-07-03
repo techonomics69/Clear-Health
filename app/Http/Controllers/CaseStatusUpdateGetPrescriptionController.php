f25c33efdd9fcfed100e2bf7000c5fef80314ccc
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-03 15:00:18 +0530

    Update CaseStatusUpdateGetPrescriptionController.php

--- app/Http/Controllers/CaseStatusUpdateGetPrescriptionController.php
+++ app/Http/Controllers/CaseStatusUpdateGetPrescriptionController.php
@@ -558,7 +558,32 @@ class CaseStatusUpdateGetPrescriptionController extends Controller
     public function save_prescription_response($response,$user_id,$case_id,$system_case_id){
       $prescription_data = json_decode($response);
 
+      
+      // echo "<pre>";
+      // print_r($response);
+      // echo "<pre>";
+
+      // echo "<pre>";
+      // print_r($prescription_data[0]->dosespot_prescription_sync_status);
+      // echo "<pre>";
+      
+      // exit();
+
       foreach($prescription_data as $key=>$prescription){
+
+
+
+         // $prescription = json_decode(json_encode($prescription), true);
+
+         // echo "<pre> data is ";
+         // print_r($prescription->dosespot_prescription_sync_status);
+         // echo "<pre>";
+         // exit();
+
+          // echo "<pre>";
+          // print_r($prescription);
+          // echo "<pre>";
+          // exit();
          
             $input_prescription['dosespot_prescription_id'] = (isset($prescription->dosespot_prescription_id)) ? $prescription->dosespot_prescription_id : NULL;
 
@@ -580,7 +605,11 @@ class CaseStatusUpdateGetPrescriptionController extends Controller
             $input_prescription['user_id'] = $user_id;
             $input_prescription['system_case_id'] = $system_case_id;
 
-        
+            echo "<pre>";
+            print_r($input_prescription);
+            echo "<pre>"; 
+            exit();
+
             $CasePrescription_data = CasePrescriptions::create($input_prescription);
           
             

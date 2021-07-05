10c8e24fe1e1ecb3a9389c8eb74ecd265d169f1b
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-05 10:10:12 +0530

    Update UserController.php

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -344,7 +344,8 @@ public function addUserPic(Request $request)
       'right_pic'=>$file_name_right_pic,
       'other_pic' => $file_name_other_pic,*/
     ));
-    return $this->sendResponse($parent, 'User picture saved successfully.');
+     $userpic=UserPics::where('user_id',$request['user_id'])->where('case_id',$request['case_id'])->first();
+    return $this->sendResponse($userpic, 'User picture saved successfully.');
   }
 }
 

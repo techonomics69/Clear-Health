621a2848db6b488253e21bcd44a43d0b048d6b3d
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-05 17:30:26 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/CaseManagementController.php
+++ app/Http/Controllers/CaseManagementController.php
@@ -724,7 +724,7 @@ die();*/
          $noti_input_data['month'] = $follow_up;
 
          $trigger_input = array();
-         $trigger_input['user_id'] = $user_id
+         $trigger_input['user_id'] = $user_id;
          $trigger_input['case_id'] = $case_id;
          $trigger_input['md_case_id'] = $md_case_id;
          $trigger_input['name'] = "pickup_medication_notification";

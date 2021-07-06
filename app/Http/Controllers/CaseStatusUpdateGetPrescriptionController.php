4023df6f3c1019d2d77c7e1e0e4883c345313028
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-06 19:18:45 +0530

    Update CaseStatusUpdateGetPrescriptionController.php

--- app/Http/Controllers/CaseStatusUpdateGetPrescriptionController.php
+++ app/Http/Controllers/CaseStatusUpdateGetPrescriptionController.php
@@ -61,7 +61,16 @@ class CaseStatusUpdateGetPrescriptionController extends Controller
        $user_phone = $value['mobile'];
 
         $order_data = Checkout::where([['user_id', $user_id],['case_id', $system_case_id],['md_case_id', $case_id]])->first();
+
+        
         $ShiStation = shipStationHelper::getOrderData($order_data['shipstation_order_id']);
+
+          echo "<pre>";
+          print_r( $ShiStation);
+          echo "<pre>";
+          exit();
+
+
      /*  $cart_ids = explode(',', $order_data['cart_id']);
        $pharmacy_data  =  Cart::select('pharmacy_pickup')->where('user_id',$user_id)->whereIn('id',$cart_ids)->where('order_type', '!=', 'AddOn')->first();
        $preferred_pharmacy_id = $pharmacy_data['pharmacy_pickup'];*/

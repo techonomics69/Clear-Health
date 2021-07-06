5586c5d0cce16ea5c1dd4b927c0280d4cfa2edd8
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-06 12:22:53 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -1862,20 +1862,20 @@
 	$("#photoSelect").on('change',function(){
 		if($(this).val() == 'user_photos'){
 			$(".user_photos").show();
-			$(".followups").hide();
+			// $(".followups").hide();
 			$(".followups").each(function(v,i){
 				$(".followup"+(parseFloat(i+1))).hide();
 			});
 		}else if($.isNumeric($(this).val())){
 			$(".user_photos").hide();
-			$(".followups").show();
-			$(".followups").each(function(v,i){
-				$(".followup"+(parseFloat(i+1))).hide();
-			});
+			// $(".followups").show();
+			// $(".followups").each(function(v,i){
+			// 	$(".followup"+(parseFloat(i+1))).hide();
+			// });
 			$(".followup"+$(this).val()).show();
 		}else{
 			$(".user_photos").hide();
-			$(".followups").hide();
+			// $(".followups").hide();
 			$(".followups").each(function(v,i){
 				$(".followup"+(parseFloat(i+1))).hide();
 			});

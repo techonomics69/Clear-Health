7ef3597a0013552f7009b3fe2c81d0b219314816
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-06 11:50:11 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -1220,7 +1220,7 @@
 									<?php
 										for($i=1;$i<=count($followup_que);$i++){
 								?>
-									<option value="{{$i}}">Follow Up: {{$i}}</option>
+									<option value="followup{{$i}}">Follow Up: {{$i}}</option>
 								<?php			
 										}
 									?>
@@ -1231,79 +1231,82 @@
 						</div>
 					</div>													
 					<div class="row" style="padding: 20px;">
-						<div class="col-md-12">
-							<h2>User Photos</h2>
-						</div>
-						@if(isset($user_pic->left_pic))
-						<div class="col-md-3">
-							<section class="card">
-								<div class="card-body">
-									<div class="box-block mtb32">
-										<h3 class="font-weight-bold"><span class="text-underline">Left Face</span></h3>
-										<a href="{{$user_pic->left_pic}}" target="_blank">
-											<img src="{{$user_pic->left_pic}}" class="img" width="100%">
-										</a>
+						<div class="user_photos">
+							<div class="col-md-12">
+								<h2>User Photos</h2>
+							</div>
+							@if(isset($user_pic->left_pic))
+							<div class="col-md-3">
+								<section class="card">
+									<div class="card-body">
+										<div class="box-block mtb32">
+											<h3 class="font-weight-bold"><span class="text-underline">Left Face</span></h3>
+											<a href="{{$user_pic->left_pic}}" target="_blank">
+												<img src="{{$user_pic->left_pic}}" class="img" width="100%">
+											</a>
+										</div>
 									</div>
-								</div>
-							</section>
-						</div>
-						@endif
-						@if(isset($user_pic->right_pic))
-						<div class="col-md-3">
-							<section class="card">
-								<div class="card-body">
-									<div class="box-block mtb32">
-										<h3 class="font-weight-bold"><span class="text-underline">Right Face</span></h3>
-										<a href="{{$user_pic->right_pic}}" target="_blank">
-											<img src="{{$user_pic->right_pic}}" class="img" width="100%">
-										</a>
+								</section>
+							</div>
+							@endif
+							@if(isset($user_pic->right_pic))
+							<div class="col-md-3">
+								<section class="card">
+									<div class="card-body">
+										<div class="box-block mtb32">
+											<h3 class="font-weight-bold"><span class="text-underline">Right Face</span></h3>
+											<a href="{{$user_pic->right_pic}}" target="_blank">
+												<img src="{{$user_pic->right_pic}}" class="img" width="100%">
+											</a>
+										</div>
 									</div>
-								</div>
-							</section>
-						</div>
-						@endif
-						@if(isset($user_pic->straight_pic))
-						<div class="col-md-3">
-							<section class="card">
-								<div class="card-body">
-									<div class="box-block mtb32">
-										<h3 class="font-weight-bold"><span class="text-underline">Straight Face</span></h3>
-										<a href="{{$user_pic->straight_pic}}" target="_blank">
-											<img src="{{$user_pic->straight_pic}}" class="img" width="100%">
-										</a>
+								</section>
+							</div>
+							@endif
+							@if(isset($user_pic->straight_pic))
+							<div class="col-md-3">
+								<section class="card">
+									<div class="card-body">
+										<div class="box-block mtb32">
+											<h3 class="font-weight-bold"><span class="text-underline">Straight Face</span></h3>
+											<a href="{{$user_pic->straight_pic}}" target="_blank">
+												<img src="{{$user_pic->straight_pic}}" class="img" width="100%">
+											</a>
+										</div>
 									</div>
-								</div>
-							</section>
-						</div>
-						@endif
-						@if(isset($user_pic->back_photo))
-						<div class="col-md-3">
-							<section class="card">
-								<div class="card-body">
-									<div class="box-block mtb32">
-										<h3 class="font-weight-bold"><span class="text-underline">Back Photo</span></h3>
-										<a href="{{$user_pic->back_photo}}" target="_blank">
-											<img src="{{$user_pic->back_photo}}" class="img" width="100%">
-										</a>
+								</section>
+							</div>
+							@endif
+							@if(isset($user_pic->back_photo))
+							<div class="col-md-3">
+								<section class="card">
+									<div class="card-body">
+										<div class="box-block mtb32">
+											<h3 class="font-weight-bold"><span class="text-underline">Back Photo</span></h3>
+											<a href="{{$user_pic->back_photo}}" target="_blank">
+												<img src="{{$user_pic->back_photo}}" class="img" width="100%">
+											</a>
+										</div>
 									</div>
-								</div>
-							</section>
-						</div>
-						@endif
-						@if(isset($user_pic->other_pic))
-						<div class="col-md-3">
-							<section class="card">
-								<div class="card-body">
-									<div class="box-block mtb32">
-										<h3 class="font-weight-bold"><span class="text-underline">Other Picture</span></h3>
-										<a href="{{$user_pic->other_pic}}" target="_blank">
-											<img src="{{$user_pic->other_pic}}" class="img" width="100%">
-										</a>
+								</section>
+							</div>
+							@endif
+							@if(isset($user_pic->other_pic))
+							<div class="col-md-3">
+								<section class="card">
+									<div class="card-body">
+										<div class="box-block mtb32">
+											<h3 class="font-weight-bold"><span class="text-underline">Other Picture</span></h3>
+											<a href="{{$user_pic->other_pic}}" target="_blank">
+												<img src="{{$user_pic->other_pic}}" class="img" width="100%">
+											</a>
+										</div>
 									</div>
-								</div>
-							</section>
-						</div>
-						@endif
+								</section>
+							</div>
+							@endif			
+						</div>				
+						
 						@if(isset($followup_que))
 						@foreach($followup_que as $key => $data)
 						<div class="col-md-12">

3737099ea7663ef7da730da0e1185e22e17369c9
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-07 16:05:27 +0530

    Create 2021_07_07_103303_change_fielde_type_in_ipledge_agreement_table.php

---
+++ database/migrations/2021_07_07_103303_change_fielde_type_in_ipledge_agreement_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeFieldeTypeInIpledgeAgreementTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('ipledge_agreement', function (Blueprint $table) {
+            $table->date('patients_signature_date')->nullable()->change();
+            $table->date('guardians_signature_date')->nullable()->change()
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        /*Schema::table('ipledge_agreement', function (Blueprint $table) {
+            //
+        });*/
+    }
+}

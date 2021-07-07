dd691c598edbcebae09f2ecc1788319cb4706cbc
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-07 18:17:08 +0530

    Create 2021_07_07_124603_change_field_in_case_files_table.php

---
+++ database/migrations/2021_07_07_124603_change_field_in_case_files_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeFieldInCaseFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('case_files', function (Blueprint $table) {
+            $table->string('case_id')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        /*Schema::table('case_files', function (Blueprint $table) {
+            //
+        });*/
+    }
+}

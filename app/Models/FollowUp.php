9f00ae6f922bde4379ca474740d278829435b709
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-07 18:29:36 +0530

    Update FollowUp.php

--- app/Models/FollowUp.php
+++ app/Models/FollowUp.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class FollowUp extends Model
 {
 	protected $table = 'follow_up';
-	protected $fillable = ['id','user_id','case_id','answer','left_face','right_face','center_face','back_photo','chest_photo','follow_up_status','follow_up_no','pregnancy_test', 'pregnancy_test_verify'];
+	protected $fillable = ['id','user_id','case_id','answer','left_face','right_face','center_face','back_photo','chest_photo','follow_up_status','follow_up_no','pregnancy_test', 'pregnancy_test_verify','left_face_file_id','right_face_file_id','center_face_file_id','back_photo_file_id','chest_photo_file_id','pregnancy_test_file_id'];
 }

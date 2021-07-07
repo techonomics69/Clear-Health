e29fb3dc6ff425917f1f641f90ad130ead2a44ec
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-07 15:36:08 +0530

    Update index.blade.php

--- resources/views/ipledgeimports/index.blade.php
+++ resources/views/ipledgeimports/index.blade.php
@@ -236,7 +236,20 @@
 <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-lg .modal-dialog-centered">
     <div class="modal-content">
-      ...
+	<div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Sample csv with mentioned fields to be considered while uploading a csv</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+	  <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>						
+      
     </div>
   </div>
 </div>

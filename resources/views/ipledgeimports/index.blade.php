986746b66bb9b86229e61797453b441a71070d73
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-07 15:49:43 +0530

    Update index.blade.php

--- resources/views/ipledgeimports/index.blade.php
+++ resources/views/ipledgeimports/index.blade.php
@@ -259,7 +259,7 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-        <button type="button" class="btn btn-primary">Ok</button>
+        <button type="button" class="btn btn-primary" onclick="gotoadd();">Ok</button>
       </div>						
       
     </div>
@@ -327,6 +327,10 @@
 			} 
 		});
 	};
+
+	function gotoadd(){
+		window.location.href="{{ route('ipledgeimports.create') }}";
+	}
 </script>
 @endsection
 

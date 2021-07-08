4228a286c4ce4348e7b462cceae4b6110b589cb8
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-08 10:26:48 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -204,7 +204,7 @@
 						<div class="card">
 						  <div class="card-header" id="headingOne">
 							<h2 class="mb-0">
-							  <button class="btn active btn-primary text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+							  <button class="btn active btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
 								Collapsible Group Item #1
 							  </button>
 							</h2>
@@ -219,7 +219,7 @@
 						<div class="card">
 						  <div class="card-header" id="headingTwo">
 							<h2 class="mb-0">
-							  <button class="btn btn-primary text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+							  <button class="btn  btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
 								Collapsible Group Item #2
 							  </button>
 							</h2>
@@ -233,7 +233,7 @@
 						<div class="card">
 						  <div class="card-header" id="headingThree">
 							<h2 class="mb-0">
-							  <button class="btn btn-primary text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+							  <button class="btn  btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
 								Collapsible Group Item #3
 							  </button>
 							</h2>

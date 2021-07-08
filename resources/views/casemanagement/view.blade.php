f7234c5b3df0dd05c93e3143767596c41fc5a04f
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-08 10:25:52 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -204,9 +204,9 @@
 						<div class="card">
 						  <div class="card-header" id="headingOne">
 							<h2 class="mb-0">
-							  <a class="btn active"  data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+							  <button class="btn active btn-primary text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
 								Collapsible Group Item #1
-							  </a>
+							  </button>
 							</h2>
 						  </div>
 					  
@@ -219,9 +219,9 @@
 						<div class="card">
 						  <div class="card-header" id="headingTwo">
 							<h2 class="mb-0">
-							  <a class="btn"  data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+							  <button class="btn btn-primary text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
 								Collapsible Group Item #2
-							  </a>
+							  </button>
 							</h2>
 						  </div>
 						  <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
@@ -233,9 +233,9 @@
 						<div class="card">
 						  <div class="card-header" id="headingThree">
 							<h2 class="mb-0">
-							  <a class="btn"  data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+							  <button class="btn btn-primary text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
 								Collapsible Group Item #3
-							  </a>
+							  </button>
 							</h2>
 						  </div>
 						  <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">

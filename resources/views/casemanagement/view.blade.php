b714ca4b2b733820d5a24d8ba06a3d85281af7fb
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-08 10:22:44 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -204,13 +204,13 @@
 						<div class="card">
 						  <div class="card-header" id="headingOne">
 							<h2 class="mb-0">
-							  <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+							  <button class="btn active btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
 								Collapsible Group Item #1
 							  </button>
 							</h2>
 						  </div>
 					  
-						  <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+						  <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
 							<div class="card-body">
 							  Some placeholder content for the first accordion panel. This panel is shown by default, thanks to the <code>.show</code> class.
 							</div>
@@ -219,7 +219,7 @@
 						<div class="card">
 						  <div class="card-header" id="headingTwo">
 							<h2 class="mb-0">
-							  <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+							  <button class="btn  btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
 								Collapsible Group Item #2
 							  </button>
 							</h2>
@@ -233,7 +233,7 @@
 						<div class="card">
 						  <div class="card-header" id="headingThree">
 							<h2 class="mb-0">
-							  <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+							  <button class="btn  btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
 								Collapsible Group Item #3
 							  </button>
 							</h2>

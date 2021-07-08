188c888c9b5a7b726eeac532b2137bb2085e28fb
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-08 14:46:43 +0530

    Update index.blade.php

--- resources/views/casemanagement/index.blade.php
+++ resources/views/casemanagement/index.blade.php
@@ -41,7 +41,7 @@
           <div class="row" style="padding: 20px;">
             <div class="col-md-12">
               <div class="">
-                <table class="table table-responsive-xl table-striped table-bordered " style=" width:100%" id="CaseManagementList">
+                <table class="table table-responsive-lg table-striped table-bordered " style=" width:100%" id="CaseManagementList">
                   <thead>
                     <tr>
                       <th>SR</th>

f9515d107b9ca7489cff3bc83d8716efac3bd5b9
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-08 10:06:09 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/CaseManagementController.php
+++ app/Http/Controllers/CaseManagementController.php
@@ -196,7 +196,7 @@ class CaseManagementController extends Controller
 
     $general = Answers::where('case_id', $user_case_management_data['id'])->where('user_id', $user_case_management_data['user_id'])->where('category_id', 7)->first();
 
-
+    dd($general);
     if (isset($general)) {
       $general_que = json_decode($general->answer);
     } else {

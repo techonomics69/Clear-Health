a47408369fc3d5f540ed447d867b70f6e8d1b076
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-08 15:27:26 +0530

    Update 2021_07_08_092859_add_fields_in_products_table.php

--- database/migrations/2021_07_08_092859_add_fields_in_products_table.php
+++ database/migrations/2021_07_08_092859_add_fields_in_products_table.php
@@ -14,7 +14,15 @@ class AddFieldsInProductsTable extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            //
+            $table->longtext("section1_title")->after('section1')->nullable();
+            $table->longtext("section1_content")->after('section1_title')->nullable();
+            $table->longtext("section1_image")->after('section1_content')->nullable();
+            $table->longtext("section2_title")->after('section2')->nullable();
+            $table->longtext("section2_image")->after('section1_content')->nullable();
+            $table->longtext("section2_content")->after('section2_title')->nullable();
+            $table->longtext("section3_title")->after('section3')->nullable();
+            $table->longtext("section3_content")->after('section3_title')->nullable();
+            $table->longtext("section3_image")->after('section3_content')->nullable();
         });
     }
 
@@ -25,8 +33,8 @@ class AddFieldsInProductsTable extends Migration
      */
     public function down()
     {
-        Schema::table('products', function (Blueprint $table) {
+        /*Schema::table('products', function (Blueprint $table) {
             //
-        });
+        });*/
     }
 }

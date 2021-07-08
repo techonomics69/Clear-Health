27150a705c5f2c99d0286e4166de6624761a0b92
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-08 17:26:38 +0530

    Trigger  changes

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -830,7 +830,7 @@
 												<input type="hidden" name="case_id" value="{{$user_case_management_data->id}}">
 											<input type="hidden" name="user_id" value="{{$user_case_management_data->user_id}}">
 											<input type="hidden" name="md_case_id" value="" id="md_case_id">
-											<input type="hidden" name="follow_up_id" value="" id="follow_up_id">
+											<!-- <input type="hidden" name="follow_up_id" value="" id="follow_up_id"> -->
 
 													
 													<div class="ipledge_button">

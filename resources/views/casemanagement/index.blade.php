dce719951c06c88b346e7340d9c7a67bc69d8ad6
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-09 18:14:29 +0530

    Update index.blade.php

--- resources/views/casemanagement/index.blade.php
+++ resources/views/casemanagement/index.blade.php
@@ -201,7 +201,7 @@
         $('#filter1').on('change', function(){
           
             var filter_value = $(this).val();
-            Datatable.serverSide = true;
+            Datatable.processing = true;
           });
    });
 

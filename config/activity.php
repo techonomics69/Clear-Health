11814d5af890a68c9797f1711c947268d4e780a7
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-12 11:55:18 +0530

    Update activity.php

--- config/activity.php
+++ config/activity.php
@@ -8,7 +8,13 @@
 
         'action_module' =>  [
             'case_management'   =>  [
-                'action_items'  =>  [],
+                'action_items'  =>  [
+                    'ipledge'   =>  'Ipledge',
+                    'pregnancy_test'    =>  'Pregnancy test',
+                    'boodwork'  =>  'Bloodwork',
+                    'prior_auth'    =>  'Prior auth',
+                    'triggers'  =>  'Triggers',
+                ],
             ],
         ],
     ];

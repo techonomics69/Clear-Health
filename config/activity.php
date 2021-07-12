ef00d68a54137a4f6d145681b7fa0763b4a9bce8
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-12 11:47:55 +0530

    Activity Tab changes

---
+++ config/activity.php
@@ -0,0 +1,15 @@
+<?php
+    return [
+        'action' => [
+            'insert'    =>  'Insert',
+            'update'    =>  'Update',
+            'delete'    =>  'Delete',
+        ],
+
+        'action_module' =>  [
+            'case_management'   =>  [
+                'action_items'  =>  [],
+            ],
+        ],
+    ];
+?>
---
+++ database/migrations/2021_07_12_061503_alter_table_activity_log.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTableActivityLog extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activity_log', function (Blueprint $table) {
+            $table->string('ref_id')->nullable()->after('description');
+            $table->string('case_id')->nullable()->after('ref_id');
+            $table->string('md_case_id')->nullable()->after('case_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}

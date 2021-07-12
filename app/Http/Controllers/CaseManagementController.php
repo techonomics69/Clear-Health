c456aa4649fc5991a6964d923a0fcc4cc198cb42
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-12 13:04:32 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/CaseManagementController.php
+++ app/Http/Controllers/CaseManagementController.php
@@ -1005,8 +1005,9 @@ die();*/
 
   
     $update = $follow_up->update($data);
-    dd($update);
+    
     if ($update) :
+      dd($update);
       $userRole = DB::table('users')->join('roles','users.role','=','roles.id')->select('users.id as userId','roles.name as roleName')
                   ->where('users.id',Auth::user()->id)->get();
 

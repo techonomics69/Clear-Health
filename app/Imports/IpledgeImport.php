74d9aaeb7dd633fd4cef58f1fb3edaf96898507d
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-12 16:00:38 +0530

    Update IpledgeImport.php

--- app/Imports/IpledgeImport.php
+++ app/Imports/IpledgeImport.php
@@ -6,10 +6,11 @@ use App\Models\Ipledge;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Exceptions\NoTypeDetectedException;
 use Maatwebsite\Excel\Concerns\WithStartRow;
-
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Row;
+use Maatwebsite\Excel\Concerns\OnEachRow;
 
-class IpledgeImport implements ToModel, WithHeadingRow, WithStartRow
+class IpledgeImport implements ToModel, WithHeadingRow, WithStartRow, OnEachRow
 
 //class IpledgeImport implements ToModel
 {
@@ -36,32 +37,48 @@ class IpledgeImport implements ToModel, WithHeadingRow, WithStartRow
      //        $this->$patients_type = $patients_type;
      // }
  
-    public function model(array $row)
-    {
+    // public function model(array $row)
+    // {
         
-        print_r($row)."<br>";
-        // $ipledge_data = Ipledge::where('patient_id',$row['patient_id'])->get(); 
+    //     //print_r($row)."<br>";
+    //     // $ipledge_data = Ipledge::where('patient_id',$row['patient_id'])->get(); 
 
-        //     $patientstype = $this->patients_type;
+    //     //     $patientstype = $this->patients_type;
 
-        // if(!empty($ipledge_data) && count($ipledge_data)>0){
+    //     // if(!empty($ipledge_data) && count($ipledge_data)>0){
                 
-        // }else{
+    //     // }else{
 
-        //     return new Ipledge([
-        //     'patient_id'     => $row['patient_id'],
-        //     'addon_date'    => isset($row['addon_date']) ? $row['addon_date'] : null,
-        //     'addon_by' => isset($row['addon_by']) ? $row['addon_by'] : "",
-        //     'patient_name' => isset($row['patient_name']) ? $row['patient_name'] : "",
-        //     'patients_type' => $patientstype,
-        //     'gender' => isset($row['gender']) ? $row['gender'] : "",
-        //     'assigned_date' => isset($row['assigned_date']) ? $row['assigned_date'] : null,
-        //     'assigned_by' => isset($row['assigned_by']) ? $row['assigned_by'] : "",
-        //     'notes' => isset($row['notes']) ? $row['notes'] : "",
-        // ]); 
-        // }
+    //     //     return new Ipledge([
+    //     //     'patient_id'     => $row['patient_id'],
+    //     //     'addon_date'    => isset($row['addon_date']) ? $row['addon_date'] : null,
+    //     //     'addon_by' => isset($row['addon_by']) ? $row['addon_by'] : "",
+    //     //     'patient_name' => isset($row['patient_name']) ? $row['patient_name'] : "",
+    //     //     'patients_type' => $patientstype,
+    //     //     'gender' => isset($row['gender']) ? $row['gender'] : "",
+    //     //     'assigned_date' => isset($row['assigned_date']) ? $row['assigned_date'] : null,
+    //     //     'assigned_by' => isset($row['assigned_by']) ? $row['assigned_by'] : "",
+    //     //     'notes' => isset($row['notes']) ? $row['notes'] : "",
+    //     // ]); 
+    //     // }
        
         
+    // }
+
+    public function onRow(Row $row)
+    {
+        $rowIndex = $row->getIndex();
+        $row      = $row->toArray();
+        
+        echo $rowIndex."<br>";
+
+        // $group = Group::firstOrCreate([
+        //     'name' => $row[1],
+        // ]);
+    
+        // $group->users()->create([
+        //     'name' => $row[0],
+        // ]);
     }
 
     

c5cc710dbf8ecd5ce0c2b2069b912eca057209f1
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-12 15:42:45 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/CaseManagementController.php
+++ app/Http/Controllers/CaseManagementController.php
@@ -1265,10 +1265,11 @@ die();*/
     $documents = $request->file('file');
 
     $this->validate($request, [
-      'file' => 'required',
+      'file' => 'required|max:5000',
     ], [
       'file.required' => 'Blood Work Test file field is required.',
-      //'file.mimes' => 'Blood Work  File must be a file of type:jpg,jpeg,png,pdf',
+      // 'file.mimes' => 'Blood Work  File must be a file of type:jpg,jpeg,png,pdf',
+      'file.max'  =>  'Please upload file size less than 5MB'
 
     ]);
 
@@ -1276,7 +1277,10 @@ die();*/
     if (!empty($documents)) {
       $file =  $documents->getClientOriginalName();
       $ext = pathinfo($file, PATHINFO_EXTENSION);
-      dd($ext);
+      if($ext == 'exe'){
+        toastr()->error('Excutable file is not allowed');
+        return redirect()->back();
+      }
       $doc_file_name =  time() . '-' . $file;
       //$doc_file_name = time() . '-' . $doc->getClientOriginalExtension();
 

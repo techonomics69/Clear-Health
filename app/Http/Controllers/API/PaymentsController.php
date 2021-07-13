56087ed1a6c30a29fc800834fb28aa91a21b3c26
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-13 12:47:30 +0530

    Update PaymentsController.php

--- app/Http/Controllers/API/PaymentsController.php
+++ app/Http/Controllers/API/PaymentsController.php
@@ -374,19 +374,21 @@ class PaymentsController extends BaseController
             $product_id = explode(",", $subscription['product_id']);
             $productArray = [];
             foreach ($product_id as $key => $value) {
-                // $products = Product::where('category_id','6')->where('id',$value)->get();
-                // if(count($products)>0){
-                //     foreach($products as $pkey => $pvalue){
-                //         if($pvalue->id == $value){
-                //             $products[$pkey]['isAdded'] = true;
-                //         }else{
-                //             $products[$pkey]['isAdded'] = true;
-                //         }       
-                //         array_push($productArray, $products);  
-                //     } 
-                // }
                 $product = Product::find($value);
                 array_push($productArray, $product);
+                $products = Product::where('category_id','6')->get();
+                if(count($products)>0){
+                    foreach($products as $pkey => $pvalue){
+                        if($pvalue->id == $value){
+                            $products[$pkey]['isAdded'] = true;
+                        }else{
+                            $products[$pkey]['isAdded'] = true;
+                        }       
+                        array_push($productArray, $products);  
+                    } 
+                }
+
+                // array_push($productArray, $product);
             }
             $subscription['product'] = $productArray;
             return $this->sendResponse($subscription, 'subscription retrieve successfully.');

e9b8e6325ac096a85195d8bf565705ffbfde7970
jalpa k <jalpa@itidoltechnologies.com>
2021-07-14 15:35:04 +0530

    Update BirthControlController.php

--- app/Http/Controllers/API/BirthControlController.php
+++ app/Http/Controllers/API/BirthControlController.php
@@ -70,7 +70,7 @@ class BirthControlController extends BaseController
     public function show($id)
     {
         try{
-            $birthControl = DB::table('birthcontrol')->where('user_id',$id)->get();
+            $birthControl = DB::table('birthcontrol')->where('user_id',$id)->orderBy('id', 'DESC')->first();
             if(count($birthControl)>0){
                 return $this->sendResponse($birthControl, 'Records found');
             }

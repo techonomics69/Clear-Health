f9550584176f4f4be7ead2baf4aba238ef280476
jalpa k <jalpa@itidoltechnologies.com>
2021-07-14 17:30:22 +0530

    Update index.php

--- resources/views/messages/index.php
+++ resources/views/messages/index.php
@@ -1,5 +1,28 @@
-<?php
+@extends('layouts.app')
 
-echo 'message';
+@section('title', 'clearHealth | Messages')
+@section('content')
 
-?>
+<div class="app-content content">
+    @if ($message = Session::get('success'))
+    <div class="alert alert-success">
+        <p>{{ $message }}</p>
+    </div>
+    @endif
+
+    <div class="content-wrapper">
+        <div class="content-header row">
+            <div class="content-header-left col-md-6 col-12 mb-2">
+                <h3 class="content-header-title mb-0">Messages</h3>
+                <div class="row breadcrumbs-top">
+                    <div class="breadcrumb-wrapper col-12 d-flex">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{url('/admin/dashboard')}}">Dashboard</a></li>
+                            <li class="breadcrumb-item active">Messages</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

40655ac176e3ded4ea270df5ac0ba6f9754da0b5
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-14 13:23:42 +0530

    Update PaymentsController.php

--- app/Http/Controllers/API/PaymentsController.php
+++ app/Http/Controllers/API/PaymentsController.php
@@ -188,6 +188,7 @@ class PaymentsController extends BaseController
             $product_id = explode(",",$previousData['product_id']);
             $products = explode(",",$request->products);
             $diff = array_diff($product_id, $products);
+            dd(count($diff));
             if(count($diff)>0){
             }else{
                 return $this->sendError('Can not update plan! please select new products');    

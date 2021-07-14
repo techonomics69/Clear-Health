8484f7706ff5c644b8b9caaf9e925daf545d285d
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-14 13:43:17 +0530

    Update SubscriptionLog.php

--- app/Models/SubscriptionLog.php
+++ app/Models/SubscriptionLog.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class SubscriptionLog extends Model
 {
     use HasFactory;
-    protected $table = ['subscription_log'];
+    protected $table = 'subscription_log';
 
     protected $fillable = ['subscription_id','user_id','case_id','md_case_id','product_id','subscr_id',
                             'customer','plan_id','plan_amount','plan_currency','plan_interval',

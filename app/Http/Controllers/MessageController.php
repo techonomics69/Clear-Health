c03336b5785791169c443a0ad93ad24582b1a9cc
jalpa k <jalpa@itidoltechnologies.com>
2021-07-15 17:32:48 +0530

    Update MessageController.php

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -35,7 +35,10 @@ class MessageController extends Controller
     }
 
     public function getMedicalMessage(Request $request)
-    {
+    {   
+        $data = $request->all();
+        dd($data->case_id);
+        $message = MdMessages::where('case_id', $data->case_id)
         echo '<pre>';
         print_r($request->all());
         die;

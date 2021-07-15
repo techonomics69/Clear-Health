0c76020aae09bbc48796465133324596084d3d91
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-15 14:47:48 +0530

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -345,10 +345,10 @@ class ProductController extends Controller
         unset($data['_token']);
         unset($data['_method']);
 
-        print_r($data['section1_image']);
-        print_r($data['section2_image']);
-        print_r($data['section3_image']);
-        die();
+        // print_r($data['section1_image']);
+        // print_r($data['section2_image']);
+        // print_r($data['section3_image']);
+        // die();
 
         $product =Product::where('id',$id)->update($data);       
                 

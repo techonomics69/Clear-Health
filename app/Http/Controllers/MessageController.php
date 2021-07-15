03cbf86d06b989e1972537e0f6549c700401c381
jalpa k <jalpa@itidoltechnologies.com>
2021-07-15 11:54:43 +0530

    Update MessageController.php

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -10,15 +10,12 @@ class MessageController extends Controller
 {
     public function index(Request $request)
     {
-        $mdList = MdMessages::orderBy('id','DESC')->paginate(10);
-        echo '<pre>';
-        print_r($mdList);
-        die;
-        dd($mdList); 
+        $mdList = MdMessages::groupBy('md_case_id')->orderBy('id','DESC')->paginate(10);
+        dd($mdList);
         $msg_tab = '';
         $msg_history = [];
         $user_case_management_data['user_id'] = '';
         $user_case_management_data['id'] = '';
-        return view('messages.index', compact('msg_tab', 'msg_history', 'user_case_management_data'));
+        return view('messages.index', compact('msg_tab', 'msg_history', 'user_case_management_data', 'mdList'));
     }
 }

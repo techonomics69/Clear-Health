52ed757f0217f432d087de5b694b4e6c155a55fe
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-15 17:28:14 +0530

    Update view.blade.php

--- resources/views/ordermanagement/view.blade.php
+++ resources/views/ordermanagement/view.blade.php
@@ -389,73 +389,35 @@
 								</div>
 							</div>
 
-							    <section class="card" >
-                            <div class="row" style="padding: 20px;">
-                                <div class="col-md-12">
-                                   <table class="table table-responsive-md table-striped table-bordered " style="width:100%" id="ordermanagement">
-                                    <thead>
-                                        <tr>
-                                            <th width="60px">SR No</th>
-                                            <th>Order Id</th>
-                                            <th>Case Id</th>
-                                            <th>Email</th>
-                                            <th>Date</th>
-                                            <th>Product Name</th>
-                                            <th>Order Type</th>
-                                            <!--  <th>Order Status</th> -->
-                                            <th width="200px">Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php $i=1;?>
-                                        @foreach ($order as $key => $order_data)
-                                        <tr> 
-                                            <td>{{ $i++ }}</td>
-                                            <td>{{ $order_data->order_id }} </td>
-                                            <td>{{ $order_data->case_id }}</td>
-                                            <td>{{ $order_data->email }}</td>
+							<section class="card" >
+		                            <div class="row">
+		                                <div class="col-md-12">
+		                                   <table class="table table-responsive-md table-striped table-bordered " style="width:100%" >
+			                                    <thead>
+			                                        <tr>
+			                                            <th width="60px">SR No</th>
+			                                            <th>Order Id</th>
+			                                            <th>Case Id</th>
+			                                            <th>Email</th>
+			                                            <th>Date</th>
+			                                        </tr>
+			                                    </thead>
+			                                    <tbody>
+			                                    	<tr>
+			                                    		<td>df</td>
+			                                    		<td>fd</td>
+			                                    		<td>fd</td>
+			                                    		<td>fd</td>
+			                                    		<td>fd</td>
+			                                    	</tr>
+			                                    </tbody>
+		                                    
+		                                  </table>
+		                            	</div>
+		                        	</div>
+		                    </section>
 
-                                            <td>{{\Carbon\Carbon::parse($order_data->created_at)->format('d/m/Y')}} </td>
-                                            <!-- <td>{{ $order_data->created_at }}</td> -->
-                                            <td>{{ $order_data->product_name }}</td>
-                                            <!-- <td>{{ $order_data->product_price }}</td>  -->
-                                            <td><?php if($order_data->medication_type == 1){
-                                                echo "Prescribed";
-                                            }else{
-                                                echo "Non Prescribed";
-                                            } ?></td>
 
-                                            <?php if($order_data->medication_type == 2 ) { ?> 
-                                             <td> 
-                                                <div class="d-flex">
-                                                    <a class="icons edit-icon" href="{{ route('ordermanagement.show',$order_data->id)}}">
-                                                        <i class="fa fa-eye"></i>
-                                                    </a> 
-                                                </div>  
-                                            </td>
-                                        <?php }else { ?>
-                                            <?php if(isset($order_data->case_id)){?>
-                                           <td> 
-                                                <div class="d-flex">
-                                                    <a class="icons edit-icon" href="{{ route('casemanagement.show',$order_data->case_id)}}">
-                                                        <i class="fa fa-eye"></i> 
-                                                    </a> 
-                                                </div>  
-                                            </td> 
-                                        <?php }else{
-                                            ?>
-                                            <td></td>
-                                            <?php } ?> 
-                                    <?php  }?>
-                                        </tr>
-                                        @endforeach
-                                    </tbody> 
-                                </table>
-                            </div>
-                        </div>
-                    </section>
-
-                    
 						</section>
 					</div>
 				</div>

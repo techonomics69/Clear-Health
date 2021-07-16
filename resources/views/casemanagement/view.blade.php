04c1c74c496548abc23f8be1e1a3952ee06bcd25
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-16 14:10:14 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -210,7 +210,7 @@
 											<td width="20%">Date</td>
 											<td wisth="20%">Product</td>
 											<td width="20%">Visit type</td>
-											<td width="20%">Note</td>
+											<td width="20%" style="padding-left: 40px;">Note</td>
 										</tr>
 									</table>
 								</div>
@@ -227,7 +227,7 @@
 												<td width="20%" onclick="questionsCollapse('collapseOne')"><span><?php if(count($accutane_que)>0){ echo "Accutane"; }else if(count($topical_que)>0){ echo "Topical"; } ?></span></td>
 												<td width="20%" onclick="questionsCollapse('collapseOne')">Initials</td>
 
-												<td width="20%" style="padding-left: 20px;" data-toggle="modal" data-target="#exampleModalCenter"><i class="fa fa-file-text" aria-hidden="true" ></i></td>
+												<td width="20%" style="padding-left: 40px;" data-toggle="modal" data-target="#exampleModalCenter"><i class="fa fa-file-text" aria-hidden="true" ></i></td>
 											</tr>
 										</table>
 									
@@ -320,7 +320,7 @@
 											<td width="20%" onclick="questionsCollapse('collapseF{{$findex}}')"><?php echo date("m-d-Y",strtotime($fvalue->created_at)); ?></td>
 											<td width="20%" onclick="questionsCollapse('collapseF{{$findex}}')"><span><?php if(count($accutane_que)>0){ echo "Accutane"; }else if(count($topical_que)>0){ echo "Topical"; } ?></span></td>
 											<td width="20%" onclick="questionsCollapse('collapseF{{$findex}}')">Follow Up: {{$fvalue->follow_up_no}}</td>
-											<td width="20%" style="padding-left: 20px;" data-toggle="modal" data-target="#exampleModalCenter"><i class="fa fa-file-text" aria-hidden="true" ></i></td>
+											<td width="20%" style="padding-left: 40px;" data-toggle="modal" data-target="#exampleModalCenter"><i class="fa fa-file-text" aria-hidden="true" ></i></td>
 										</tr>
 									</table>
 								</div>

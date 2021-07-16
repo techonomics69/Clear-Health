20d50e80a4aa880cb56ed82e49c9df70991e05a3
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-16 13:58:12 +0530

    Update PaymentsController.php

--- app/Http/Controllers/API/PaymentsController.php
+++ app/Http/Controllers/API/PaymentsController.php
@@ -203,6 +203,8 @@ class PaymentsController extends BaseController
 
                 // echo $diffCounter;
                 // die();
+                $diffProds = array_diff_assoc($products, $product_id);
+                return $this->sendError($diffProds); 
                 if($products !== $product_id){
                     return $this->sendError('Products are not same');    
                 }else{

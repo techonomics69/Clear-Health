f68afcd13958a1f93f9eb947616feeffe1a8cdeb
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-16 15:30:20 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -1873,7 +1873,7 @@
       </div>
       <div class="modal-body">  
           <button class="collapsible active">Open Section 1</button>
-			<div class="content">
+			<div class="content" style="max-height: 267px;">
 			  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 			</div>
 			<button class="collapsible">Open Section 2</button>

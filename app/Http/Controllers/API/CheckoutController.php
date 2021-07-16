574ce70e31e822eecd6b17e0596c3c874684b73e
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-16 16:38:28 +0530

    Update CheckoutController.php

--- app/Http/Controllers/API/CheckoutController.php
+++ app/Http/Controllers/API/CheckoutController.php
@@ -354,14 +354,17 @@ class CheckoutController extends BaseController
       $md_case_data = Mdcases::where('case_id',$orderlist['md_case_id'])->first();
 
       $system_status = $md_case_data['system_status'];
+      $case_type = $md_case_data['case_type'];
 
     }else{
 
       $system_status = "";
+      $case_type = "";
 
     }
 
     $orderlist['system_status'] = $system_status;
+    $orderlist['case_type'] = $case_type;
       
 
     
@@ -433,7 +436,7 @@ class CheckoutController extends BaseController
 
           $curl = curl_init();
           curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://api.mdintegrations.xyz/v1/partner/pharmacies/' . $pharmacy_id,
+            CURLOPT_URL => 'https://api.mdintegrations.xyz/v1/partner/pharmacies/'.$pharmacy_id,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
@@ -450,11 +453,11 @@ class CheckoutController extends BaseController
           curl_close($curl);
           $response1 = json_decode($response);
           if(isset($response1)){
-            if(is_array($response1)){
-              if(count($response1)>0){
+           /* if(is_array($response1)){
+              if(count($response1)>0){*/
                 $skincare_summary['pharmacy_pickup'] =  $response1->name;
-              }
-            }
+             /* }
+            }*/
           }
           //$products[$product_key]['pharmacy_pickup'] =  $response1->name;
         } else {

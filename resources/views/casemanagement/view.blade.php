6cdc3915de731caae92ce1d5634e606fcc58a611
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-16 16:07:35 +0530

    Update view.blade.php

--- resources/views/casemanagement/view.blade.php
+++ resources/views/casemanagement/view.blade.php
@@ -1886,7 +1886,14 @@
 			</div>
 			<button class="collapsible">07/15/2021</button>
 			<div class="content">
-			  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				<span class="case_status">
+					<p class="status_heading m-0">Case Status</p>
+				     <p> Processing</p>
+				</span>		
+				<span class="case_status">
+					<p class="status_heading m-0">Reason</p>
+				     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+				</span>	
 			</div>
       </div>
     </div>

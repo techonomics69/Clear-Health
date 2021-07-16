876934d47d5da609e17f92e1792c1b21941eee3e
jalpa k <jalpa@itidoltechnologies.com>
2021-07-16 18:29:59 +0530

    Update MessageController.php

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -85,7 +85,7 @@ class MessageController extends Controller
         $message = DB::table('messages')
             ->where('user_id', $data['user_id'])
             ->join('users', 'users.id', '=', 'messages.user_id')
-            ->select('users.first_name, users.last_name, messages.*')
+            ->select('users.first_name, users.last_name')
             ->get();
         $username = '<b>' . $message[0]->first_name . ' ' . $message[0]->last_name . '</b>';
         $user_id = $message[0]->user_id;

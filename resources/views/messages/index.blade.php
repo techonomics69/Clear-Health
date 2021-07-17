56b19033ff8535849eb8f5594f67c118056d7273
jalpa k <jalpa@itidoltechnologies.com>
2021-07-17 13:20:57 +0530

    Update index.blade.php

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -70,7 +70,7 @@
                                             Follow up {{$value->follow_up}}
                                             @endif</td>
                                         <td>{{$value->product_type}}</td>
-                                        <td></td>
+                                        <td><i class="fas fa-envelope-open-text"></i></td>
                                     </tr>
                                     @endforeach
                                 </tbody>

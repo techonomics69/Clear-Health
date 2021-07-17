7f2c12b29ea3254a19b89fd79e61991a0bb37d7f
jalpa k <jalpa@itidoltechnologies.com>
2021-07-17 17:34:19 +0530

    Update view.blade.php

--- resources/views/messages/view.blade.php
+++ resources/views/messages/view.blade.php
@@ -88,43 +88,46 @@
                                                         </div>
                                                         <div class="col-md-9">
                                                             <div class="errwrw">
-                                                            <div class="right-cht ">
-                                                                <div class="chating-section nonmedicalmessages" id="chating-section">
-                                                                    <h3 id="usernameLabelAdmin"></h3>
-                                                                    <ul id="messageDataAdmin"></ul>
-                                                                </div>
-
-                                                                <div class="row">
-                                                                    <div class="col-lg-12">
-                                                                        <div class="imgDiv" id="imgDiv">
-                                                                            <img id="blah" src="#" alt="image" />
-                                                                            <i class="fa fa-close" id="clearImg"></i>
-                                                                        </div>
+                                                                <div class="right-cht ">
+                                                                    <div class="chating-section nonmedicalmessages" id="chating-section">
+                                                                        <h3 id="usernameLabelAdmin"></h3>
+                                                                        <ul id="messageDataAdmin"></ul>
                                                                     </div>
-                                                                </div>
 
-                                                                <div id="last-typing-section" class="last-typing-section">
-                                                                    <div class="attachment lastimg pinclip">
-                                                                        <div class="variants">
-                                                                            <div class='file'>
-                                                                                <label for='file'>
-                                                                                    <img src="{{asset('public/images/paperclip.png')}}" alt="">
-                                                                                </label>
-                                                                                <input id="file" type="file" name="file" onchange="loadFile(event)">
+                                                                    <div class="row">
+                                                                        <div class="col-lg-12">
+                                                                            <div class="imgDiv" id="imgDiv">
+                                                                                <img id="blah" src="#" alt="image" />
+                                                                                <i class="fa fa-close" id="clearImg"></i>
                                                                             </div>
                                                                         </div>
                                                                     </div>
 
-                                                                    <div class="search">
-                                                                        <input class="form-control" type="text" name="text" placeholder="Type a message..." id="text">
-                                                                    </div>
-                                                                    <div class="sending lastimg">
-                                                                        <button type="button" id="sendAdminMsg"><img src="{{asset('public/images/telegram.png')}}" alt=""></button>
-                                                                        <input type="hidden" id="userId" value="">
+                                                                    <div id="last-typing-section" class="last-typing-section">
+                                                                        <form method="post" id="upload-image-form" enctype="multipart/form-data">
+                                                                            @csrf
+                                                                            <div class="attachment lastimg pinclip">
+                                                                                <div class="variants">
+                                                                                    <div class='file'>
+                                                                                        <label for='file'>
+                                                                                            <img src="{{asset('public/images/paperclip.png')}}" alt="">
+                                                                                        </label>
+                                                                                        <input id="file" type="file" name="file" onchange="loadFile(event)">
+                                                                                    </div>
+                                                                                </div>
+                                                                            </div>
+
+                                                                            <div class="search">
+                                                                                <input class="form-control" type="text" name="text" placeholder="Type a message..." id="text">
+                                                                            </div>
+                                                                            <div class="sending lastimg">
+                                                                                <button type="button" id="sendAdminMsg"><img src="{{asset('public/images/telegram.png')}}" alt=""></button>
+                                                                                <input type="hidden" id="userId" value="">
+                                                                            </div>
+                                                                        </form>
                                                                     </div>
                                                                 </div>
                                                             </div>
-                                                        </div>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -190,30 +193,35 @@
         });
 
         $('#sendAdminMsg').on('click', function() {
-            var text = $('#text').val();
-            var image = $('#file').val();
-            console.log(image);
-            var user_id = $('#userId').val();
-            $.ajax({
-                url: "{{route('sendNonMedicalMessage')}}",
-                type: "post",
-                data: formData,
-                contentType: false,
-                processData: false,
-                data: {
-                    "_token": "{{ csrf_token() }}",
-                    user_id: user_id,
-                    text: text
-                },
-                success: function(result) {
-                    if (result != false) {
-                        $('#text').val('');
-                        $('#messageDataAdmin').append(result);
+            $('#upload-image-form').submit(function(e) {
+                var text = $('#text').val();
+                var image = $('#file').val();
+                console.log(image);
+                var user_id = $('#userId').val();
+                let formData = new FormData(this);
+                console.log(formData);
+                return false;
+                $.ajax({
+                    url: "{{route('sendNonMedicalMessage')}}",
+                    type: "post",
+                    data: formData,
+                    contentType: false,
+                    processData: false,
+                    data: {
+                        "_token": "{{ csrf_token() }}",
+                        user_id: user_id,
+                        text: text
+                    },
+                    success: function(result) {
+                        if (result != false) {
+                            $('#text').val('');
+                            $('#messageDataAdmin').append(result);
+                        }
+                        $("#messageDataAdmin").animate({
+                            scrollTop: $("#messageDataAdmin")[0].scrollHeight
+                        }, 1000);
                     }
-                    $("#messageDataAdmin").animate({
-                        scrollTop: $("#messageDataAdmin")[0].scrollHeight
-                    }, 1000);
-                }
+                });
             });
         });
         var input = document.getElementById("text");

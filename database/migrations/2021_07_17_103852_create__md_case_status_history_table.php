b911a4ab5ac1f321073eb52aaa485c933208d77c
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-17 16:21:44 +0530

    Create 2021_07_17_103852_create__md_case_status_history_table.php

---
+++ database/migrations/2021_07_17_103852_create__md_case_status_history_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMdCaseStatusHistoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('md_case_status_history', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->string('case_id');
+            $table->string('md_case_id');
+            $table->string('case_status');
+            $table->text('reason');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_md_case_status_history');
+    }
+}

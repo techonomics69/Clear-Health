67482c9c0ec34bd6512770c5290c335bf5d344dc
jalpa k <jalpa@itidoltechnologies.com>
2021-07-17 13:17:02 +0530

    Update index.blade.php

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -29,52 +29,52 @@
                 <section class="card">
                     <div class="row" style="padding: 20px;">
                         <div class="col-md-12">
-                        <table class="table table-responsive table-striped table-bordered" style=" width:100%" id="CaseManagementList">
-                  <thead>
-                    <tr>
-                      <th>SR</th>
-                      <th>Date</th>
-                      <th>CaseID</th>
-                      <th>FirstName</th>
-                      <th>LastName</th>                      
-                      <th>VisitNumber</th>
-                      <th>MDCaseID</th>
-                      <th>CaseStatus</th>
-                      <th>MDStatus</th>
-                      <th>VisitType</th>
-                      <th>TreatmentPlan</th>                      
-                      <th>Action</th>                      
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                        @foreach($user_case_management_data as $key => $value)                        
-                        <td>{{$key+1}}</td>
-                        <td>{{$value->created_at}}</td>
-                        <td>{{$value->ref_id}}</td>
-                        <td>{{$value->first_name}}</td>
-                        <td>{{$value->last}}</td>                        
-                        <td>
-                            @if(empty($value->follow_up))
-                            1
-                            @else
-                            {{$value->follow_up + 1}}
-                            @endif
-                        </td>
-                        <td>{{$value->md_case_id}}</td>
-                        <td>{{$value->system_status}}</td>
-                        <td>{{$value->md_case_status}}</td>
-                        <td>@if(empty($value->follow_up))
-                            Initial
-                            @else
-                            Follow up {{$value->follow_up}}
-                            @endif</td>
-                        <td>{{$value->product_type}}</td>
-                        <td></td>
-                        @endforeach
-                    </tr>
-                  </tbody>
-                </table>
+                            <table class="table table-responsive table-striped table-bordered" style=" width:100%" id="CaseManagementList">
+                                <thead>
+                                    <tr>
+                                        <th>SR</th>
+                                        <th>Date</th>
+                                        <th>CaseID</th>
+                                        <th>FirstName</th>
+                                        <th>LastName</th>
+                                        <th>VisitNumber</th>
+                                        <th>MDCaseID</th>
+                                        <th>CaseStatus</th>
+                                        <th>MDStatus</th>
+                                        <th>VisitType</th>
+                                        <th>TreatmentPlan</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($user_case_management_data as $key => $value)
+                                    <tr>
+                                        <td>{{$key+1}}</td>
+                                        <td>{{$value->created_at}}</td>
+                                        <td>{{$value->ref_id}}</td>
+                                        <td>{{$value->first_name}}</td>
+                                        <td>{{$value->last}}</td>
+                                        <td>
+                                            @if(empty($value->follow_up))
+                                            1
+                                            @else
+                                            {{$value->follow_up + 1}}
+                                            @endif
+                                        </td>
+                                        <td>{{$value->md_case_id}}</td>
+                                        <td>{{$value->system_status}}</td>
+                                        <td>{{$value->md_case_status}}</td>
+                                        <td>@if(empty($value->follow_up))
+                                            Initial
+                                            @else
+                                            Follow up {{$value->follow_up}}
+                                            @endif</td>
+                                        <td>{{$value->product_type}}</td>
+                                        <td></td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </section>

774b34aa7bb51f1770e6c53e3ebfff7780c46f4e
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-19 14:20:37 +0530

    Update OrderManagementController.php

--- app/Http/Controllers/OrderManagementController.php
+++ app/Http/Controllers/OrderManagementController.php
@@ -156,6 +156,7 @@ public function cancelOrder(Request $request){
         if(count($chargeId) > 0){
             if(!empty($chargeId[0]->transaction_complete_details)){
                 $charge = json_decode($chargeId[0]->transaction_complete_details);
+                dd($charge);
                 $chId = $charge['id'];
                 Stripe::setApiKey('sk_test_51J08tDJofjMgVsOdzxZs5Aqlf5A9riwPPwlxUTriC8YPiHvTjlCBoaMjgxiqdIVfvOMPcllgR9JY7EZlihr6TJHy00ixztHFtz'); 
                     

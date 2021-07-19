ae2d9d1037669be40d9dff7d59559d5be9ed4272
jalpa k <jalpa@itidoltechnologies.com>
2021-07-19 11:07:01 +0530

    Update CaseManagementController.php

--- app/Http/Controllers/API/CaseManagementController.php
+++ app/Http/Controllers/API/CaseManagementController.php
@@ -1044,14 +1044,14 @@ public function CreateCase(Request $request){
   }
 
   public function getMessagesNonMedical(Request $request){
-    //$case_id = $request['case_id'];
+    $case_id = $request['case_id'];
     $user_id = $request['user_id'];
     //$md_case_id = $request['md_case_id'];
 
 
     //$message_details = Messages::join('message_files', 'messages.id', '=', 'message_files.msg_id')->select('messages.*','message_files.*')->where('case_id', $case_id)->where('md_case_id',$md_case_id)->where('user_id',$user_id)->get();
 
-    $message_details = Messages::join('message_files', 'messages.id', '=', 'message_files.msg_id')->join('users', 'users.id', '=', 'messages.user_id')->select('messages.*','message_files.*','users.first_name','users.last_name')->where('user_id',$user_id)->OrderBy('messages.id','asc')->get();
+    $message_details = Messages::join('message_files', 'messages.id', '=', 'message_files.msg_id')->join('users', 'users.id', '=', 'messages.user_id')->select('messages.*','message_files.*','users.first_name','users.last_name')->where('case_id', $case_id)->where('user_id',$user_id)->OrderBy('messages.id','asc')->get();
 
     $message_data = array();
     foreach($message_details as $key=>$value){

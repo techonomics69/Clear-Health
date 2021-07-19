c64815e199efc8e4e0974a7fdc7b6412f668c896
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-19 17:08:16 +0530

    Update HeadingValidation.php

--- app/Imports/HeadingValidation.php
+++ app/Imports/HeadingValidation.php
@@ -5,14 +5,31 @@ namespace App\Imports;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\OnEachRow;
+use Maatwebsite\Excel\Concerns\WithStartRow;
 
-class HeadingValidation implements ToModel
+class HeadingValidation implements OnEachRow
 {
     private $rows = 0;
 
-    public function model(array $row)
+    public function startRow(): int
     {
-        ++$this->rows;
+        return 3;
+    }
+
+    // public function model(array $row)
+    // {
+    //     ++$this->rows;
+    // }
+    public function onRow(Row $row)
+    {
+        $rowIndex = $row->getIndex();
+        $row      = $row->toArray();
+        if($rowIndex == 4){
+            if($row[0]=="Patient ID #"){
+                ++$this->rows;
+            }
+        }
     }
 
     public function getRowCount(): int

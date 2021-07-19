60454584f6d4396dad24f898763744105908211a
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-19 10:55:54 +0530

    Update CheckoutController.php

--- app/Http/Controllers/API/CheckoutController.php
+++ app/Http/Controllers/API/CheckoutController.php
@@ -708,23 +708,12 @@ class CheckoutController extends BaseController
 
       $pharmacy_data  =  Cart::select('pharmacy_pickup')->where('user_id',$user_id)->whereIn('id',$cart_ids)->where('order_type', '!=', 'AddOn')->where('order_type', '!=', 'Non-Prescribe')->first();
 
-      echo "<pre>";
-      print_r($pharmacy_data);
-      echo "<pre>";
-      exit();
-
-
 
       $r = get_token();
       $token_data = json_decode($r);
       $token = $token_data->access_token;
 
       if(isset($md_patient_id) && $md_patient_id != ''){
-
-        echo "<pre>";
-        print_r($pharmacy_data);
-        echo "<pre>";
-        exit();
 
         //code to remove pharmacy of patient
 

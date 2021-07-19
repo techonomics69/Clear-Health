af7d34e14a74f89f640213eb0bf4004c42a5cd20
jalpa k <jalpa@itidoltechnologies.com>
2021-07-19 12:51:46 +0530

    Update view.blade.php

--- resources/views/messages/view.blade.php
+++ resources/views/messages/view.blade.php
@@ -201,7 +201,7 @@
                     $("#messageDataAdmin").animate({
                         scrollTop: $("#messageDataAdmin")[0].scrollHeight
                     }, 1000);
-                    $(this).children("msg_count").hide();
+                    $(this).children(".msg_count").hide();
                 }
             });
         });

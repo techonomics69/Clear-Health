51226536f9dfa1b106dc208df588decb3bff2dab
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-20 11:34:48 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -96,6 +96,7 @@ Route::group(['middleware' => ['auth:api']], function () {
     Route::post('addCheckoutAddress', [CheckoutController::class, 'addCheckoutAddress']);
     Route::post('updateCheckoutAddress', [CheckoutController::class, 'updateCheckoutAddress']);
     Route::post('orderList', [CheckoutController::class, 'orderList']);
+    Route::get("cart/getpresproducts",[CartController::class, 'getCartByUserPresPurchased']);
 
     Route::post('getOrderDetail', [CheckoutController::class, 'getCheckoutdetail']);
     Route::post('getCheckoutAddress', [CheckoutController::class, 'getCheckoutAddress']);

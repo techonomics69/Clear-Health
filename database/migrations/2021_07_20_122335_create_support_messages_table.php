3160e9b5b41d8d9977f0b6d6606d2f50643678bc
phpmaster464 <phpmaster464@gmail.com>
2021-07-20 17:56:31 +0530

    support table

---
+++ database/migrations/2021_07_20_122335_create_support_messages_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSupportMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('support_messages', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('support_messages');
+    }
+}

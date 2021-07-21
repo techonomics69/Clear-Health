59a98b3090a5fc84ac27afdb639d45dcc84ac420
devalfullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-21 10:38:12 +0530

    Update CheckoutController.php

--- app/Http/Controllers/API/CheckoutController.php
+++ app/Http/Controllers/API/CheckoutController.php
@@ -769,7 +769,6 @@ class CheckoutController extends BaseController
 
     $user_id = $request['user_id'];
     //$case_id = $request['case_id'];
-    //$md_case_id = $request['md_case_id'];
     $checkout_id = $request['order_id'];
     $pharmacy_id =  $request['pharmacy_id'];
 
@@ -783,11 +782,7 @@ class CheckoutController extends BaseController
    
       $order_data = Checkout::where('id', $checkout_id)->first();
 
-      echo "<pre>";
-      print_r($order_data);
-      echo "<pre>";
-      exit();
-
+      $md_case_id = $order_data['md_case_id'];
        
       $cart_ids = explode(',', $order_data['cart_id']);
      
@@ -872,6 +867,11 @@ class CheckoutController extends BaseController
       curl_close($curl);
 
       $pharmacy_added = json_decode($response1);
+
+
+      /*update pharmacy id to MD*/
+       $update_prescription_data = UpdateCasePrescriptions($user_id,$md_case_id,$preferred_pharmacy_id,$checkout_id);
+      /*end of update pharmacy id to MD*/
 
        $update_pharmacy = Cart::where('id',$pharmacy_data['id'])->update(['pharmacy_pickup'=>$pharmacy_id]);
        if($update_pharmacy){

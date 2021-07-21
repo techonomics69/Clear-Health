7946b438dd209b1058682fbc285b07091e639b37
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-21 11:02:55 +0530

    Update index.blade.php

--- resources/views/casemanagement/index.blade.php
+++ resources/views/casemanagement/index.blade.php
@@ -245,50 +245,50 @@
     var filter_value = $(this).val();
     $("#CaseManagementList").DataTable().destroy();
     
-      var Datatable = $('#CaseManagementList').DataTable({
-        "dom": '<"top"if>rt<"bottom"lp><"clear">',
-        "bLengthChange": false,
-        "bInfo": false,
-          language: {search: "", searchPlaceholder: "Search"},
-        'searching': true,
-        'processing': true,
-        'serverSide': true,
-        'serverMethod': 'post',
-        "lengthChange": false,
-        "filter": true,
-          //"lengthMenu": [[10, 25, 50, -1], [10, 25, 50, 'All']],
+      // var Datatable = $('#CaseManagementList').DataTable({
+      //   "dom": '<"top"if>rt<"bottom"lp><"clear">',
+      //   "bLengthChange": false,
+      //   "bInfo": false,
+      //     language: {search: "", searchPlaceholder: "Search"},
+      //   'searching': true,
+      //   'processing': true,
+      //   'serverSide': true,
+      //   'serverMethod': 'post',
+      //   "lengthChange": false,
+      //   "filter": true,
+      //     //"lengthMenu": [[10, 25, 50, -1], [10, 25, 50, 'All']],
 
-        'ajax': {
-            'url':url,
-            'data': {_token:token, filterValue:filter_value},
-        },
-        'columns': [
-              { data: 'srno' },
-              { data: 'date' },
-              { data: 'caseid' },
-              { data: 'firstname' },
-              { data: 'lastname' },
-              { data: 'gender' },
-              { data: 'visitnumber' },
-              { data: 'mdcaseid' },
-              { data: 'mdstatus' },
-              { data: 'visittype' },
-              { data: 'treatmentplan' },
-              { data: 'pharmacy' },
-              { data: 'action1' },
-              { data: 'action' },
-              ],aoColumnDefs: [
-                {
-                  bSortable: false,
-                  aTargets: [ 6,8,9,10,11,12 ]
-                }
-          ],
-          language: {
-                "processing": "Loading....."
-            },
-          "order": [[ 0, "desc" ]],  
+      //   'ajax': {
+      //       'url':url,
+      //       'data': {_token:token, filterValue:filter_value},
+      //   },
+      //   'columns': [
+      //         { data: 'srno' },
+      //         { data: 'date' },
+      //         { data: 'caseid' },
+      //         { data: 'firstname' },
+      //         { data: 'lastname' },
+      //         { data: 'gender' },
+      //         { data: 'visitnumber' },
+      //         { data: 'mdcaseid' },
+      //         { data: 'mdstatus' },
+      //         { data: 'visittype' },
+      //         { data: 'treatmentplan' },
+      //         { data: 'pharmacy' },
+      //         { data: 'action1' },
+      //         { data: 'action' },
+      //         ],aoColumnDefs: [
+      //           {
+      //             bSortable: false,
+      //             aTargets: [ 6,8,9,10,11,12 ]
+      //           }
+      //     ],
+      //     language: {
+      //           "processing": "Loading....."
+      //       },
+      //     "order": [[ 0, "desc" ]],  
 
-      });
+      // });
     
   });
    

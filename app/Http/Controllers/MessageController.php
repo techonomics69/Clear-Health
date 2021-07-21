56509e06318ce3b84327a4183f3e25eb0840c7f0
jalpa k <jalpa@itidoltechnologies.com>
2021-07-21 17:00:52 +0530

    Update MessageController.php

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -69,9 +69,10 @@ class MessageController extends Controller
             ->where('case_id', $case_id)
             ->join('users', 'users.id', '=', 'support_messages.user_id')
             ->select(
-                DB::raw('count(*) as user_count, support_messages.user_id, users.first_name, users.last_name, support_messages.case_id'),
-                DB::raw('(SELECT m.text m.md_case_id from support_messages as m where m.user_id=users.id order by m.id desc limit 1) as last_msg'),
-                DB::raw('(SELECT m.created_at from support_messages as m where m.user_id=users.id order by m.id desc limit 1) as msg_time')
+                DB::raw('count(*) as user_count, support_messages.user_id, support_messages.md_case_id, users.first_name, users.last_name, support_messages.case_id'),
+                DB::raw('(SELECT m.text from support_messages as m where m.user_id=users.id order by m.id desc limit 1) as last_msg'),
+                DB::raw('(SELECT m.created_at from support_messages as m where m.user_id=users.id order by m.id desc limit 1) as msg_time'),
+                
             )
             ->groupBy('support_messages.user_id')
             ->orderBy('msg_time', 'desc')

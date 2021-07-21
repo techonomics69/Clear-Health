3a91f55c0c5be7ce6eaea72ca732bf8a067db3f6
jalpa k <jalpa@itidoltechnologies.com>
2021-07-21 17:35:14 +0530

    Update MessageController.php

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -453,8 +453,8 @@ class MessageController extends Controller
         $message = DB::table('support_messages')
             ->where('support_messages.md_case_id', $data['md_case_id'])
             ->join('users', 'users.id', '=', 'support_messages.user_id')
-            ->leftjoin('support_messages_files', 'support_messages.id', '=', 'support_messages_files.msg_id')
-            ->select('users.first_name', 'users.last_name', 'support_messages.user_id', 'support_messages.created_at', 'support_messages.text', 'support_messages.from', 'support_messages_files.url', 'support_messages_files.url_thumbnail', 'support_messages_files.mime_type')
+            //->leftjoin('support_messages_files', 'support_messages.id', '=', 'support_messages_files.msg_id')
+            //->select('users.first_name', 'users.last_name', 'support_messages.user_id', 'support_messages.created_at', 'support_messages.text', 'support_messages.from', 'support_messages_files.url', 'support_messages_files.url_thumbnail', 'support_messages_files.mime_type')
             ->get();
         $username = '<b> MD Support</b>';
 

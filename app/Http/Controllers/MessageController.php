e259c924e087ff28df78cff9d6259c1470a8de57
jalpa k <jalpa@itidoltechnologies.com>
2021-07-21 13:31:51 +0530

    Update MessageController.php

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -241,7 +241,9 @@ class MessageController extends Controller
         $token_data = json_decode($r);
         $token = $token_data->access_token;
     
-               
+        echo '<pre>';
+        print_r($request);
+        die;       
         $documents = $request->file('file');
         $name = $request->name;
         $user_id = $request->user_id;

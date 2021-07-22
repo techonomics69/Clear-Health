c992122618a7084c4dc4a0bc864a7c5b6701e6b6
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-22 17:24:50 +0530

    Update QuizController.php

--- app/Http/Controllers/API/QuizController.php
+++ app/Http/Controllers/API/QuizController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\API\BaseController as BaseController;
 use App\Models\Quiz;
 use App\Models\QuizCategory;
+use App\Models\Birthcontrolanswer;
 use Validator;
 use Exception;
 
@@ -66,6 +67,42 @@ class QuizController extends BaseController
     }
 
     public function StoreBirthControlAnswer(Request $request){
-
+        try{
+            // $validator = Validator::make($data, [
+            //     'user_id' => 'required',
+            //     'case_id' => 'required',
+            //     'answer'  => 'required',
+            //     'last_stpe' => 'required'
+            // ]);
+            // if($validator->fails()){
+            //     return $this->sendError('Validation Error.', $validator->errors()->all());       
+            // }
+            unset($request->_method);
+            unset($request->_token);
+            $data = $request->all();
+            if(isset($data['id'])){    
+                $getFirst = Birthcontrolanswer::select('answer')->where('id',$data['id'])->get();
+                if(count($getFirst)>0){
+                    $que = json_decode($getFirst[0]->answer);
+                    $arr1 = array();
+                    if(count($que) > 0){
+                        foreach($que as $k => $v){
+                            array_push($arr1, $que[$k]);
+                        }
+                    }
+                    array_push($arr1, $data['answer']);
+                    $data['answer'] = $arr1;
+                    $update = Birthcontrolanswer::where('id',$data['id'])->update($data);
+                    return $this->sendResponse(array(), 'Data Updated Successfully');
+                }else{
+                    return $this->sendError('No Records found');
+                }
+            }else{
+                $insert = Birthcontrolanswer::create($data);
+                return $this->sendResponse(array(), 'Data Saved Successfully');
+            }
+        }catch(\Exception $ex){
+            return $this->sendError('Server error',array($ex->getMessage()));
+        }
     }
 }

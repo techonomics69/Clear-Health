09e944bd1d9f4776b0f0e5cf9af04d22c8062827
jalpa k <jalpa@itidoltechnologies.com>
2021-07-22 15:12:27 +0530

    Update 2021_07_22_082209_create_change_treatments_table.php

--- database/migrations/2021_07_22_082209_create_change_treatments_table.php
+++ database/migrations/2021_07_22_082209_create_change_treatments_table.php
@@ -15,6 +15,23 @@ class CreateChangeTreatmentsTable extends Migration
     {
         Schema::create('change_treatments', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
+            $table->integer('case_id');
+            $table->string('md_case_id')->nullable();
+            $table->longtext('answer')->nullable();
+            $table->string('order_id')->nullable();
+            $table->string('product_type')->nullable();
+            $table->string('left_face')->nullable();
+            $table->string('left_face_file_id')->nullable();
+            $table->string('right_face')->nullable();
+            $table->string('right_face_file_id')->nullable();
+            $table->string('center_face')->nullable();
+            $table->string('center_face_file_id')->nullable();
+            $table->string('back_photo')->nullable();
+            $table->string('back_photo_file_id')->nullable();
+            $table->string('chest_photo')->nullable();
+            $table->string('chest_photo_file_id')->nullable();
+            $table->string('status')->default('pending');
             $table->timestamps();
         });
     }

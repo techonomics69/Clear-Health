45ae332a3ff148c036ef5844847c69db2211c5b7
Deval Fullstackdeveloper <52404213+devalfullstackdeveloper@users.noreply.github.com>
2021-07-22 13:23:35 +0530

    Update index.blade.php

--- resources/views/dashboards/index.blade.php
+++ resources/views/dashboards/index.blade.php
@@ -120,14 +120,14 @@
         </div>
 
         <div class="col-xl-4 col-lg-4 col-md-6 col-12">
-          <div class="card">
+          <div class="card bg-midnight-bloom">
             <div class="card-content">
               <a href="{{ route('ordermanagement.index') }}">
               <div class="media align-items-stretch">
-                <div class="p-2 text-center bg-danger bg-darken-2" style="background-color: #323232 !important;">
+                <div class="p-2 text-center">
                   <i class="icon-user font-large-2 white"></i>
                 </div>
-                <div class="p-2  white media-body" style="background-color: #323232 !important;">
+                <div class="p-2  white media-body">
                   <h5>Non-Prescribed orders</h5>
                   <h5>{{ $non_prescribed_orders }}</h5>
                 </div>
